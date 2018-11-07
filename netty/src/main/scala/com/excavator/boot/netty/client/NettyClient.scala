7faf8f33b52c97c925ebb129303ad08b5d269a81
CMonkey <42.codemonkey@gmail.com>
2018-11-07 11:08:54 +0800

    add NettyClient.this method

--- netty/src/main/scala/com/excavator/boot/netty/client/NettyClient.scala
+++ netty/src/main/scala/com/excavator/boot/netty/client/NettyClient.scala
@@ -1,3 +1,12 @@
-package com.excavator.boot.netty
+package com.excavator.boot.netty.client
 
-class NettyClient()
+import java.nio.charset.Charset
+
+import com.google.common.base.Charsets
+
+class NettyClient(host:String, port:Int, charset: Charset){
+  def this(host:String, port:Int) = {
+    this(host, port, Charsets.UTF_8)
+  }
+
+}
