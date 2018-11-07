f916a4d2327fd84679c1f5d162eb8afafd0a1179
CMonkey <42.codemonkey@gmail.com>
2018-11-07 12:10:16 +0800

    add component{RpcDecoder RpcHandler}

---
+++ netty/src/main/scala/com/excavator/boot/netty/component/RpcDecoder.scala
@@ -0,0 +1,69 @@
+package com.excavator.boot.netty.component
+
+import java.nio.charset.Charset
+
+import com.excavator.boot.netty.enums.ResponseViewMode
+import io.netty.buffer.ByteBuf
+import io.netty.channel.ChannelHandlerContext
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder
+import org.slf4j.LoggerFactory
+
+class RpcDecoder(maxFrameLength: Int, position: Int, charset: Charset, responseViewMode: ResponseViewMode)
+  extends LengthFieldBasedFrameDecoder(maxFrameLength, 0, position, 0, position){
+  val logger = LoggerFactory.getLogger(classOf[RpcDecoder])
+
+  if(logger.isDebugEnabled()){
+    logger.debug(s"rpcDecoder init maxFrameLength = ${maxFrameLength}, position = ${position}, charset = ${charset}, responseViewMode = ${responseViewMode}")
+  }
+
+  override def decode(ctx: ChannelHandlerContext, in: ByteBuf): AnyRef = {
+
+    if(in.readableBytes() < position){
+      return null
+    }
+
+    val beginIndex = in.readerIndex()
+
+    val headLengthByte = Array.fill[Byte](position)(0)
+
+    in.readBytes(headLengthByte)
+
+    val headLength = new String(headLengthByte, charset)
+
+    val length = Integer.parseInt(headLength)
+
+    if(logger.isDebugEnabled){
+      logger.debug(s"decode headLength = ${headLength} to IntLength = ${length}")
+    }
+
+    if((in.readableBytes() + 1 ) < length){
+      in.readerIndex(beginIndex)
+      return null
+    }
+
+    in.readerIndex(beginIndex + position + length)
+
+    val dataByte = in.slice(position, length)
+    dataByte.retain()
+
+    val bodyByte = Array.fill[Byte](length)(0)
+    dataByte.readBytes(bodyByte)
+
+    var msg = new String(bodyByte, charset)
+
+    if(responseViewMode == ResponseViewMode.FULL){
+      msg = headLength + msg
+
+      if(logger.isDebugEnabled()){
+        logger.debug(s"decode responseViewMode = ${responseViewMode}, fullMsg = ${msg}")
+      }
+    }else{
+      if(logger.isDebugEnabled()){
+        logger.debug(s"decode responseViewMode = ${responseViewMode}, bodyMsg = ${msg}")
+      }
+    }
+
+    msg
+  }
+
+}
