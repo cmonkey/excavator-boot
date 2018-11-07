3048087f9f73f75fc0f7fbfb363fc4fcf8261d22
CMonkey <42.codemonkey@gmail.com>
2018-11-07 15:59:02 +0800

    add LengthFieldRpcHandler

---
+++ netty/src/main/scala/com/excavator/boot/netty/component/LengthFieldRpcHandler.scala
@@ -0,0 +1,33 @@
+package com.excavator.boot.netty.component
+
+import java.util.UUID
+
+import io.netty.channel.ChannelHandler.Sharable
+import io.netty.channel.{ChannelHandlerContext, SimpleChannelInboundHandler}
+import org.slf4j.LoggerFactory
+
+@Sharable
+class LengthFieldRpcHandler extends SimpleChannelInboundHandler[String] {
+
+  val logger = LoggerFactory.getLogger(classOf[LengthFieldRpcHandler])
+
+  override def channelRead0(ctx: ChannelHandlerContext, msg: String): Unit = {
+    val remoteAddress = ctx.channel.remoteAddress.toString
+    val localAddress = ctx.channel.localAddress.toString
+
+    val globalId = UUID.randomUUID.toString.replaceAll("-", "")
+
+    var result = msg
+
+    if(logger.isDebugEnabled()) {
+      logger.debug(s"remoteAddress = ${remoteAddress}, localAddress = ${localAddress}")
+      logger.debug(s"Channel send msg  = ${result}")
+    }
+
+    val length = result.getBytes.length
+
+    result = "%08d".format(length) + result
+
+    ctx.pipeline.writeAndFlush(result)
+  }
+}
