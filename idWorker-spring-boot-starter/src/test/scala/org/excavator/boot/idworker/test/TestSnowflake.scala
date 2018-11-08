2e973e03352ca56de3b663c09d61ee985595d7f7
CMonkey <42.codemonkey@gmail.com>
2018-11-08 10:53:47 +0800

    change TestSnowflake

--- idWorker-spring-boot-starter/src/test/scala/org/excavator/boot/idworker/test/TestSnowflake.scala
+++ idWorker-spring-boot-starter/src/test/scala/org/excavator/boot/idworker/test/TestSnowflake.scala
@@ -1,5 +1,26 @@
 package org.excavator.boot.idworker.test
 
-object TestSnowflake {
+import org.excavator.boot.idworker.algorithm.Snowflake
 
+object TestSnowflake extends App{
+
+  val snowflake = Snowflake.create(1)
+
+  print(s"${snowflake.nextId()}")
+
+  val groupName = "sss"
+  val workerId = "bbb"
+  println(s"/${groupName}/${workerId}")
+
+  val size = 1000
+
+  val ids = new Array[Long](size)
+
+  println(ids.size)
+
+  for(i <- 0 until size){
+    ids(i) = 10L
+  }
+
+  println(ids.size)
 }
