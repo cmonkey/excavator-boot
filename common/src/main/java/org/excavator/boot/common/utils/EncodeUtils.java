18dffd6e95c3d8bf9c60540683798b85b32174db
CMonkey <42.codemonkey@gmail.com>
2018-11-08 15:21:52 +0800

    add utils package

---
+++ common/src/main/java/org/excavator/boot/common/utils/EncodeUtils.java
@@ -0,0 +1,259 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.excavator.boot.common.utils;
+
+import org.excavator.boot.common.exception.BusinessException;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+/**
+ * 封装各种格式的编码解码工具类.
+ * 1.Commons-Codec的 hex/base64 编码
+ * 2.自制的base62 编码
+ * 3.Commons-Lang的xml/html escape
+ * 4.JDK提供的URLEncoder
+ */
+public class EncodeUtils {
+
+    private static final String DEFAULT_URL_ENCODING = "UTF-8";
+    private static final char[] BASE62               = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+                                                         .toCharArray();
+
+    private static final Logger LOGGER               = LoggerFactory.getLogger(EncodeUtils.class);
+
+    /**
+     * Hex编码.
+     */
+    public static String encodeHex(byte[] input) {
+        return new String(Hex.encodeHex(input));
+    }
+
+    /**
+     * Hex解码.
+     */
+    public static byte[] decodeHex(String input) {
+        try {
+            return Hex.decodeHex(input.toCharArray());
+        } catch (DecoderException e) {
+            throw new BusinessException(e.getMessage());
+        }
+    }
+
+    /**
+     * Base64编码.
+     */
+    public static String encodeBase64(byte[] input) {
+        return new String(Base64.encodeBase64(input));
+    }
+
+    /**
+     * Base64编码.
+     */
+    public static String encodeBase64(String input) {
+        try {
+            return new String(Base64.encodeBase64(input.getBytes(DEFAULT_URL_ENCODING)));
+        } catch (UnsupportedEncodingException e) {
+            return "";
+        }
+    }
+
+    //	/**
+    //	 * Base64编码, URL安全(将Base64中的URL非法字符'+'和'/'转为'-'和'_', 见RFC3548).
+    //	 */
+    //	public static String encodeUrlSafeBase64(byte[] input) {
+    //		return Base64.encodeBase64URLSafe(input);
+    //	}
+
+    /**
+     * Base64解码.
+     */
+    public static byte[] decodeBase64(String input) {
+        return Base64.decodeBase64(input.getBytes());
+    }
+
+    /**
+     * Base64解码.
+     */
+    public static String decodeBase64String(String input) {
+        try {
+            return new String(Base64.decodeBase64(input.getBytes()), DEFAULT_URL_ENCODING);
+        } catch (UnsupportedEncodingException e) {
+            return "";
+        }
+    }
+
+    /**
+     * Base62编码。
+     */
+    public static String encodeBase62(byte[] input) {
+        char[] chars = new char[input.length];
+        for (int i = 0; i < input.length; i++) {
+            chars[i] = BASE62[((input[i] & 0xFF) % BASE62.length)];
+        }
+        return new String(chars);
+    }
+
+    /**
+     * Html 转码.
+     */
+    public static String escapeHtml(String html) {
+        return StringEscapeUtils.escapeHtml4(html);
+    }
+
+    /**
+     * Html 解码.
+     */
+    public static String unescapeHtml(String htmlEscaped) {
+        return StringEscapeUtils.unescapeHtml4(htmlEscaped);
+    }
+
+    /**
+     * Xml 转码.
+     */
+    public static String escapeXml(String xml) {
+        return StringEscapeUtils.escapeXml10(xml);
+    }
+
+    /**
+     * Xml 解码.
+     */
+    public static String unescapeXml(String xmlEscaped) {
+        return StringEscapeUtils.unescapeXml(xmlEscaped);
+    }
+
+    /**
+     * URL 编码, Encode默认为UTF-8.
+     */
+    public static String urlEncode(String part) {
+        try {
+            return URLEncoder.encode(part, DEFAULT_URL_ENCODING);
+        } catch (UnsupportedEncodingException e) {
+            throw new BusinessException(e.getMessage());
+        }
+    }
+
+    /**
+     * URL 解码, Encode默认为UTF-8.
+     */
+    public static String urlDecode(String param) {
+        String content = null;
+        try {
+            content = URLDecoder.decode(param, DEFAULT_URL_ENCODING);
+        } catch (UnsupportedEncodingException e) {
+            LOGGER.error("url decode failed ", e.getMessage());
+        }
+        return content;
+    }
+
+    /**
+     * URL 解码, Encode默认为UTF-8.
+     */
+    public static String[] urlDecode(String param[]) {
+        if (param == null) {
+            return null;
+        }
+        for (int i = 0; i < param.length; i++) {
+            param[i] = urlDecode(param[i]);
+        }
+        return param;
+    }
+
+    /**
+     * unicode解码
+     * @param theString
+     * @return
+     */
+    public static String unicodeDecode(String theString) {
+        char aChar;
+        int len = theString.length();
+        StringBuilder outBuffer = new StringBuilder(len);
+        for (int x = 0; x < len;) {
+            aChar = theString.charAt(x++);
+            if (aChar == '\\') {
+                aChar = theString.charAt(x++);
+
+                if (aChar == 'u') {
+                    // Read the xxxx
+                    int value = 0;
+                    for (int i = 0; i < 4; i++) {
+                        aChar = theString.charAt(x++);
+                        switch (aChar) {
+                            case '0':
+                            case '1':
+                            case '2':
+                            case '3':
+                            case '4':
+                            case '5':
+                            case '6':
+                            case '7':
+                            case '8':
+                            case '9':
+                                value = (value << 4) + aChar - '0';
+                                break;
+                            case 'a':
+                            case 'b':
+                            case 'c':
+                            case 'd':
+                            case 'e':
+                            case 'f':
+                                value = (value << 4) + 10 + aChar - 'a';
+                                break;
+                            case 'A':
+                            case 'B':
+                            case 'C':
+                            case 'D':
+                            case 'E':
+                            case 'F':
+                                value = (value << 4) + 10 + aChar - 'A';
+                                break;
+                            default:
+                                throw new IllegalArgumentException(
+                                    "Malformed   \\uxxxx   encoding.");
+                        }
+                    }
+                    outBuffer.append((char) value);
+                } else {
+                    switch (aChar) {
+                        case 't':
+                            aChar = '\t';
+                            break;
+                        case 'r':
+                            aChar = '\r';
+                            break;
+                        case 'n':
+                            aChar = '\n';
+                            break;
+                        case 'f':
+                            aChar = '\f';
+                            break;
+                    }
+                    outBuffer.append(aChar);
+                }
+            } else
+                outBuffer.append(aChar);
+        }
+        return outBuffer.toString();
+    }
+}
