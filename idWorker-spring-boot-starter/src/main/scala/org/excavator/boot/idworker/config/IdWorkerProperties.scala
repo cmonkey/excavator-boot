5a9e37f272cdbc26893588449deaf870538d52f6
CMonkey <42.codemonkey@gmail.com>
2018-11-08 10:53:36 +0800

    add IdWorkerProperties

--- idWorker-spring-boot-starter/src/main/scala/org/excavator/boot/idworker/config/IdWorkerProperties.scala
+++ idWorker-spring-boot-starter/src/main/scala/org/excavator/boot/idworker/config/IdWorkerProperties.scala
@@ -1,5 +1,26 @@
 package org.excavator.boot.idworker.config
 
-class IdWorkerProperties {
+import org.springframework.boot.context.properties.ConfigurationProperties
 
+import scala.beans.BeanProperty
+
+@ConfigurationProperties("idWorker")
+case class IdWorkerProperties() {
+  @BeanProperty var group: String = "default"
+
+  @BeanProperty var serverList: String = _
+
+  @BeanProperty var namespace: String = "idWorker"
+
+  @BeanProperty var baseSleepTimeMillisSeconds: Int = 1000
+
+  @BeanProperty var maxSleepTimeMillisSeconds: Int = 3000
+
+  @BeanProperty var maxRetries: Int = 3
+
+  @BeanProperty var sessionTimeoutMillisSeconds: Int = _
+
+  @BeanProperty var connectionTimeoutMillisSeconds: Int = _
+
+  @BeanProperty var digest: String = _
 }
