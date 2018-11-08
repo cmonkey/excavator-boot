9d74d27679817cda12b1c9522be5236028561fbe
CMonkey <42.codemonkey@gmail.com>
2018-11-08 15:21:45 +0800

    add helper package

---
+++ common/src/main/java/org/excavator/boot/common/helper/HttpHeader.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.excavator.boot.common.helper;
+
+public enum HttpHeader {
+
+    AUTHORIZATION("Authorization"),
+
+    AUTHENTICATION_TYPE_BASIC("Basic"),
+
+    X_AUTH_TOKEN("X-AUTH-TOKEN"),
+
+    WWW_Authenticate("WWW-Authenticate"),
+
+    X_FORWARDED_FOR("X-Forwarded-For"),
+
+    PROXY_CLIENT_IP("Proxy-Client-IP"),
+
+    WL_PROXY_CLIENT_IP("WL-Proxy-Client-IP"),
+
+    HTTP_CLIENT_IP("HTTP_CLIENT_IP"),
+
+    HTTP_X_FORWARDED_FOR("HTTP_X_FORWARDED_FOR");
+
+    private String key;
+
+    HttpHeader(String key) {
+        this.key = key;
+    }
+
+    public String key() {
+        return this.key;
+    }
+}
---
+++ common/src/main/java/org/excavator/boot/common/helper/RemoteIpHelper.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.excavator.boot.common.helper;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.excavator.boot.common.helper.HttpHeader.*;
+
+public class RemoteIpHelper {
+
+    private static final String UNKNOWN = "unknown";
+
+    public static String getRemoteIpFrom(HttpServletRequest request) {
+        String ip = null;
+        int tryCount = 1;
+
+        while (!isIpFound(ip) && tryCount <= 6) {
+            switch (tryCount) {
+                case 1:
+                    ip = request.getHeader(X_FORWARDED_FOR.key());
+                    break;
+                case 2:
+                    ip = request.getHeader(PROXY_CLIENT_IP.key());
+                    break;
+                case 3:
+                    ip = request.getHeader(WL_PROXY_CLIENT_IP.key());
+                    break;
+                case 4:
+                    ip = request.getHeader(HTTP_CLIENT_IP.key());
+                    break;
+                case 5:
+                    ip = request.getHeader(HTTP_X_FORWARDED_FOR.key());
+                    break;
+                default:
+                    ip = request.getRemoteAddr();
+            }
+
+            tryCount++;
+        }
+
+        return ip;
+    }
+
+    private static boolean isIpFound(String ip) {
+        return ip != null && ip.length() > 0 && !UNKNOWN.equalsIgnoreCase(ip);
+    }
+}
