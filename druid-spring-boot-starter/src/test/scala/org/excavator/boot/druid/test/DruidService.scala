58053694eb81da38f318bac412a68831488dda27
CMonkey <42.codemonkey@gmail.com>
2018-11-09 11:57:03 +0800

    add DruidService.getId function

--- druid-spring-boot-starter/src/test/scala/org/excavator/boot/druid/test/DruidService.scala
+++ druid-spring-boot-starter/src/test/scala/org/excavator/boot/druid/test/DruidService.scala
@@ -1,5 +1,25 @@
 package org.excavator.boot.druid.test
 
-class DruidService {
+import javax.annotation.Resource
+import javax.sql.DataSource
+import org.springframework.stereotype.Service
 
+@Service
+class DruidService{
+
+  @Resource
+  val dataSource: DataSource = null
+
+
+  def getId(): Long = {
+
+    val sql = "select id from orderRecord"
+    val rs = dataSource.getConnection.prepareStatement(sql).executeQuery()
+    var id = 0L
+    while(rs.next()){
+      id = rs.getLong("id")
+    }
+
+    id
+  }
 }
