9ba48d0d6b1b26a0462ffc821fa038fc711d0bde
CMonkey <42.codemonkey@gmail.com>
2018-11-09 12:05:59 +0800

    change DruidApplication

--- druid-spring-boot-starter/src/test/scala/org/excavator/boot/druid/test/DruidApplication.scala
+++ druid-spring-boot-starter/src/test/scala/org/excavator/boot/druid/test/DruidApplication.scala
@@ -2,17 +2,14 @@ package org.excavator.boot.druid.test
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.ComponentScan
 
+@SpringBootApplication
 class DruidApplication {
+}
+
+object DruidApplication{
 
   def main(args: String*) = {
     SpringApplication.run(classOf[DruidApplication], args:_*)
   }
-}
-
-@SpringBootApplication
-//@ComponentScan(basePackages = Array("org.excavator.boot.druid"))
-object DruidApplication{
-
 }
