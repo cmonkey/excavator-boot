6e02b66f211cdd3a1609a2e2a38ff64452ec302a
CMonkey <42.codemonkey@gmail.com>
2018-11-09 15:18:17 +0800

    add KafkaTests to test

---
+++ kafka-spring-boot-starter/src/test/scala/org/excavator/boot/kafka/test/KafkaTests.scala
@@ -0,0 +1,35 @@
+package org.excavator.boot.kafka.test
+
+import java.util.concurrent.TimeUnit
+
+import javax.annotation.Resource
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.Assert._
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(classOf[SpringRunner])
+@SpringBootTest(classes = Array(classOf[KafkaApplication]))
+class KafkaTests {
+
+  @Resource
+  val kafkaService: KafkaService = null
+
+  val msg = "foo testing"
+
+  @Test
+  def testSendMsg() = {
+    val topic = "foo"
+    val r = kafkaService.sendMsg(topic, msg)
+
+    assertNotNull(r)
+  }
+
+  @Test
+  def pollMsg(): Unit = {
+    val r = kafkaService.takeMsg()
+
+    assertEquals(msg, r)
+  }
+}
