f4a736b498ff33d1b3da7f7d6d63854b705ae5a5
CMonkey <42.codemonkey@gmail.com>
2018-11-09 10:43:45 +0800

    add DruidDataSourceAutoConfiguration

---
+++ druid-spring-boot-starter/src/main/scala/org/excavator/boot/druid/autoconfigure/DruidDataSourceAutoConfiguration.scala
@@ -0,0 +1,59 @@
+package org.excavator.boot.druid.autoconfigure
+
+import java.sql.SQLException
+
+import com.alibaba.druid.pool.DruidDataSource
+import org.excavator.boot.druid.properties.DruidDataSourceProperties
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.AutoConfigureBefore
+import org.springframework.boot.autoconfigure.condition.{ConditionalOnClass, ConditionalOnMissingBean}
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.{Bean, Configuration}
+;
+
+@Configuration
+@ConditionalOnClass(DruidDataSource.class)
+@EnableConfigurationProperties(DruidDataSourceProperties.class)
+@AutoConfigureBefore(DataSourceAutoConfiguration.class)
+class DruidDataSourceAutoConfiguration{
+  val logger = LoggerFactory.getLogger(classOf[DruidDataSourceAutoConfiguration])
+
+  @Bean(destroyMethod = "close")
+  @ConditionalOnMissingBean
+  def dataSource(properties: DruidDataSourceProperties) = {
+    val druidDataSource = new DruidDataSource()
+
+      druidDataSource.setDriverClassName(properties.getDriverClassName());
+      druidDataSource.setUrl(properties.getUrl());
+      druidDataSource.setUsername(properties.getUsername());
+      druidDataSource.setPassword(properties.getPassword());
+      druidDataSource.setInitialSize(properties.getInitialSize());
+      druidDataSource.setMinIdle(properties.getMinIdle());
+      druidDataSource.setMaxActive(properties.getMaxActive());
+      druidDataSource.setMaxWait(properties.getMaxWait());
+      druidDataSource.setTimeBetweenEvictionRunsMillis(properties
+          .getTimeBetweenEvictionRunsMillis());
+      druidDataSource.setMinEvictableIdleTimeMillis(properties.getMinEvictableIdleTimeMillis());
+      druidDataSource.setValidationQuery(properties.getValidationQuery());
+      druidDataSource.setTestWhileIdle(properties.getTestWhileIdle());
+      druidDataSource.setTestOnBorrow(properties.getTestOnBorrow());
+      druidDataSource.setTestOnReturn(properties.getTestOnReturn());
+      druidDataSource.setPoolPreparedStatements(properties.getPoolPreparedStatements());
+      druidDataSource.setMaxPoolPreparedStatementPerConnectionSize(properties
+          .getMaxPoolPreparedStatementPerConnectionSize());
+      druidDataSource.setConnectionProperties(properties.getConnectionProperties());
+
+      try {
+          druidDataSource.setFilters(properties.getFilters());
+          druidDataSource.init();
+      } catch {
+        case e:SQLException => e.printStackTrace()
+      }
+
+      logger.info(s"init druidDataSource in properties ${properties}")
+
+      druidDataSource
+
+  }
+}
