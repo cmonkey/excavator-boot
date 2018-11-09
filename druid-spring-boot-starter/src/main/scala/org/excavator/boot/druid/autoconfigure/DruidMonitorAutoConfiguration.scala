cf76c6af4609d8156f1466fdf9c9d8fe93b41266
CMonkey <42.codemonkey@gmail.com>
2018-11-09 11:56:10 +0800

    add logger to DruidMonitorAutoConfiguration

--- druid-spring-boot-starter/src/main/scala/org/excavator/boot/druid/autoconfigure/DruidMonitorAutoConfiguration.scala
+++ druid-spring-boot-starter/src/main/scala/org/excavator/boot/druid/autoconfigure/DruidMonitorAutoConfiguration.scala
@@ -9,17 +9,20 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 import com.alibaba.druid.support.http.StatViewServlet
 import com.alibaba.druid.support.http.WebStatFilter
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.boot.web.servlet.ServletRegistrationBean
 import org.springframework.context.annotation.Bean
 
 @Configuration
-@ConditionalOnClass(DruidDataSource.class)
-@EnableConfigurationProperties(DruidMonitorProperties.class)
+@ConditionalOnClass(Array(classOf[DruidDataSource]))
+@EnableConfigurationProperties(Array(classOf[DruidMonitorProperties]))
 @ConditionalOnProperty(prefix = "druid.monitor", name = Array("enabled"), havingValue = "true")
-@AutoConfigureAfter(DataSource.class)
-class DruidMonitorAutoCofngiuration{
+@AutoConfigureAfter(Array(classOf[DataSource]))
+class DruidMonitorAutoConfigiuration{
+
+  val logger = LoggerFactory.getLogger(classOf[DruidMonitorAutoConfigiuration])
 
   @Bean
   @ConditionalOnMissingBean
@@ -35,16 +38,22 @@ class DruidMonitorAutoCofngiuration{
     servletRegistrationBean.addInitParameter("loginPassword", properties.getLoginPassword)
     //是否能够重置数据.
     servletRegistrationBean.addInitParameter("resetEnable", properties.getResetEnable)
+
+    logger.info(s"init druidServlet in servletRegistrationBean = ${servletRegistrationBean}")
+
     servletRegistrationBean
   }
 
   @Bean
-  @ConditionalOnMissingBean def filterRegistrationBean(properties: DruidMonitorProperties): FilterRegistrationBean = {
+  @ConditionalOnMissingBean
+  def filterRegistrationBean(properties: DruidMonitorProperties): FilterRegistrationBean = {
     val filterRegistrationBean = new FilterRegistrationBean
 
     filterRegistrationBean.setFilter(new WebStatFilter)
     filterRegistrationBean.addUrlPatterns(properties.getUrlPatterns)
     filterRegistrationBean.addInitParameter("exclusions", properties.getExclusions)
+
+    logger.info(s"init filterRegistrationBean = ${filterRegistrationBean}")
 
     filterRegistrationBean
   }
