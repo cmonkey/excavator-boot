cb8a9dc04a9493ca8dfefebd100ac071745b280b
CMonkey <42.codemonkey@gmail.com>
2018-11-09 14:54:41 +0800

    add KafkaProducerAutoConfiguration.producerFactory function

--- kafka-spring-boot-starter/src/main/scala/org/excavator/boot/kafka/autoconfigure/KafkaProducerAutoConfiguration.scala
+++ kafka-spring-boot-starter/src/main/scala/org/excavator/boot/kafka/autoconfigure/KafkaProducerAutoConfiguration.scala
@@ -1,5 +1,33 @@
 package org.excavator.boot.kafka.autoconfigure
 
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.context.annotation.{Bean, Configuration}
+import org.springframework.kafka.core.{DefaultKafkaProducerFactory, ProducerFactory}
+
+@Configuration
 class KafkaProducerAutoConfiguration {
+
+  @Value("${spring.kafka.bootstrap-servers}")
+  val bootstrapServers:String =  null
+
+  @Bean
+  @ConditionalOnMissingBean
+  def producerFactory(): ProducerFactory[String, String] = {
+    val map = Map(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG -> bootstrapServers, 
+      ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG -> classOf[StringSerializer], 
+      ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG -> classOf(StringSerializer), 
+      ProducerConfig.RETRIES_CONFIG -> 0, 
+      ProducerConfig.BATCH_SIZE_CONFIG ->16304,
+      ProducerConfig.BUFFER_MEMORY_CONFIG -> 33354432
+      )
+
+    import scala.collection.JavaConverters._
+    val javaMap = map.map{case (k, v) => k -> v.asInstanceOf[Object]}.asJava
+
+    new DefaultKafkaProducerFactory[String, String](javaMap)
+  }
 
 }
