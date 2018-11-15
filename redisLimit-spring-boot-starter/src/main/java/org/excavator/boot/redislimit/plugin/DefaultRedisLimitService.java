036d8f9080ef429f53146408d579eb9c15271117
CMonkey <42.codemonkey@gmail.com>
2018-11-15 11:19:21 +0800

    add DefaultRedisLimitService

---
+++ redisLimit-spring-boot-starter/src/main/java/org/excavator/boot/redislimit/plugin/DefaultRedisLimitService.java
@@ -0,0 +1,6 @@
+package org.excavator.boot.redislimit.plugin;
+
+import org.excavator.boot.redislimit.service.RedisLimitService;
+
+public class DefaultRedisLimitService implements RedisLimitService {
+}
