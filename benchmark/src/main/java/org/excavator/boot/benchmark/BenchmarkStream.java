5cbfc4e1c9f5c0b7216614cd5a4ae547ac5c5b90
CMonkey <42.codemonkey@gmail.com>
2018-11-21 17:13:15 +0800

    add License to BenchmarkStream BenchmarkRunner

--- benchmark/src/main/java/org/excavator/boot/benchmark/BenchmarkStream.java
+++ benchmark/src/main/java/org/excavator/boot/benchmark/BenchmarkStream.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.excavator.boot.benchmark;
 
 import org.openjdk.jmh.Main;
@@ -14,7 +30,7 @@ public class BenchmarkStream {
     public static int[] ints;
 
     @Setup
-    public void setup(){
+    public void setup() {
         ints = new int[1000_000_00];
 
         Random random = new Random();
@@ -26,14 +42,14 @@ public class BenchmarkStream {
 
     @Benchmark
     @Fork(value = 1, warmups = 1)
-    public static void forLoop(){
+    public static void forLoop() {
 
         int[] a = ints;
         int e = a.length;
         int m = Integer.MIN_VALUE;
 
         for (int i = 0; i < e; i++) {
-            if(a[i] > m){
+            if (a[i] > m) {
                 m = a[i];
             }
         }
