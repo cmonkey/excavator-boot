4bfe7dede08f4a86b4c06d3b2c07522845a133ad
CMonkey <42.codemonkey@gmail.com>
2018-11-21 17:14:10 +0800

    add format style to benchmark

--- benchmark/src/main/java/org/excavator/boot/benchmark/CountPerf.java
+++ benchmark/src/main/java/org/excavator/boot/benchmark/CountPerf.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.excavator.boot.benchmark;
 
 import org.openjdk.jmh.annotations.*;
@@ -12,28 +28,24 @@ import java.util.concurrent.TimeUnit;
 @OutputTimeUnit(TimeUnit.SECONDS)
 @State(Scope.Thread)
 public class CountPerf {
-    
+
     @Benchmark
-    public void count(){
+    public void count() {
         for (int i = 0; i < 1_000_000; i++) {
-            
+
         }
     }
 
     @Benchmark
-    public void countSub(){
-        for (int i = 1_000_000; i > 0; i--){
+    public void countSub() {
+        for (int i = 1_000_000; i > 0; i--) {
 
         }
     }
 
     public static void main(String[] args) throws RunnerException {
-        Options opts = new OptionsBuilder()
-                .include(CountPerf.class.getSimpleName())
-                .forks(2)
-                .warmupIterations(5)
-                .measurementIterations(5)
-                .build();
+        Options opts = new OptionsBuilder().include(CountPerf.class.getSimpleName()).forks(2)
+            .warmupIterations(5).measurementIterations(5).build();
 
         new Runner(opts).run();
     }
