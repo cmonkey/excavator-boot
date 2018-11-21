1530cb54b456cd0e89fe830d90b336a8b0834b38
CMonkey <42.codemonkey@gmail.com>
2018-11-21 15:21:58 +0800

    add FirstBenchmark

--- benchmark/src/main/java/org/excavator/boot/benchmark/FirstBenchmark.java
+++ benchmark/src/main/java/org/excavator/boot/benchmark/FirstBenchmark.java
@@ -1,4 +1,36 @@
 package org.excavator.boot.benchmark;
 
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@State(Scope.Thread)
 public class FirstBenchmark {
+
+    @Benchmark
+    public int sleepAWhile(){
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opts = new OptionsBuilder()
+                .include(FirstBenchmark.class.getSimpleName())
+                .forks(1)
+                .warmupIterations(5)
+                .measurementIterations(5)
+                .build();
+        new Runner(opts).run();
+    }
 }
