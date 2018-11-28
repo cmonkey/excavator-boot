68bc8e7682564547533427f6be57de7007b09e53
CMonkey <42.codemonkey@gmail.com>
2018-11-28 16:18:17 +0800

    add logAspect.enabled to LogAspectAutoConfigure

--- logAspect-spring-boot-starter/src/main/scala/org/excavator/boot/logAspect/autoconfigure/LogAspectAutoConfigure.scala
+++ logAspect-spring-boot-starter/src/main/scala/org/excavator/boot/logAspect/autoconfigure/LogAspectAutoConfigure.scala
@@ -1,9 +1,11 @@
 package org.excavator.boot.logAspect.autoconfigure
 
 import org.excavator.boot.logAspect.aspect.WebLogAspect
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.{Configuration, Import}
 
 @Configuration
+@ConditionalOnProperty(name = Array("logAspect.enabled"), havingValue = "true")
 @Import(Array(classOf[WebLogAspect]))
 class LogAspectAutoConfigure {
 
