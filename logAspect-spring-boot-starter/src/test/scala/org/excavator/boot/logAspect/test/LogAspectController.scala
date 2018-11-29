2d013d5c71cca20db0136d4b32dc51c81961921e
CMonkey <42.codemonkey@gmail.com>
2018-11-29 17:44:28 +0800

    clear unused import package in  LogAspectController

--- logAspect-spring-boot-starter/src/test/scala/org/excavator/boot/logAspect/test/LogAspectController.scala
+++ logAspect-spring-boot-starter/src/test/scala/org/excavator/boot/logAspect/test/LogAspectController.scala
@@ -1,6 +1,5 @@
 package org.excavator.boot.logAspect.test
 
-import org.excavator.boot.common.helper.RemoteIpHelper
 import org.excavator.boot.common.utils.IPThreadLocal
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.{RequestMapping, RestController}
