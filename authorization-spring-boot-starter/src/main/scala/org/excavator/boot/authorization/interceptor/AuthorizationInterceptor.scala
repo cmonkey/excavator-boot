0e5800518c86e83a7b68e2f4c758dc4c03f35f05
CMonkey <42.codemonkey@gmail.com>
2018-11-29 11:14:16 +0800

    add AuthorizationInterceptor

--- authorization-spring-boot-starter/src/main/scala/org/excavator/boot/authorization/interceptor/AuthorizationInterceptor.scala
+++ authorization-spring-boot-starter/src/main/scala/org/excavator/boot/authorization/interceptor/AuthorizationInterceptor.scala
@@ -1,5 +1,46 @@
 package org.excavator.boot.authorization.autoconfigure.mananger.impl.interceptor
 
-class AuthorizationInterceptor {
 
+import com.alibaba.fastjson.JSON
+import javax.annotation.Resource
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
+import org.excavator.boot.authorization.annotation.Authorization
+import org.excavator.boot.authorization.constant.TokenConstants
+import org.excavator.boot.authorization.manager.TokenManager
+import org.springframework.http.{HttpStatus, ResponseEntity}
+import org.springframework.stereotype.Component
+import org.springframework.web.method.HandlerMethod
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter
+
+@Component
+class AuthorizationInterceptor extends HandlerInterceptorAdapter {
+  @Resource
+  private[interceptor] val tokenManager:TokenManager = null
+
+  @throws[Exception]
+  override def preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean = {
+    if (!handler.isInstanceOf[HandlerMethod]) return true
+
+    val handlerMethod = handler.asInstanceOf[HandlerMethod]
+
+    val method = handlerMethod.getMethod
+
+    if (method.getAnnotation(classOf[Authorization]) != null) {
+      val authorization = request.getHeader(TokenConstants.AUTHORIZATION)
+      val token = tokenManager.getToken(authorization)
+      if (tokenManager.checkToken(token)) {
+        request.setAttribute(TokenConstants.CURRENT_USER_ID, token.getCustomerId)
+        return true
+      }
+      else {
+        val responseEntity = new ResponseEntity[String]("authorization failed", HttpStatus.UNAUTHORIZED)
+        response.setContentType("application/json; charset=UTF-8")
+        val printout = response.getWriter
+        printout.print(JSON.toJSONString(responseEntity))
+        printout.flush()
+        return false
+      }
+    }
+    true
+  }
 }
