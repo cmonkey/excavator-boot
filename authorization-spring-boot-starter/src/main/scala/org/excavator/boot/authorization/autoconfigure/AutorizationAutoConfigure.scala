5df8d7aed68bcc9a1bbacd03be081d0acd37dfa2
CMonkey <42.codemonkey@gmail.com>
2018-11-29 17:16:26 +0800

    add load AuthorizationProperties on AutorizationAutoConfigure

--- authorization-spring-boot-starter/src/main/scala/org/excavator/boot/authorization/autoconfigure/AutorizationAutoConfigure.scala
+++ authorization-spring-boot-starter/src/main/scala/org/excavator/boot/authorization/autoconfigure/AutorizationAutoConfigure.scala
@@ -2,7 +2,7 @@ package org.excavator.boot.authorization.autoconfigure
 
 import javax.annotation.Resource
 import org.excavator.boot.authorization.autoconfigure.mananger.impl.interceptor.AuthorizationInterceptor
-import org.excavator.boot.authorization.config.AuthorizationConfig
+import org.excavator.boot.authorization.config.{AuthorizationConfig, AuthorizationProperties}
 import org.excavator.boot.authorization.factory.HandlerMethodArgumentResolverFactory
 import org.excavator.boot.authorization.manager.TokenManager
 import org.excavator.boot.authorization.mananger.impl.CacheTokenManager
@@ -11,12 +11,14 @@ import org.slf4j.{Logger, LoggerFactory}
 import org.springframework.beans.factory.config.ServiceLocatorFactoryBean
 import org.springframework.boot.autoconfigure.AutoConfigureAfter
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.{Bean, Configuration, Import}
 import org.springframework.data.redis.core.StringRedisTemplate
 
 
 @Configuration
 @AutoConfigureAfter(Array(classOf[RedisAutoConfiguration]))
+@EnableConfigurationProperties(Array(classOf[AuthorizationProperties]))
 @Import(Array(classOf[AuthorizationInterceptor], classOf[AuthorizationConfig], classOf[HandlerMethodArgumentResolverFactory]))
 class AutorizationAutoConfigure {
   val logger = LoggerFactory.getLogger(classOf[AutorizationAutoConfigure])
