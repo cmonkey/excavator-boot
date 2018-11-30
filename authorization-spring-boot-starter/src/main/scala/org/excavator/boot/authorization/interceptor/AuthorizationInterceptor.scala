1fb1a31cb4d824f300b824f20f49acca0831bb5e
CMonkey <42.codemonkey@gmail.com>
2018-11-30 12:19:57 +0800

    fix authorization interceptor default return value

--- authorization-spring-boot-starter/src/main/scala/org/excavator/boot/authorization/interceptor/AuthorizationInterceptor.scala
+++ authorization-spring-boot-starter/src/main/scala/org/excavator/boot/authorization/interceptor/AuthorizationInterceptor.scala
@@ -19,41 +19,47 @@ class AuthorizationInterceptor extends HandlerInterceptorAdapter {
 
   @throws[Exception]
   override def preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean = {
-    if (!handler.isInstanceOf[HandlerMethod]) return true
+    var r = true
+    if (!handler.isInstanceOf[HandlerMethod]) {
+      r = true
+    }else{
+      val handlerMethod = handler.asInstanceOf[HandlerMethod]
 
-    val handlerMethod = handler.asInstanceOf[HandlerMethod]
+      val method = handlerMethod.getMethod
 
-    val method = handlerMethod.getMethod
+      if (method.getAnnotation(classOf[Authorization]) != null) {
 
-    if (method.getAnnotation(classOf[Authorization]) != null) {
+        val authorization = request.getHeader(TokenConstants.AUTHORIZATION)
 
-      val authorization = request.getHeader(TokenConstants.AUTHORIZATION)
+        val tokenOpt = tokenManager.getToken(authorization)
 
-      val tokenOpt = tokenManager.getToken(authorization)
-
-      if(tokenOpt.isPresent){
-        val token = tokenOpt.get()
-        if(tokenManager.checkToken(token)){
-          request.setAttribute(TokenConstants.CURRENT_USER_ID, token.getCustomerId)
-          request.setAttribute(TokenConstants.AUTHORIZATION, authorization)
-          true
+        if(tokenOpt.isPresent){
+          val token = tokenOpt.get()
+          if(tokenManager.checkToken(token)){
+            request.setAttribute(TokenConstants.CURRENT_USER_ID, token.getCustomerId)
+            request.setAttribute(TokenConstants.AUTHORIZATION, authorization)
+            r = true
+          }else{
+            val responseEntity = new ResponseEntity[String]("authorization failed", HttpStatus.UNAUTHORIZED)
+            response.setContentType("application/json; charset=UTF-8")
+            val printout = response.getWriter
+            printout.print(JSONProxy.toJSONString(responseEntity))
+            printout.flush()
+            r = false
+          }
         }else{
           val responseEntity = new ResponseEntity[String]("authorization failed", HttpStatus.UNAUTHORIZED)
           response.setContentType("application/json; charset=UTF-8")
           val printout = response.getWriter
           printout.print(JSONProxy.toJSONString(responseEntity))
           printout.flush()
-          false
+          r = false
         }
       }else{
-        val responseEntity = new ResponseEntity[String]("authorization failed", HttpStatus.UNAUTHORIZED)
-        response.setContentType("application/json; charset=UTF-8")
-        val printout = response.getWriter
-        printout.print(JSONProxy.toJSONString(responseEntity))
-        printout.flush()
-        false
+        r = true
       }
     }
-    true
+
+    r
   }
 }
