73db3a5cec2b26d275be750478cd91d37a7c177f
CMonkey <42.codemonkey@gmail.com>
2018-12-03 16:46:54 +0800

    add LockConfig

---
+++ lock-spring-boot-starter/src/main/java/org/excavator/boot/lock/config/LockConfig.java
@@ -0,0 +1,116 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.excavator.boot.lock.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = LockConfig.PREFIX)
+public class LockConfig {
+
+    public static final String PREFIX    = "excavator.lock";
+
+    private Redis              redis;
+
+    //lock
+    private long               waitTime  = 60;
+    private long               leaseTime = 60;
+
+    public long getWaitTime() {
+        return waitTime;
+    }
+
+    public void setWaitTime(long waitTime) {
+        this.waitTime = waitTime;
+    }
+
+    public long getLeaseTime() {
+        return leaseTime;
+    }
+
+    public void setLeaseTime(long leaseTime) {
+        this.leaseTime = leaseTime;
+    }
+
+    public Redis getRedis() {
+        return redis;
+    }
+
+    public void setRedis(Redis redis) {
+        this.redis = redis;
+    }
+
+    public static class Redis {
+        private String  codec = "org.redisson.codec.JsonJacksonCodec";
+        private String  password;
+        private String  address;
+        private Cluster cluster;
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getCodec() {
+            return codec;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public Cluster getCluster() {
+            return cluster;
+        }
+
+        public void setCluster(Cluster cluster) {
+            this.cluster = cluster;
+        }
+
+        public void setCodec(String codec) {
+            this.codec = codec;
+        }
+
+        public static class Cluster {
+            private boolean  enabled;
+            private String[] address;
+
+            public boolean isEnabled() {
+                return enabled;
+            }
+
+            public void setEnabled(boolean enabled) {
+                this.enabled = enabled;
+            }
+
+            public String[] getAddress() {
+                return address;
+            }
+
+            public void setAddress(String[] address) {
+                this.address = address;
+            }
+        }
+    }
+
+}
