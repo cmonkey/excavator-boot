4b0181a99dca733b502f5872bcb6b7e5a4dc9e49
CMonkey <42.codemonkey@gmail.com>
2018-12-03 16:47:47 +0800

    add test

---
+++ lock-spring-boot-starter/src/test/java/org/excavator/boot/lock/test/LockTests.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.excavator.boot.lock.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LockTestApplication.class)
+public class LockTests {
+    private final static Logger logger = LoggerFactory.getLogger(LockTests.class);
+
+    @Autowired
+    TestLockService testLockService;
+
+    /**
+     * 同一进程内多线程获取锁测试
+     * @throws Exception
+     */
+    @Test
+	public void multithreadingTest()throws Exception{
+        int nThreads = Runtime.getRuntime().availableProcessors();
+
+		ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
+
+		List<Callable<Object>> tasks = new ArrayList<>(nThreads);
+
+        for (int i = 0; i < nThreads; i++) {
+            Callable<Object> callable = () -> testLockService.getValue("test");
+            tasks.add(callable);
+        }
+
+        List<Future<Object>> futures = executorService.invokeAll(tasks);
+
+        long count = futures.stream().map(future -> {
+            Object obj = null;
+            try {
+                obj = future.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+
+            return obj;
+        }).filter(Objects::nonNull).count();
+
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.SECONDS);
+
+        assertEquals(count, nThreads);
+
+	}
+}
