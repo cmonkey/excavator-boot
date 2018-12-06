5e4b36e8f1c2f05cc41c4fce1a3581cbd3e0afa8
CMonkey <42.codemonkey@gmail.com>
2018-12-06 10:52:00 +0800

    add SpringJobFactory

---
+++ quartz-spring-boot-starter/src/main/scala/org/excavator/boot/quartz/factory/SpringJobFactory.scala
@@ -0,0 +1,26 @@
+package org.excavator.boot.quartz.factory
+
+import javax.annotation.Resource
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory
+import org.springframework.scheduling.quartz.AdaptableJobFactory
+import org.quartz.spi.TriggerFiredBundle
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+
+@Component
+class SpringJobFactory extends AdaptableJobFactory {
+  val logger = LoggerFactory.getLogger(classOf[SpringJobFactory])
+
+  @Resource
+  val autowireCapableBeanFactory:AutowireCapableBeanFactory = null
+
+  override def createJobInstance(bundle: TriggerFiredBundle): AnyRef = {
+    val instance = super.createJobInstance(bundle)
+
+    autowireCapableBeanFactory.autowireBean(instance)
+
+    logger.info(s"createJobInstance = ${instance}")
+
+    instance
+  }
+}
