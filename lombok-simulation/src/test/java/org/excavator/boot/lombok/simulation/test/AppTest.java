495ed56dd14aa7bd30db9bca176c5f53becc381a
CMonkey <42.codemonkey@gmail.com>
2018-12-17 16:49:42 +0800

    add AppTest

---
+++ lombok-simulation/src/test/java/org/excavator/boot/lombok/simulation/test/AppTest.java
@@ -0,0 +1,10 @@
+package org.excavator.boot.lombok.simulation.test;
+
+public class AppTest {
+
+    public static void main(String[] args) {
+        App app = new App("hello jsr 269");
+
+        //System.out.println(app.getValue());
+    }
+}
