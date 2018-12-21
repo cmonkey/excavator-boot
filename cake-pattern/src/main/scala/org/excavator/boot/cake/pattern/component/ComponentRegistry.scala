888239f94c77967b7d57721d61f35392fff0f44b
CMonkey <42.codemonkey@gmail.com>
2018-12-21 15:20:27 +0800

    add ComponentRegistry

--- cake-pattern/src/main/scala/org/excavator/boot/cake/pattern/component/ComponentRegistry.scala
+++ cake-pattern/src/main/scala/org/excavator/boot/cake/pattern/component/ComponentRegistry.scala
@@ -1,5 +1,18 @@
 package org.excavator.boot.cake.pattern.component
+import org.excavator.boot.cake.pattern.dao.UserRepository
+import org.excavator.boot.cake.pattern.dao.mock.MockUserRepository
+import org.excavator.boot.cake.pattern.service.UserService
 
-class ComponentRegistry {
+object ComponentRegistry extends UserServiceComponent with UserRepositoryComponent {
+  override val userService: UserService = {
 
+    val userService = new UserService
+
+    userService.userRepository = userRepository
+
+    userService
+  }
+  override val userRepository: UserRepository = {
+    new MockUserRepository
+  }
 }
