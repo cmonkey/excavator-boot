2368f50d47dc250841d4cb68bc44ee41676c73c1
CMonkey <42.codemonkey@gmail.com>
2018-12-21 15:20:38 +0800

    add UserServiceComponent

--- cake-pattern/src/main/scala/org/excavator/boot/cake/pattern/component/UserServiceComponent.scala
+++ cake-pattern/src/main/scala/org/excavator/boot/cake/pattern/component/UserServiceComponent.scala
@@ -1,5 +1,11 @@
 package org.excavator.boot.cake.pattern.component
 
-class UserServiceComponent {
+import org.excavator.boot.cake.pattern.service.UserService
+
+trait UserServiceComponent {
+
+  this: UserRepositoryComponent =>
+
+  val userService: UserService
 
 }
