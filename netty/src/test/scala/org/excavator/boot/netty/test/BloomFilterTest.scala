757dc2f5f5ff384f5d6b04c19cdb9f124208cc03
CMonkey <42.codemonkey@gmail.com>
2019-01-17 14:33:49 +0800

    add BloomFilterTest.hashMapTest function

---
+++ netty/src/test/scala/org/excavator/boot/netty/test/BloomFilterTest.scala
@@ -0,0 +1,30 @@
+package org.excavator.boot.netty.test
+
+import java.util
+
+import org.junit.jupiter.api.{DisplayName, Test}
+import org.slf4j.LoggerFactory
+
+class BloomFilterTest {
+
+  val logger = LoggerFactory.getLogger(classOf[BloomFilterTest])
+
+  @Test
+  @DisplayName("testMapTest")
+  def hashMapTest() = {
+    val startTime = System.currentTimeMillis()
+
+    val num = 10000000
+    val set = new util.HashSet[Int](num)
+
+    for(i <-0 until num){
+      set.add(i)
+    }
+
+    assert(set.contains(1))
+    assert(set.contains(2))
+    assert(set.contains(3))
+
+    logger.info(s"execute time = ${System.currentTimeMillis() - startTime}")
+  }
+}
