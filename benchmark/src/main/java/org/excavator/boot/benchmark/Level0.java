6eafe6caa85bae036317aaf8fde2dc2932f035e2
CMonkey <42.codemonkey@gmail.com>
2019-02-26 17:19:58 +0800

    add Level0

---
+++ benchmark/src/main/java/org/excavator/boot/benchmark/Level0.java
@@ -0,0 +1,15 @@
+package org.excavator.boot.benchmark;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+@FunctionalInterface
+public interface Level0 {
+    Level1 next();
+}
+
