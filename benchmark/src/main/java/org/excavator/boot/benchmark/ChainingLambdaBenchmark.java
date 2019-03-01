2d4706b7b19e0ea261d7ba9a6793a4464fe64d0c
CMonkey <42.codemonkey@gmail.com>
2019-03-01 11:23:20 +0800

    add License to ChainingLambdaBenchmark

--- benchmark/src/main/java/org/excavator/boot/benchmark/ChainingLambdaBenchmark.java
+++ benchmark/src/main/java/org/excavator/boot/benchmark/ChainingLambdaBenchmark.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.excavator.boot.benchmark;
 
 import org.openjdk.jmh.annotations.*;
@@ -16,15 +32,13 @@ import java.util.concurrent.TimeUnit;
 @State(Scope.Benchmark)
 public class ChainingLambdaBenchmark {
 
-    @Param({"77"})
+    @Param({ "77" })
     public static Integer value;
 
     public static void main(String[] args) throws RunnerException {
 
-        Options opt =
-                new OptionsBuilder()
-                        .include(ChainingLambdaBenchmark.class.getSimpleName())
-                        .build();
+        Options opt = new OptionsBuilder().include(ChainingLambdaBenchmark.class.getSimpleName())
+            .build();
         new Runner(opt).run();
     }
 
