730156fd44bc6f26e529b0aaabd12e5007ea21eb
CMonkey <42.codemonkey@gmail.com>
2019-03-12 16:05:39 +0800

    change InMemoryConfig in scala print

--- cake-pattern/src/main/scala/org/excavator/boot/cake/pattern/config/InMemoryConfig.scala
+++ cake-pattern/src/main/scala/org/excavator/boot/cake/pattern/config/InMemoryConfig.scala
@@ -4,6 +4,6 @@ trait InMemoryConfig extends Config {
 
   override lazy val text: String = "inMemory config"
 
-  override def load: Unit = println(s"load in memory config ${text}")
+  override def load: Unit = println(s"load in memory config $text")
 
 }
