9140c4c333b64496a64a2c0dc8b56d0f5af49f5f
CMonkey <42.codemonkey@gmail.com>
2019-03-25 15:08:51 +0800

    add LockConfig.Redis.Single

--- lock-spring-boot-starter/src/main/java/org/excavator/boot/lock/config/LockConfig.java
+++ lock-spring-boot-starter/src/main/java/org/excavator/boot/lock/config/LockConfig.java
@@ -84,6 +84,27 @@ public class LockConfig {
                 this.address = address;
             }
         }
+
+        public static class Single {
+            private String address;
+            private String password;
+
+            public String getAddress() {
+                return address;
+            }
+
+            public void setAddress(String address) {
+                this.address = address;
+            }
+
+            public String getPassword() {
+                return password;
+            }
+
+            public void setPassword(String password) {
+                this.password = password;
+            }
+        }
     }
 
 }
