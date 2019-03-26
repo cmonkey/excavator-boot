9c8eaccf237bf59b7080bae4ca7c3bc64b3e820d
CMonkey <42.codemonkey@gmail.com>
2019-03-26 16:44:29 +0800

    change KafkaTests in jupiter test

--- kafka-spring-boot-starter/src/test/scala/org/excavator/boot/kafka/test/KafkaTests.scala
+++ kafka-spring-boot-starter/src/test/scala/org/excavator/boot/kafka/test/KafkaTests.scala
@@ -1,13 +1,13 @@
 package org.excavator.boot.kafka.test
 
 import javax.annotation.Resource
-import org.junit.Assert._
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@RunWith(classOf[SpringRunner])
+@ExtendWith(Array(classOf[SpringExtension]))
 @SpringBootTest(classes = Array(classOf[KafkaApplication]))
 class KafkaTests {
 
