9122f79de5963a0ac0b0100e27b2ec1f109a1a09
CMonkey <42.codemonkey@gmail.com>
2019-03-26 16:36:47 +0800

    change CumulativeTests in jupiter  test

--- cumulative-spring-boot-starter/src/test/java/org/excavator/boot/cumulative/test/CumulativeTests.java
+++ cumulative-spring-boot-starter/src/test/java/org/excavator/boot/cumulative/test/CumulativeTests.java
@@ -18,13 +18,16 @@ package org.excavator.boot.cumulative.test;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = CumulativeApplication.class)
 public class CumulativeTests {
 
