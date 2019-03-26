ab614ed9554c898d95f3bf73026b362d8deff218
CMonkey <42.codemonkey@gmail.com>
2019-03-26 16:38:01 +0800

    replace junit4 junit5 to CumulativeTests

--- cumulative-spring-boot-starter/src/test/java/org/excavator/boot/cumulative/test/CumulativeTests.java
+++ cumulative-spring-boot-starter/src/test/java/org/excavator/boot/cumulative/test/CumulativeTests.java
@@ -16,14 +16,12 @@
  */
 package org.excavator.boot.cumulative.test;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
