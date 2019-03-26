4396147e739db05d2e9da28a4ace0671565c3e1c
CMonkey <42.codemonkey@gmail.com>
2019-03-26 16:59:00 +0800

    change RedisLimitTests in jupiter test

--- redisLimit-spring-boot-starter/src/test/scala/org/excavator/boot/redislimit/test/RedisLimitTests.scala
+++ redisLimit-spring-boot-starter/src/test/scala/org/excavator/boot/redislimit/test/RedisLimitTests.scala
@@ -1,12 +1,13 @@
 package org.excavator.boot.redislimit.test
 
 import javax.annotation.Resource
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
-import org.junit.Assert._
-@RunWith(classOf[SpringRunner])
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(Array(classOf[SpringExtension]))
 @SpringBootTest
 class RedisLimitTests {
   @Resource
