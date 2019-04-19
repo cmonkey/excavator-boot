43f39fbd28d144e177beefc947a2bb92e180c0f8
CMonkey <42.codemonkey@gmail.com>
2019-04-19 17:47:57 +0800

    add LogBackInitializerExtension with BeforeAllCallBack

--- logging/src/test/scala/org/excavator/boot/logging/test/LogBackInitializerExtension.java
+++ logging/src/test/scala/org/excavator/boot/logging/test/LogBackInitializerExtension.java
@@ -1,6 +1,31 @@
 package org.excavator.boot.logging.test;
 
+import ch.qos.logback.classic.LoggerContext;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.Extension;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.slf4j.LoggerFactory;
 
-public class LogBackInitializerExtension implements Extension {
+public class LogBackInitializerExtension implements BeforeAllCallback {
+    @Override
+    public void beforeAll(ExtensionContext context) throws Exception {
+
+        pauseTillLogBackReady();
+    }
+
+    private void pauseTillLogBackReady() {
+        do {
+
+        }while (!isLogBackReady())
+    }
+
+    private boolean isLogBackReady() {
+        try{
+            LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+        }catch(ClassCastException e){
+            return false;
+        }
+
+        return true;
+    }
 }
