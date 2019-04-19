96b545814f882fa0443f068535562353ae447e93
CMonkey <42.codemonkey@gmail.com>
2019-04-19 15:48:13 +0800

    add LogProducingService.writeSomeLoggingStatements

--- logging/src/main/scala/org/excavator/boot/logging/LogProducingService.scala
+++ logging/src/main/scala/org/excavator/boot/logging/LogProducingService.scala
@@ -1,5 +1,23 @@
 package org.excavator.boot.logging
 
+import java.util.concurrent.Executors
+
+import org.slf4j.LoggerFactory
+
 class LogProducingService {
+
+  val logger = LoggerFactory.getLogger(classOf[LogProducingService])
+
+  def writeSomeLoggingStatemenets(message:String): Unit = {
+    logger.info(s"Let's assert some logs! ${message}")
+
+    val executor = Executors.newSingleThreadExecutor()
+
+    val future = executor.submit[Unit](() => logger.info("this message is in a separate thread"))
+
+    do{
+
+    }while(!future.isDone())
+  }
 
 }
