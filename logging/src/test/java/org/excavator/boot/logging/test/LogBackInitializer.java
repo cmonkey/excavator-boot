75926833850e6287dab798a2697880090903505d
CMonkey <42.codemonkey@gmail.com>
2019-04-19 17:47:25 +0800

    init LogBackInitializer LogBackInitializerExtension

---
+++ logging/src/test/java/org/excavator/boot/logging/test/LogBackInitializer.java
@@ -0,0 +1,4 @@
+package org.excavator.boot.logging;
+
+public class LogBackInitializer {
+}
---
+++ logging/src/test/scala/org/excavator/boot/logging/test/LogBackInitializerExtension.java
@@ -0,0 +1,6 @@
+package org.excavator.boot.logging.test;
+
+import org.junit.jupiter.api.extension.Extension;
+
+public class LogBackInitializerExtension implements Extension {
+}
