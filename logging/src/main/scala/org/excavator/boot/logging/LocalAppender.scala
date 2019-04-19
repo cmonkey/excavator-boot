36a1498f6538e8274eaa7a6566f254484d6a4dfe
CMonkey <42.codemonkey@gmail.com>
2019-04-19 17:31:39 +0800

    add LocalAppender

--- logging/src/main/scala/org/excavator/boot/logging/LocalAppender.scala
+++ logging/src/main/scala/org/excavator/boot/logging/LocalAppender.scala
@@ -2,18 +2,28 @@ package org.excavator.boot.logging
 
 import java.util
 
+import ch.qos.logback.classic.Logger
 import ch.qos.logback.classic.LoggerContext
 import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.AppenderBase
-import org.slf4j.{Logger, LoggerFactory}
+import org.slf4j.LoggerFactory
 
 class LocalAppender extends AppenderBase[ILoggingEvent]{
   start()
 
 
   override def append(eventObject: ILoggingEvent): Unit = {
-
+    LocalAppender.events.add(eventObject)
   }
+
+  def getEvents() = {
+    LocalAppender.events
+  }
+
+  def clearEvents() = {
+    LocalAppender.events.clear()
+  }
+
 }
 
 object LocalAppender{
@@ -26,7 +36,32 @@ object LocalAppender{
     for(loggerName <- loggers) {
       localAppender.setContext(LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext])
       val logger = LoggerFactory.getLogger(loggerName).asInstanceOf[Logger]
-      logger.
+      logger.addAppender(localAppender)
     }
+
+    localAppender
+  }
+
+  def isLogBackReady() = {
+
+    try{
+      val context = LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext]
+      true
+    }catch{
+      case e: ClassCastException => false
+      case _ => false
+    }
+
+  }
+
+  def pauseTillLogbackReady() = {
+    do{
+
+    }while(!isLogBackReady())
+  }
+
+  def cleanup(localAppender: LocalAppender) = {
+    localAppender.stop()
+    localAppender.clearEvents()
   }
 }
