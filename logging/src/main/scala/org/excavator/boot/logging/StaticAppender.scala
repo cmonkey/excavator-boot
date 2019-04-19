7887a324ee5bd7c883cd52c9d4d89c3866ef6763
CMonkey <42.codemonkey@gmail.com>
2019-04-19 16:29:26 +0800

    change StaticAppender

--- logging/src/main/scala/org/excavator/boot/logging/StaticAppender.scala
+++ logging/src/main/scala/org/excavator/boot/logging/StaticAppender.scala
@@ -6,11 +6,15 @@ import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.AppenderBase
 
 class StaticAppender extends AppenderBase[ILoggingEvent]{
-  val events = new util.ArrayList[ILoggingEvent]()
 
   override def append(eventObject: ILoggingEvent): Unit = {
-    events.add(eventObject)
+    StaticAppender.events.add(eventObject)
   }
+
+}
+
+object StaticAppender{
+  val events = new util.ArrayList[ILoggingEvent]()
 
   def getEvents(): util.List[ILoggingEvent] = {
     events
