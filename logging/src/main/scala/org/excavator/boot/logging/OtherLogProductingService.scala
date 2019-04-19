871c015a03f3507f8be7c781a401ae7a4fd8f221
CMonkey <42.codemonkey@gmail.com>
2019-04-19 16:59:35 +0800

    add OtherLogProductingService.writeSomeLoggingStatements

--- logging/src/main/scala/org/excavator/boot/logging/OtherLogProductingService.scala
+++ logging/src/main/scala/org/excavator/boot/logging/OtherLogProductingService.scala
@@ -1,5 +1,23 @@
 package org.excavator.boot.logging
 
+import java.util.concurrent.{Executor, Executors}
+
+import org.slf4j.LoggerFactory
+
 class OtherLogProductingService {
+
+  val logger = LoggerFactory.getLogger(classOf[OtherLogProductingService])
+
+  def writeSomeLoggingStatements(message: String) : Unit = {
+    logger.info(s"Let's a assert some logs! ${message}")
+
+    val executor = Executors.newSingleThreadExecutor()
+
+    val future = executor.submit[Unit](() => logger.info("This message is in a separate thread"))
+
+    do{
+
+    }while(!future.isDone)
+  }
 
 }
