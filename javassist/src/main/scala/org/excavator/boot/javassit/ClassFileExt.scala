399bf03bdc32eadb85bcbf2d974b3bbe0c48c29a
CMonkey <42.codemonkey@gmail.com>
2019-04-24 16:17:48 +0800

    add ClassFileExt.createClass in return type ClassFile

--- javassist/src/main/scala/org/excavator/boot/javassit/ClassFileExt.scala
+++ javassist/src/main/scala/org/excavator/boot/javassit/ClassFileExt.scala
@@ -2,15 +2,17 @@ package org.excavator.boot.javassit
 
 import javassist.bytecode.{AccessFlag, ClassFile, FieldInfo}
 
-class ClassFileExt {
+object ClassFileExt {
 
-  def createClass(className:String, fileName: String): Unit = {
+  def createClass(className:String, fileName: String): ClassFile = {
     val classfile = new ClassFile(false, className, null)
     classfile.setInterfaces(Array("java.lang.Cloneable"))
 
     val fieldInfo = new FieldInfo(classfile.getConstPool, fileName, "I")
     fieldInfo.setAccessFlags(AccessFlag.PUBLIC)
     classfile.addField(fieldInfo)
+
+    classfile
   }
 
 }
