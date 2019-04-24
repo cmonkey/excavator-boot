33e7c62927332804430b4670ccbed91d39e78c88
CMonkey <42.codemonkey@gmail.com>
2019-04-24 15:31:16 +0800

    add Point

---
+++ javassist/src/main/java/org/excavator/boot/javassit/Point.java
@@ -0,0 +1,11 @@
+package org.excavator.boot.javassit;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public void move(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
