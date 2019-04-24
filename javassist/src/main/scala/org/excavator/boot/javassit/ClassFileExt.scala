8f42d3e170a9a5155010cf2a01f8ab02e42391d0
CMonkey <42.codemonkey@gmail.com>
2019-04-24 15:53:11 +0800

    add fileName param to ClassFileExt.createClass

--- javassist/src/main/scala/org/excavator/boot/javassit/ClassFileExt.scala
+++ javassist/src/main/scala/org/excavator/boot/javassit/ClassFileExt.scala
@@ -4,11 +4,11 @@ import javassist.bytecode.{AccessFlag, ClassFile, FieldInfo}
 
 class ClassFileExt {
 
-  def createClass(className:String): Unit = {
-    val classfile = new ClassFile(false, "org.excavator.boot.javassit."+className, null)
+  def createClass(className:String, fileName: String): Unit = {
+    val classfile = new ClassFile(false, className, null)
     classfile.setInterfaces(Array("java.lang.Cloneable"))
 
-    val fieldInfo = new FieldInfo(classfile.getConstPool, "id", "I")
+    val fieldInfo = new FieldInfo(classfile.getConstPool, fileName, "I")
     fieldInfo.setAccessFlags(AccessFlag.PUBLIC)
     classfile.addField(fieldInfo)
   }
