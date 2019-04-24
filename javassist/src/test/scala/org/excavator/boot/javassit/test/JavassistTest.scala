3bd12d39cedc66bd564aabda104e4d5c47cd4924
CMonkey <42.codemonkey@gmail.com>
2019-04-24 16:26:27 +0800

    add JavassistTest.testLoadByteCode function

--- javassist/src/test/scala/org/excavator/boot/javassit/test/JavassistTest.scala
+++ javassist/src/test/scala/org/excavator/boot/javassit/test/JavassistTest.scala
@@ -1,9 +1,14 @@
 package org.excavator.boot.javassit.test
 
-import javassist.{ClassPool}
+import java.util
+
+import javassist.ClassPool
+import javassist.bytecode.Mnemonic
 import org.excavator.boot.javassit.{ClassFileExt, ClassFileHelper}
 import org.junit.jupiter.api.{DisplayName, Test}
 import org.junit.jupiter.api.Assertions._
+
+import scala.collection.mutable
 
 class JavassistTest {
 
@@ -20,6 +25,29 @@ class JavassistTest {
     val fields = ClassFileHelper.getFields(classPool, classFile)
 
     assertEquals(fields(0).getName , fileName)
+  }
+
+  @Test
+  @DisplayName("testLoadByteCode")
+  def testLoadByteCode() = {
+    val classPool = ClassPool.getDefault
+    val classFile = classPool.get("org.excavator.boot.javassit.Point").getClassFile
+
+    val methodInfo = classFile.getMethod("move")
+    val codeAttribute = methodInfo.getCodeAttribute
+    val codeIterator = codeAttribute.iterator()
+
+    val operations = new java.util.LinkedList[String]()
+
+    while(codeIterator.hasNext){
+      val index = codeIterator.next()
+      val op = codeIterator.byteAt(index)
+      operations.add(Mnemonic.OPCODE(op))
+    }
+
+    operations.forEach(operation => println(operation))
+
+    assertEquals(operations, util.Arrays.asList("aload_0", "iload_1", "putfield", "aload_0", "iload_2", "putfield", "return"))
   }
 
 }
