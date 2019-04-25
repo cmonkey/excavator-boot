64b28045a145206cd2598c21af3a2537c02bcc80
CMonkey <42.codemonkey@gmail.com>
2019-04-25 10:06:31 +0800

    add InstrumenetationHelper.premain agentmain

--- instrumentation/src/main/scala/org/excavator/boot/instrumentation/InstrumenetationHelper.scala
+++ instrumentation/src/main/scala/org/excavator/boot/instrumentation/InstrumenetationHelper.scala
@@ -1,5 +1,31 @@
 package org.excavator.boot.instrumentation
 
-class InstrumenetationHelper {
+import java.lang.instrument.Instrumentation
+
+import org.slf4j.LoggerFactory
+
+class InstrumenetationHelper{
 
 }
+
+object InstrumenetationHelper {
+  val logger = LoggerFactory.getLogger(classOf[InstrumenetationHelper])
+
+  def transformClass(className: String, instrumentation: Instrumentation) = {
+
+  }
+
+  def premain(agnetArgs: String, instrumentation: Instrumentation) = {
+    logger.info("[Agent] in premain method")
+    val className = ""
+    transformClass(className, instrumentation)
+  }
+
+  def agentmain(agentArgs: String, instrumentation: Instrumentation) = {
+
+    logger.info("[Agent] in agentmain method")
+    val className = ""
+    transformClass(className, instrumentation)
+  }
+}
+
