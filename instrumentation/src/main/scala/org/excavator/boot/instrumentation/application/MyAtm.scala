3280ff1101e7753a7d0c32324ddea6704819863b
CMonkey <42.codemonkey@gmail.com>
2019-04-25 11:48:30 +0800

    add MyAtm.withdrawMoney

--- instrumentation/src/main/scala/org/excavator/boot/instrumentation/application/MyAtm.scala
+++ instrumentation/src/main/scala/org/excavator/boot/instrumentation/application/MyAtm.scala
@@ -1,5 +1,15 @@
 package org.excavator.boot.instrumentation.application
 
-class MyAtm {
+import org.slf4j.LoggerFactory
 
+class MyAtm {
+}
+
+object MyAtm{
+  val account = 10
+  val logger = LoggerFactory.getLogger(classOf[MyAtm])
+  def withdrawMoney(amount: Int) = {
+    Thread.sleep(20001)
+    logger.info(s"[Application] successful withdrawal of [{}] units ${amount}")
+  }
 }
