5983bf8e8e2b26696581684ec02b22cd27df3e18
CMonkey <42.codemonkey@gmail.com>
2019-04-25 10:30:13 +0800

    add AtmTransformer.transform

--- instrumentation/src/main/scala/org/excavator/boot/instrumentation/AtmTransformer.scala
+++ instrumentation/src/main/scala/org/excavator/boot/instrumentation/AtmTransformer.scala
@@ -1,5 +1,54 @@
 package org.excavator.boot.instrumentation
 
-class AtmTransformer {
+import java.io.IOException
+import java.lang.instrument.ClassFileTransformer
+import java.security.ProtectionDomain
 
+import javassist.{CannotCompileException, ClassPool, CtClass, NotFoundException}
+import org.slf4j.LoggerFactory
+
+class AtmTransformer(val targetClassName: String, val targetClassLoader: ClassLoader) extends ClassFileTransformer{
+  val logger = LoggerFactory.getLogger(classOf[AtmTransformer])
+  val WITHDRAW_MONEY_METHOD = "withdrawMoney"
+
+  override def transform(loader: ClassLoader, className: String, classBeingRedefined: Class[_], protectionDomain: ProtectionDomain, classfileBuffer: Array[Byte]): Array[Byte] = {
+
+    var byteCode = classfileBuffer
+    val finalTragetClassName = targetClassName.replaceAll("\\.", ",")
+
+    if(!className.equals(finalTragetClassName)){
+      byteCode
+    }else{
+      if(className.equals(finalTragetClassName) && loader.equals(targetClassLoader)){
+        logger.info("[Agent] Transforming class AtmTransformer")
+
+        try{
+          val classPool = ClassPool.getDefault
+          val ctClass = classPool.get(targetClassName)
+          val ctMethod = ctClass.getDeclaredMethod(WITHDRAW_MONEY_METHOD)
+          ctMethod.insertBefore("startTime = System.currentTimeMillis();")
+
+          val endBlock = new StringBuilder
+
+          ctMethod.addLocalVariable("endTime", CtClass.longType)
+          ctMethod.addLocalVariable("opTime", CtClass.longType)
+
+          endBlock.append("endTime = System.currentTimeMillis();")
+          endBlock.append("opTime = (endTime - startTime / 1000;")
+
+          endBlock.append("logger.info(\"[Application] Withdrawal operation completed in:\" + opTime + \" seconds\");")
+
+          ctMethod.insertAfter(endBlock.toString())
+
+          byteCode = ctClass.toBytecode
+          ctClass.detach()
+        }catch{
+          case ex: NotFoundException => logger.error(s"Exception ${ex}")
+          case ex: CannotCompileException => logger.error(s"Exception ${ex}")
+          case ex: IOException  => logger.error(s"Exception ${ex}")
+        }
+      }
+    }
+
+  }
 }
