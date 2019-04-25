765f73c1cc04930a7623fe309d7fbc0b02d33447
CMonkey <42.codemonkey@gmail.com>
2019-04-25 14:52:32 +0800

    change call AgentLoader.run to AgentTests

--- instrumentation/src/test/scala/org/excavator/boot/instrumentation/test/AgentTests.scala
+++ instrumentation/src/test/scala/org/excavator/boot/instrumentation/test/AgentTests.scala
@@ -1,7 +1,7 @@
 package org.excavator.boot.instrumentation.test
 
 import org.excavator.boot.instrumentation.application.AgentLoader
-import org.junit.jupiter.api.{BeforeAll, BeforeEach, DisplayName, Test}
+import org.junit.jupiter.api.{BeforeAll, DisplayName, Test}
 import org.junit.jupiter.api.Assertions._
 
 class AgentTests {
@@ -9,20 +9,21 @@ class AgentTests {
   @Test
   @DisplayName("testJavassistAgentLoader")
   def testJavassistAgentLoader() = {
-    assertThrows(classOf[RuntimeException], () => AgentTests.agentLoader.runJavassist(Array("")))
+    assertThrows(classOf[RuntimeException], () => AgentTests.agentLoader.run(Array("")))
   }
 
   @Test
   @DisplayName("testByteBuddyAgentLoader")
   def testByteBuddyAgentLoader() = {
 
+    assertThrows(classOf[RuntimeException], () => AgentTests.agentLoader.run(Array("")))
   }
 }
 
 object AgentTests{
   var agentLoader: AgentLoader = null
 
-  @BeforeEach
+  @BeforeAll
   def initLoader() = {
     agentLoader = new AgentLoader
   }
