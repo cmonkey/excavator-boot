20e1c0a07caf04b1fdd26f2800fb830a72868bac
CMonkey <42.codemonkey@gmail.com>
2019-04-25 14:33:56 +0800

    add ByteBuddyAgent.premain agentmain

--- instrumentation/src/main/scala/org/excavator/boot/instrumentation/ByteBuddyAgent.scala
+++ instrumentation/src/main/scala/org/excavator/boot/instrumentation/ByteBuddyAgent.scala
@@ -1,5 +1,50 @@
 package org.excavator.boot.instrumentation
 
+import java.lang.instrument.Instrumentation
+
+import net.bytebuddy.agent.builder.AgentBuilder
+import net.bytebuddy.description.`type`.TypeDescription
+import net.bytebuddy.dynamic.DynamicType
+import net.bytebuddy.implementation.MethodDelegation
+import net.bytebuddy.matcher.ElementMatchers
+import net.bytebuddy.utility.JavaModule
+import org.excavator.boot.instrumentation.interceptor.TimeInterceptor
+
 class ByteBuddyAgent {
+
+}
+
+object ByteBuddyAgent{
+
+  def premain(agentArgs: String, instrumentation: Instrumentation) = {
+
+    val transformer = new AgentBuilder.Transformer {
+      override def transform(builder: DynamicType.Builder[_], typeDescription: TypeDescription, classLoader: ClassLoader, javaModule: JavaModule): DynamicType.Builder[_] = {
+        builder.method(ElementMatchers.any())
+          .intercept(MethodDelegation.to(classOf[TimeInterceptor]))
+      }
+    }
+
+    val listener = new AgentBuilder.Listener {
+      override def onDiscovery(s: String, classLoader: ClassLoader, javaModule: JavaModule, b: Boolean): Unit = ???
+
+      override def onTransformation(typeDescription: TypeDescription, classLoader: ClassLoader, javaModule: JavaModule, b: Boolean, dynamicType: DynamicType): Unit = ???
+
+      override def onIgnored(typeDescription: TypeDescription, classLoader: ClassLoader, javaModule: JavaModule, b: Boolean): Unit = ???
+
+      override def onError(s: String, classLoader: ClassLoader, javaModule: JavaModule, b: Boolean, throwable: Throwable): Unit = ???
+
+      override def onComplete(s: String, classLoader: ClassLoader, javaModule: JavaModule, b: Boolean): Unit = ???
+    }
+
+    new AgentBuilder.Default().`type`(ElementMatchers.nameStartsWith("org.excavator.boot"), ElementMatchers.any())
+      .transform(transformer)
+      .`with`(listener).installOn(instrumentation)
+
+  }
+
+  def agentmain(agentArgs: String, instrumentation: Instrumentation) = {
+
+  }
 
 }
