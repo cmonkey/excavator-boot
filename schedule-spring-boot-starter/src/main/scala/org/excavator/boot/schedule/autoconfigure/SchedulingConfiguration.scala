4c9d325bf9e4cf39bd1e4b37ceb63fb14637500d
CMonkey <42.codemonkey@gmail.com>
2019-05-15 15:26:27 +0800

    delete ScheduleThreadPool to SchedulingConfiguration

--- schedule-spring-boot-starter/src/main/scala/org/excavator/boot/schedule/autoconfigure/SchedulingConfiguration.scala
+++ schedule-spring-boot-starter/src/main/scala/org/excavator/boot/schedule/autoconfigure/SchedulingConfiguration.scala
@@ -15,6 +15,7 @@ import org.springframework.scheduling.config.ScheduledTaskRegistrar
 
 @Configuration
 @EnableScheduling
+@EnableConfigurationProperties(Array(classOf[ScheduleProperties]))
 class SchedulingConfiguration extends SchedulingConfigurer{
 
   val DEFAULT_POOLSIZE = 10
@@ -22,51 +23,50 @@ class SchedulingConfiguration extends SchedulingConfigurer{
   val DEFAULT_WAIT_FOR_TASKS_TO_COMPLETE_ON_SHUTDOWN = true
   val logger = LoggerFactory.getLogger(classOf[SchedulingConfiguration])
 
-  @Configuration
-  @EnableConfigurationProperties(Array(classOf[ScheduleProperties]))
-  class ScheduleThreadPool(scheduleProperties: ScheduleProperties){
+  @Resource
+  val scheduleProperties: ScheduleProperties = null
 
-    def getDefaultIfNull(poolSize: Int, defaultPoolSize: Int): Int = {
-      if(null == poolSize){
-        defaultPoolSize
-      }else{
-        poolSize
-      }
-    }
 
-    @Bean
-    def taskScheduler() = {
-      val scheduler = new ThreadPoolTaskScheduler
-      scheduler.setPoolSize(getDefaultIfNull(scheduleProperties.getThreadPool.getPoolSize, DEFAULT_POOLSIZE))
-      scheduler.setErrorHandler((t: Throwable) => {
-        logger.error(s"Unexpected error occured in scheduled task. ${t}")
-
-        if(t.isInstanceOf[StopSchedulingTaskException]){
-          logger.error("Stop scheduling task becauss of occuring StopSchedulingTaskException in task")
-          throw t.asInstanceOf[StopSchedulingTaskException]
-        }
-      })
-
-      scheduler.setThreadNamePrefix(scheduleProperties.getThreadNamePrefix)
-
-      scheduler.setAwaitTerminationSeconds(getDefaultIfNull(scheduleProperties.getThreadPool.getAwaitTerminaltionSeconds,
-        DEFAULT_AWAIT_TERMINATION_SECONDS))
-      scheduler.setWaitForTasksToCompleteOnShutdown(Optional.ofNullable(scheduleProperties.getThreadPool.getWaitForTasksToCompleteOnShutdown)
-        .orElse(DEFAULT_WAIT_FOR_TASKS_TO_COMPLETE_ON_SHUTDOWN))
-
-      scheduler.setRejectedExecutionHandler(new RejectedExecutionHandler {
-        override def rejectedExecution(r: Runnable, executor: ThreadPoolExecutor): Unit = {
-          logger.error("Please increase pool size excavator.schedule.threadPool.poolSize for thread pool")
-          throw new RejectedExecutionException(s"Task ${r.toString} rejected from ${executor.toString}")
-        }
-      })
-
-      scheduler
+  def getDefaultIfNull(poolSize: Int, defaultPoolSize: Int): Int = {
+    if(poolSize == 0){
+      defaultPoolSize
+    }else{
+      poolSize
     }
   }
 
-  @Resource(name = "taskScheduler")
-  val taskScheduler: ThreadPoolTaskScheduler = null
+  @Bean
+  def taskScheduler() = {
+    val scheduler = new ThreadPoolTaskScheduler
+    scheduler.setPoolSize(getDefaultIfNull(scheduleProperties.getThreadPool.getPoolSize, DEFAULT_POOLSIZE))
+    scheduler.setErrorHandler((t: Throwable) => {
+      logger.error(s"Unexpected error occured in scheduled task. ${t}")
+
+      t match {
+        case t:StopSchedulingTaskException => {
+          logger.error("Stop scheduling task becauss of occuring StopSchedulingTaskException in task")
+          throw t.asInstanceOf[StopSchedulingTaskException]
+        }
+
+      }
+    })
+
+    scheduler.setThreadNamePrefix(scheduleProperties.getThreadNamePrefix)
+
+    scheduler.setAwaitTerminationSeconds(getDefaultIfNull(scheduleProperties.getThreadPool.getAwaitTerminaltionSeconds,
+      DEFAULT_AWAIT_TERMINATION_SECONDS))
+    scheduler.setWaitForTasksToCompleteOnShutdown(Optional.ofNullable(scheduleProperties.getThreadPool.getWaitForTasksToCompleteOnShutdown)
+      .orElse(DEFAULT_WAIT_FOR_TASKS_TO_COMPLETE_ON_SHUTDOWN))
+
+    scheduler.setRejectedExecutionHandler(new RejectedExecutionHandler {
+      override def rejectedExecution(r: Runnable, executor: ThreadPoolExecutor): Unit = {
+        logger.error("Please increase pool size excavator.schedule.threadPool.poolSize for thread pool")
+        throw new RejectedExecutionException(s"Task ${r.toString} rejected from ${executor.toString}")
+      }
+    })
+
+    scheduler
+  }
 
   override def configureTasks(taskRegistrar: ScheduledTaskRegistrar): Unit = {
     taskRegistrar.setScheduler(taskScheduler)
