4d2508678bcf32d129d6944c391d83a753676e92
CMonkey <42.codemonkey@gmail.com>
2019-05-15 15:16:51 +0800

    add TaskService.(task getThreadName )

--- schedule-spring-boot-starter/src/test/scala/org/excavator/boot/schedule/test/TaskService.scala
+++ schedule-spring-boot-starter/src/test/scala/org/excavator/boot/schedule/test/TaskService.scala
@@ -1,5 +1,26 @@
 package org.excavator.boot.schedule.test
 
+import java.util.concurrent.atomic.AtomicReference
+
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
 class TaskService {
+
+  val logger = LoggerFactory.getLogger(classOf[TaskService])
+
+  val atomic = new AtomicReference[String]()
+
+  @Scheduled(fixedRate =  1 * 60)
+  def task() = {
+    atomic.set(Thread.currentThread().getName)
+    logger.info(s"Thread currentThread Name = ${atomic.get()}")
+  }
+
+  def getThreadName() = {
+    atomic.get()
+  }
 
 }
