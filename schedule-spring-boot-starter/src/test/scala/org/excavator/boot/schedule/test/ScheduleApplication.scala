1d9197d0911d0656e3a7cb4b3e8cb4e03974ff69
CMonkey <42.codemonkey@gmail.com>
2019-05-15 15:14:35 +0800

    add @SpringBootApplication to ScheduleApplication

--- schedule-spring-boot-starter/src/test/scala/org/excavator/boot/schedule/test/ScheduleApplication.scala
+++ schedule-spring-boot-starter/src/test/scala/org/excavator/boot/schedule/test/ScheduleApplication.scala
@@ -1,5 +1,15 @@
 package org.excavator.boot.schedule.test
 
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
 class ScheduleApplication {
 
+}
+
+object ScheduleApplication{
+  def main(args: Array[String]): Unit = {
+    SpringApplication.run(classOf[ScheduleApplication], args:_*)
+  }
 }
