bd5a20b2d95b259075e8aead214718989494ddee
CMonkey <42.codemonkey@gmail.com>
2019-06-04 15:28:01 +0800

    add GeneratorPublicPrivateKeyTests.testGenerator

--- common/src/test/java/org/excavator/boot/common/test/GeneratorPublicPrivateKeyTests.java
+++ common/src/test/java/org/excavator/boot/common/test/GeneratorPublicPrivateKeyTests.java
@@ -1,4 +1,22 @@
 package org.excavator.boot.common.test;
 
+import org.excavator.boot.common.utils.GeneratePublicPrivateKey;
+import org.excavator.boot.common.utils.GeneratePublicPrivateKeys;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GeneratorPublicPrivateKeyTests {
+
+    @Test
+    @DisplayName("testGeneratorByRSA")
+    public void testGenerator(){
+
+        Optional<GeneratePublicPrivateKey> optionalGeneratePublicPrivateKey = GeneratePublicPrivateKeys.generateKeys("RSA", 2048);
+
+        assertEquals(true, optionalGeneratePublicPrivateKey.isPresent());
+    }
 }
