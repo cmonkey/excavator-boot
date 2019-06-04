7024b384ab1f4037779630071d906542aa9efb3a
CMonkey <42.codemonkey@gmail.com>
2019-06-04 15:07:02 +0800

    add GeneratePublicPrivateKey.(privateKeyEncodeBase64String publicKeyEncodeBase64String) fields

--- common/src/main/java/org/excavator/boot/common/utils/GeneratePublicPrivateKey.java
+++ common/src/main/java/org/excavator/boot/common/utils/GeneratePublicPrivateKey.java
@@ -1,4 +1,14 @@
 package org.excavator.boot.common.utils;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GeneratePublicPrivateKey {
+    private String privateKeyEncodeBase64String;
+    private String publicKeyEncodeBase64String;
+
 }
