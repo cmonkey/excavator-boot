5b9cfa0f8ad04a158ff74de0c1835aba0899dbec
CMonkey <42.codemonkey@gmail.com>
2019-06-04 15:58:02 +0800

    add PublicPrivateKey.{privateKey publicKey}

--- common/src/main/java/org/excavator/boot/common/utils/PublicPrivateKey.java
+++ common/src/main/java/org/excavator/boot/common/utils/PublicPrivateKey.java
@@ -4,11 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class GeneratePublicPrivateKey {
-    private String privateKeyEncodeBase64String;
-    private String publicKeyEncodeBase64String;
+public class PublicPrivateKey {
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
 
 }
