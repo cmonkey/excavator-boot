6a9ecdb7df604c87c38eb2bebec293b240599435
CMonkey <42.codemonkey@gmail.com>
2019-06-21 10:44:40 +0800

    add CustomAuthroizationResolver.writeResponse

--- authorization-spring-boot-starter/src/test/scala/org/excavator/boot/authorization/test/resolver/CustomAuthroizationResolver.scala
+++ authorization-spring-boot-starter/src/test/scala/org/excavator/boot/authorization/test/resolver/CustomAuthroizationResolver.scala
@@ -1,5 +1,25 @@
 package org.excavator.boot.authorization.test.resolver
 
-class CustomAuthroizationResolver {
+import javax.servlet.http.HttpServletResponse
+import org.excavator.boot.authorization.interceptor.AuthorizationResolver
+import org.excavator.boot.common.helper.JSONProxy
+import org.springframework.http.{HttpStatus, ResponseEntity}
+import org.springframework.stereotype.Component
+
+@Component("authorizationResolver")
+class CustomAuthroizationResolver extends AuthorizationResolver{
+
+  override def writeResponse(response: HttpServletResponse): Unit = {
+    val responseEntity = new ResponseEntity[String]("", HttpStatus.OK)
+
+    response.setContentType("application/json; charset=UTF-8")
+    response.setStatus(HttpStatus.OK.value())
+
+    val printout = response.getWriter
+
+    printout.print(JSONProxy.toJSONString(responseEntity))
+
+    printout.flush()
+  }
 
 }
