50a1065d518d0adeb0d7ff76ea5c4793ab78a8ff
CMonkey <42.codemonkey@gmail.com>
2019-06-21 17:49:26 +0800

    add utils import to GeneratorPublicPrivateKeyTests

--- common/src/test/java/org/excavator/boot/common/test/GeneratorPublicPrivateKeyTests.java
+++ common/src/test/java/org/excavator/boot/common/test/GeneratorPublicPrivateKeyTests.java
@@ -16,6 +16,10 @@
  */
 package org.excavator.boot.common.test;
 
+import org.excavator.boot.common.enums.ResolveEnum;
+import org.excavator.boot.common.utils.GeneratePublicPrivateKey;
+import org.excavator.boot.common.utils.GeneratePublicPrivateKeys;
+import org.excavator.boot.common.utils.PublicPrivateKey;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
