f9ea4a0c71cae5be7e5a20ae049b000d154e79a6
CMonkey <42.codemonkey@gmail.com>
2019-07-10 17:30:27 +0800

    add PhaserTest.testPhaser

--- concurrent/src/test/scala/org/excavator/boot/concurrent/test/PhaserTest.scala
+++ concurrent/src/test/scala/org/excavator/boot/concurrent/test/PhaserTest.scala
@@ -1,5 +1,38 @@
 package org.excavator.boot.concurrent.test
 
+import java.util.concurrent.{Executors, Phaser}
+
+import org.excavator.boot.concurrent.LongRunningAction
+import org.junit.jupiter.api.{DisplayName, Test}
+import org.junit.jupiter.api.Assertions._
+
 class PhaserTest {
+
+  @Test
+  @DisplayName("test Phaser")
+  def testPhaser(): Unit = {
+    val executeService = Executors.newCachedThreadPool()
+
+    val phaser = new Phaser(1)
+
+    assertEquals(0, phaser.getPhase)
+
+    executeService.submit(new LongRunningAction("thread-1", phaser))
+    executeService.submit(new LongRunningAction("thread-2", phaser))
+    executeService.submit(new LongRunningAction("thread-3", phaser))
+
+    phaser.arriveAndAwaitAdvance()
+
+    assertEquals(1, phaser.getPhase)
+
+    executeService.submit(new LongRunningAction("thread-4", phaser))
+    executeService.submit(new LongRunningAction("thread-5", phaser))
+
+    phaser.arriveAndAwaitAdvance()
+
+    assertEquals(2, phaser.getPhase)
+
+    phaser.arriveAndDeregister()
+  }
 
 }
