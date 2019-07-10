4a9ce93acab29ce57f9b4257a1306cd5ab1dab78
CMonkey <42.codemonkey@gmail.com>
2019-07-10 17:13:04 +0800

    add WaitingWorker.run

--- concurrent/src/main/scala/org/excavator/boot/concurrent/WaitingWorker.scala
+++ concurrent/src/main/scala/org/excavator/boot/concurrent/WaitingWorker.scala
@@ -1,5 +1,32 @@
 package org.excavator.boot.concurrent
 
-class WaitingWorker {
+import java.util
+import java.util.concurrent.CountDownLatch
 
+import org.slf4j.LoggerFactory
+
+class WaitingWorker(val outputScraper: util.List[String],
+                    val readyThreadCounter: CountDownLatch,
+                    val callingThreadBlocker: CountDownLatch,
+                    val completedThreadCounter: CountDownLatch) extends Runnable{
+
+  val logger = LoggerFactory.getLogger(classOf[WaitingWorker])
+
+  def doSomeWork(): Unit = {
+    logger.info(s"thread name = ${Thread.currentThread().getName} now = ${java.time.Instant.now()}")
+  }
+
+  override def run(): Unit = {
+    readyThreadCounter.countDown()
+
+    callingThreadBlocker.await()
+
+    doSomeWork()
+
+    outputScraper.add("Counted down")
+
+    logger.info(s"thread name = ${Thread.currentThread().getName} outputScraper = ${outputScraper}")
+
+    completedThreadCounter.countDown()
+  }
 }
