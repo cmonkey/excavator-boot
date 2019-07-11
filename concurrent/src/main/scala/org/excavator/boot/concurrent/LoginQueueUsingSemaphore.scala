fffa1985e7313a4b31a654357d6809f8bc8ec9b0
CMonkey <42.codemonkey@gmail.com>
2019-07-11 15:30:24 +0800

    add logger to LoginQueueUsingSemaphore

--- concurrent/src/main/scala/org/excavator/boot/concurrent/LoginQueueUsingSemaphore.scala
+++ concurrent/src/main/scala/org/excavator/boot/concurrent/LoginQueueUsingSemaphore.scala
@@ -2,11 +2,16 @@ package org.excavator.boot.concurrent
 
 import java.util.concurrent.Semaphore
 
+import org.slf4j.LoggerFactory
+
 class LoginQueueUsingSemaphore(val slotLimit: Int) {
+
+  val logger = LoggerFactory.getLogger(classOf[LoginQueueUsingSemaphore])
 
   val semaphore = new Semaphore(slotLimit)
 
   def tryLogin(): Boolean = {
+    logger.info(s"thread name = ${Thread.currentThread().getName} and availableSlots = ${availableSlots()}")
     semaphore.tryAcquire()
   }
 
