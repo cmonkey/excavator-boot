462bec60faa9e9f9e3961e6d728f3e78dd2d7583
CMonkey <42.codemonkey@gmail.com>
2019-07-11 15:02:56 +0800

    add LoginQueueUsingSemaphore.(tryLogin logout availableSlots )

--- concurrent/src/main/scala/org/excavator/boot/concurrent/LoginQueueUsingSemaphore.scala
+++ concurrent/src/main/scala/org/excavator/boot/concurrent/LoginQueueUsingSemaphore.scala
@@ -1,5 +1,21 @@
 package org.excavator.boot.concurrent
 
-class LoginQueueUsingSemaphore {
+import java.util.concurrent.Semaphore
+
+class LoginQueueUsingSemaphore(val slotLimit: Int) {
+
+  val semaphore = new Semaphore(slotLimit)
+
+  def tryLogin(): Boolean = {
+    semaphore.tryAcquire()
+  }
+
+  def logout(): Unit = {
+    semaphore.release()
+  }
+
+  def availableSlots(): Int = {
+    semaphore.availablePermits()
+  }
 
 }
