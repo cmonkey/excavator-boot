c15eef0990e3f3dfb0516731afd233dd39db6e64
CMonkey <42.codemonkey@gmail.com>
2019-07-15 17:05:45 +0800

    add SM4AlgorithmTests.testGenerator method

--- common/src/test/java/org/excavator/boot/common/test/SM4AlgorithmTests.java
+++ common/src/test/java/org/excavator/boot/common/test/SM4AlgorithmTests.java
@@ -1,4 +1,40 @@
 package org.excavator.boot.common.test;
 
+import org.excavator.boot.common.utils.GenerateSymmetricencryption;
+import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.crypto.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SM4AlgorithmTests {
+
+    private static final Logger                     logger          = LoggerFactory
+                                                                        .getLogger(SM4AlgorithmTests.class);
+
+    private static final AtomicReference<SecretKey> atomicReference = new AtomicReference<>();
+    private static final String                     ALGORITHM       = "SM4";
+    private static final String                     DATA            = "Hello SM4";
+
+    @Test
+    @DisplayName("test sm4 generator")
+    @Order(1)
+    public void testGenerator(){
+        Optional<SecretKey> secretKeyOptional = GenerateSymmetricencryption.getSecretKey(ALGORITHM, 126);
+
+        assertEquals(true, secretKeyOptional.isPresent());
+
+        secretKeyOptional.ifPresent(key -> {
+            atomicReference.set(key);
+            assertEquals(ALGORITHM, key.getAlgorithm());
+        });
+    }
+
 }
