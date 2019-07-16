28050dedd7238c76cb79a974d02541ac459ead53
CMonkey <42.codemonkey@gmail.com>
2019-07-16 16:10:53 +0800

    add kafka-docker/sed shell

---
+++ kafka-docker/sed.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+echo "pre change server.properties in $1"
+export ADVERTISED_LISTENERS=PLAINTEXT://127.0.0.1:9092
+
+if [ ! -z "$ADVERTISED_LISTENERS" ]; then
+    echo "advertised listeners: $ADVERTISED_LISTENERS"
+    export ESCAPED_ADVERTISED_LISTENERS=$(echo $ADVERTISED_LISTENERS | sed 's/\//\\\//g');
+    sed -r -i "s/#(advertised.listeners)=(.*)/\1=$ESCAPED_ADVERTISED_LISTENERS/g" $1
+fi
