0044a3b2817c4dd983ec9de9547ffc27d90dc6db
CMonkey <42.codemonkey@gmail.com>
2019-07-16 16:10:06 +0800

    delete sed.sh

--- sed.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-export ADVERTISED_LISTENERS=PLAINTEXT://127.0.0.1:9092
-
-if [ ! -z "$ADVERTISED_LISTENERS" ]; then
-    echo "advertised listeners: $ADVERTISED_LISTENERS"
-    export ESCAPED_ADVERTISED_LISTENERS=$(echo $ADVERTISED_LISTENERS | sed 's/\//\\\//g');
-    sed -r -i "s/#(advertised.listeners)=(.*)/\1=$ESCAPED_ADVERTISED_LISTENERS/g" server.properties
-fi
