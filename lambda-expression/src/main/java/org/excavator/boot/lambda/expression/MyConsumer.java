87b9a0c5b6b348455646afbdda3ca179b9e962c4
CMonkey <42.codemonkey@gmail.com>
2019-07-17 15:37:02 +0800

    add logger to MyConsumer.accept

--- lambda-expression/src/main/java/org/excavator/boot/lambda/expression/MyConsumer.java
+++ lambda-expression/src/main/java/org/excavator/boot/lambda/expression/MyConsumer.java
@@ -1,11 +1,14 @@
 package org.excavator.boot.lambda.expression;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.function.Consumer;
 
+@Slf4j
 public class MyConsumer implements Consumer<String> {
 
     @Override
     public void accept(String s) {
-
+        log.info("This {} is consumed", s);
     }
 }
