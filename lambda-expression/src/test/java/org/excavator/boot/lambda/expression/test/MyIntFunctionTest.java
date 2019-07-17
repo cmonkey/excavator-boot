eca94a9afd3cf6c9b8f4c22667d06824372b3e81
CMonkey <42.codemonkey@gmail.com>
2019-07-17 15:53:53 +0800

    add MyIntFunctionTest.testIntFunction method

--- lambda-expression/src/test/java/org/excavator/boot/lambda/expression/test/MyIntFunctionTest.java
+++ lambda-expression/src/test/java/org/excavator/boot/lambda/expression/test/MyIntFunctionTest.java
@@ -1,4 +1,20 @@
 package org.excavator.boot.lambda.expression.test;
 
+import lombok.extern.slf4j.Slf4j;
+import org.excavator.boot.lambda.expression.MyIntFunction;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
 public class MyIntFunctionTest {
+
+    @Test
+    @DisplayName("test IntFunction")
+    public void testIntFunction(){
+        MyIntFunction myIntFunction = new MyIntFunction();
+
+        assertEquals("1", myIntFunction.apply(1));
+    }
 }
