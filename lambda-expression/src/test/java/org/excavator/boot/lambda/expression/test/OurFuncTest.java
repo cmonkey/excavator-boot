ae78d35e4d49e4a1cbbe806927f8562ba713bae3
CMonkey <42.codemonkey@gmail.com>
2019-07-17 15:33:10 +0800

    add OurFuncTest.testOurFunc

--- lambda-expression/src/test/java/org/excavator/boot/lambda/expression/test/OurFuncTest.java
+++ lambda-expression/src/test/java/org/excavator/boot/lambda/expression/test/OurFuncTest.java
@@ -1,4 +1,17 @@
 package org.excavator.boot.lambda.expression.test;
 
+import org.excavator.boot.lambda.expression.OurFunc;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class OurFuncTest {
+
+    @Test
+    @DisplayName("test our func")
+    public void testOurFunc(){
+        OurFunc ourFunc = new OurFunc();
+        assertEquals(10.0, ourFunc.apply(1));
+    }
 }
