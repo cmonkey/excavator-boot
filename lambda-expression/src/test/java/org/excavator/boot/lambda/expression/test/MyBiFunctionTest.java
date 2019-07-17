59f76df3197e3ee685753886f251d4e14365e4dc
CMonkey <42.codemonkey@gmail.com>
2019-07-17 16:01:02 +0800

    add MyBiFunctionTest.testBiFunction

--- lambda-expression/src/test/java/org/excavator/boot/lambda/expression/test/MyBiFunctionTest.java
+++ lambda-expression/src/test/java/org/excavator/boot/lambda/expression/test/MyBiFunctionTest.java
@@ -1,4 +1,21 @@
 package org.excavator.boot.lambda.expression.test;
 
+import lombok.extern.slf4j.Slf4j;
+import org.excavator.boot.lambda.expression.MyBiFunction;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
 public class MyBiFunctionTest {
+
+    @Test
+    @DisplayName("test bi function")
+    public void testBiFunction(){
+
+        MyBiFunction myBiFunction = new MyBiFunction();
+
+        assertEquals(3, myBiFunction.apply("foo", 10));
+    }
 }
