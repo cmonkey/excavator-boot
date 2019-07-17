951e10878bf5a10e20f1cc9e30bb844a094dc35a
CMonkey <42.codemonkey@gmail.com>
2019-07-17 15:42:02 +0800

    add MySupplierTest.testSupplier

--- lambda-expression/src/test/java/org/excavator/boot/lambda/expression/test/MySupplierTest.java
+++ lambda-expression/src/test/java/org/excavator/boot/lambda/expression/test/MySupplierTest.java
@@ -1,4 +1,18 @@
 package org.excavator.boot.lambda.expression.test;
 
+import org.excavator.boot.lambda.expression.MySupplier;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MySupplierTest {
+
+    @Test
+    @DisplayName("test supplier")
+    public void testSupplier(){
+        MySupplier mySupplier = new MySupplier();
+
+        assertEquals("Success", mySupplier.get());
+    }
 }
