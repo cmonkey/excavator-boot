a8c4cfa6587258aae9c875a044e55cfabb088036
CMonkey <42.codemonkey@gmail.com>
2019-07-17 16:08:15 +0800

    add MyIntBinaryOperatorTest.testIntBinaryOperator

--- lambda-expression/src/test/java/org/excavator/boot/lambda/expression/test/MyIntBinaryOperatorTest.java
+++ lambda-expression/src/test/java/org/excavator/boot/lambda/expression/test/MyIntBinaryOperatorTest.java
@@ -1,4 +1,20 @@
 package org.excavator.boot.lambda.expression.test;
 
+import lombok.extern.slf4j.Slf4j;
+import org.excavator.boot.lambda.expression.MyIntBinaryOperator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
 public class MyIntBinaryOperatorTest {
+
+    @Test
+    @DisplayName("test intBinaryOperator")
+    public void testIntBinaryOperator(){
+        MyIntBinaryOperator myIntBinaryOperator = new MyIntBinaryOperator();
+
+        assertEquals(2, myIntBinaryOperator.applyAsInt(1, 2));
+    }
 }
