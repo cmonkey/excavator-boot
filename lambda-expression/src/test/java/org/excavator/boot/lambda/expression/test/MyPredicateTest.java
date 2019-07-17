fa22ea785053f3e66e53d1ab2fb18ea1836af606
CMonkey <42.codemonkey@gmail.com>
2019-07-17 15:45:05 +0800

    add MyPredicateTest.testPredicate

--- lambda-expression/src/test/java/org/excavator/boot/lambda/expression/test/MyPredicateTest.java
+++ lambda-expression/src/test/java/org/excavator/boot/lambda/expression/test/MyPredicateTest.java
@@ -1,4 +1,17 @@
 package org.excavator.boot.lambda.expression.test;
 
+import org.excavator.boot.lambda.expression.MyPredicate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyPredicateTest {
+
+    @Test
+    @DisplayName("test Predicate")
+    public void testPredicate(){
+        MyPredicate  myPredicate = new MyPredicate();
+        assertEquals(myPredicate.test(10.0), true);
+    }
 }
