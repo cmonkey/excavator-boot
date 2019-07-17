9a2acac3f599af8ff6aadf6465a92ebaab3fc2bf
CMonkey <42.codemonkey@gmail.com>
2019-07-17 15:50:40 +0800

    add ExpressionTests.testLambdaExpression method

--- lambda-expression/src/test/java/org/excavator/boot/lambda/expression/test/ExpressionTests.java
+++ lambda-expression/src/test/java/org/excavator/boot/lambda/expression/test/ExpressionTests.java
@@ -1,4 +1,36 @@
 package org.excavator.boot.lambda.expression.test;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
 public class ExpressionTests {
+
+    @Test
+    @DisplayName("test lambda expression")
+    public void testLambdaExpression(){
+        Function<Integer, Double> ourFunc = i -> i * 10.0;
+
+        assertEquals(10,0, ourFunc.apply(1));
+
+        Consumer<String> consumer = s -> log.info("This {} is consumed", s);
+
+        consumer.accept("Hello");
+
+        Supplier<String> supplier = () -> "Success";
+
+        assertEquals("Success", supplier.get());
+
+        Predicate<Double> predicate = num -> num == 10.0;
+
+        assertEquals(true, predicate.test(10.0));
+    }
 }
