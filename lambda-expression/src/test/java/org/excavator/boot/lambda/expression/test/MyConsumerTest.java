f8ffef58da6b6a8644a582857f718e030caae56d
CMonkey <42.codemonkey@gmail.com>
2019-07-17 15:39:04 +0800

    add MyConsumerTest.testConsumer

--- lambda-expression/src/test/java/org/excavator/boot/lambda/expression/test/MyConsumerTest.java
+++ lambda-expression/src/test/java/org/excavator/boot/lambda/expression/test/MyConsumerTest.java
@@ -1,4 +1,15 @@
 package org.excavator.boot.lambda.expression.test;
 
+import org.excavator.boot.lambda.expression.MyConsumer;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class MyConsumerTest {
+
+    @Test
+    @DisplayName("test consumer accept")
+    public void testConsumer(){
+        MyConsumer myConsumer = new MyConsumer();
+        myConsumer.accept("Hello");
+    }
 }
