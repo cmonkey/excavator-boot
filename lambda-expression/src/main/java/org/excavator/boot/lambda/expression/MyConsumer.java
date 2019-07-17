6a8771d2c3554a10b0e0f539f63e80603f85387a
CMonkey <42.codemonkey@gmail.com>
2019-07-17 15:36:49 +0800

    add MyConsumer

---
+++ lambda-expression/src/main/java/org/excavator/boot/lambda/expression/MyConsumer.java
@@ -0,0 +1,11 @@
+package org.excavator.boot.lambda.expression;
+
+import java.util.function.Consumer;
+
+public class MyConsumer implements Consumer<String> {
+
+    @Override
+    public void accept(String s) {
+
+    }
+}
