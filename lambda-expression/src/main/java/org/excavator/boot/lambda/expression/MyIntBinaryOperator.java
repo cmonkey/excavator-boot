e8746a356c892ec605245e96170bfcde59b2a90d
CMonkey <42.codemonkey@gmail.com>
2019-07-17 16:06:21 +0800

    add MyIntBinaryOperator.applyAsInt

--- lambda-expression/src/main/java/org/excavator/boot/lambda/expression/MyIntBinaryOperator.java
+++ lambda-expression/src/main/java/org/excavator/boot/lambda/expression/MyIntBinaryOperator.java
@@ -1,4 +1,10 @@
 package org.excavator.boot.lambda.expression;
 
-public class MyIntBinaryOperator {
+import java.util.function.IntBinaryOperator;
+
+public class MyIntBinaryOperator implements IntBinaryOperator {
+    @Override
+    public int applyAsInt(int i, int j) {
+        return i > j? i: j;
+    }
 }
