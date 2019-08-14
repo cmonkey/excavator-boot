fb3593fe13ae717a1a2f078d30deba4a1e86b4c1
CMonkey <42.codemonkey@gmail.com>
2019-08-14 16:18:08 +0800

    add CryptoHelperTests.testDecrypt

--- helper/src/test/scala/org/excavator/boot/helper/test/CryptoHelperTests.scala
+++ helper/src/test/scala/org/excavator/boot/helper/test/CryptoHelperTests.scala
@@ -1,6 +1,9 @@
 package org.excavator.boot.helper.test
 
+import java.nio.charset.StandardCharsets
+
 import javax.annotation.Resource
+import org.apache.commons.codec.binary.Hex
 import org.excavator.boot.common.enums.ResolveEnum
 import org.excavator.boot.common.utils.GeneratePublicPrivateKeys
 import org.excavator.boot.helper.CryptoHelper
@@ -40,6 +43,30 @@ class CryptoHelperTests {
     val optionalPublicPrivateKey = cryptoHelper.getPublicPrivateKey(customerId, algorithm)
 
     assertEquals(true, optionalPublicPrivateKey.isPresent)
+  }
+
+
+  @Test
+  @DisplayName("test Decrypt by crypto")
+  def testDecrypt(): Unit = {
+    val text = "049ef8042ac47d18b7d67c7acfef2309e861763a7b0d54a0aadfdacb558a51f1bf90082bfceb8aa2599965ce1afbf316c522f5e90d4373ab570f7f145b0435a59334b4cfdd0f0dd82f7bd98e126799a75fe21c21106a96e0c676203ba512ad3cc208fd22d49ef6da7dfebde5f2c071ac8831989ac222ddafb13a4f918d53402945d120a9362d8278a6e1ed6cb763b617da14b53009a28ecb98c3eba31ce96b"
+
+    val customerId = "1";
+
+    val algorithm = "EC"
+
+    val optionalPublicPrivateKey = cryptoHelper.getPublicPrivateKey(customerId, algorithm);
+
+    optionalPublicPrivateKey.ifPresent(publicPrivateKey => {
+      val input = Hex.decodeHex(text)
+      val dec  = GeneratePublicPrivateKeys.decryptBySM2(input, publicPrivateKey.getPrivateKey)
+
+      dec.ifPresent(r => {
+        val result = new String(r, StandardCharsets.UTF_8)
+        println(s"${result}")
+      })
+    })
+
   }
 
 }
