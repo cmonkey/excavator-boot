9fd12e07cc89fcf3b4904538e57758fa3178f23d
CMonkey <42.codemonkey@gmail.com>
2019-08-22 16:43:31 +0800

    publish redis port

--- init-test-environment.sh
+++ init-test-environment.sh
@@ -21,4 +21,4 @@ docker run --name kafka --link zookeeper -p 9092:9092 -d kafka
 
 ### init redis 
 
-docker run --restart=always --name redis -d redis redis-server --appendonly yes
+docker run --restart=always --name redis -p 6379:6379 -d redis redis-server --appendonly yes
