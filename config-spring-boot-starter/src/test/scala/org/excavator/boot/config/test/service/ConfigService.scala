8318599364a45dd4e690f437c0c58c97fed7806a
CMonkey <42.codemonkey@gmail.com>
2019-11-13 11:52:41 +0800

    add list field to ConfigService

--- config-spring-boot-starter/src/test/scala/org/excavator/boot/config/test/service/ConfigService.scala
+++ config-spring-boot-starter/src/test/scala/org/excavator/boot/config/test/service/ConfigService.scala
@@ -1,7 +1,7 @@
 package org.excavator.boot.config.test.service
 
-import java.util.concurrent.ArrayBlockingQueue
-
+import com.google.common.collect.Lists
+import javax.annotation.PostConstruct
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
@@ -9,6 +9,36 @@ import org.springframework.stereotype.Service
 class ConfigService {
   val logger = LoggerFactory.getLogger(classOf[ConfigService])
 
-  val blockingQueue = new ArrayBlockingQueue[String](1)
+  val list = Lists.newArrayList[String]()
+
+  @PostConstruct
+  def init(): Unit = {
+    list.add("foo")
+    list.add("cmonkey")
+  }
+
+  def findAll() = {
+    list
+  }
+
+  def getUserName(userName: String) = {
+    list.stream().filter(p => p.equals(userName)).findFirst().get()
+  }
+
+  def addUserName(userName: String) = {
+    list.add(userName)
+  }
+
+  def deleteUserName(userName: String) = {
+    val iterator = list.iterator()
+
+    while(iterator.hasNext){
+      val elem = iterator.next()
+
+      if(elem.equals(userName)){
+        synchronized(iterator.remove())
+      }
+    }
+  }
 
 }
