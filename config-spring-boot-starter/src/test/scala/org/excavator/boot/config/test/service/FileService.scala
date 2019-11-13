1d58db4aebcf36d02538725980e6c36562654517
CMonkey <42.codemonkey@gmail.com>
2019-11-13 15:02:24 +0800

    add init storeFile functions to FileService

--- config-spring-boot-starter/src/test/scala/org/excavator/boot/config/test/service/FileService.scala
+++ config-spring-boot-starter/src/test/scala/org/excavator/boot/config/test/service/FileService.scala
@@ -1,5 +1,32 @@
 package org.excavator.boot.config.test.service
 
-class FileService {
+import java.io.File
+import java.nio.file.{Files, Paths, StandardCopyOption}
 
+import javax.annotation.PostConstruct
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import org.springframework.util.StringUtils
+import org.springframework.web.multipart.MultipartFile
+
+@Service
+class FileService {
+  val log = LoggerFactory.getLogger(classOf[FileService])
+  val uploadFile = System.getProperty("java.io.tmpdir") + "/uploadDir/"
+
+  @PostConstruct
+  def init(): Unit = {
+    val file = new File(uploadFile)
+
+    if(!file.exists()){
+      file.mkdir()
+    }
+  }
+
+  def storeFile(multipartFile: MultipartFile): Unit = {
+
+    val copyLocation = Paths.get(uploadFile + File.separator + StringUtils.cleanPath(multipartFile.getOriginalFilename))
+
+    Files.copy(multipartFile.getInputStream, copyLocation, StandardCopyOption.REPLACE_EXISTING)
+  }
 }
