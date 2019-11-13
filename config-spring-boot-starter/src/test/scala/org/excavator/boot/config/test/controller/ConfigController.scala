8a07c9c0894d8be3a2ef34dadfda835281ef162d
CMonkey <42.codemonkey@gmail.com>
2019-11-13 11:52:26 +0800

    add getUserName to ConfigController

--- config-spring-boot-starter/src/test/scala/org/excavator/boot/config/test/controller/ConfigController.scala
+++ config-spring-boot-starter/src/test/scala/org/excavator/boot/config/test/controller/ConfigController.scala
@@ -1,14 +1,15 @@
 package org.excavator.boot.config.test.controller
 
 import io.swagger.annotations.{Api, ApiOperation, ApiResponse, ApiResponses}
+import org.excavator.boot.config.test.service.ConfigService
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.{GetMapping, RequestMapping, RestController}
+import org.springframework.web.bind.annotation.{GetMapping, PathVariable, RequestMapping, RestController}
 
 @RestController
 @RequestMapping(Array("/v1"))
 @Api(value = "Config info Api", description = "Config Service")
-class ConfigController {
+class ConfigController(configService: ConfigService) {
   val log = LoggerFactory.getLogger(classOf[ConfigController])
 
   @ApiOperation(value = "首页", response = classOf[ResponseEntity[String]], notes = "首页详情")
@@ -18,10 +19,15 @@ class ConfigController {
     new ApiResponse(code = 10003, message = "系统异常")
   )
   )
-  @GetMapping(Array("/"))
-  def home(): ResponseEntity[String] = {
+  @GetMapping(Array("/users"))
+  def findAll(): ResponseEntity[java.util.List[String]] = {
     log.info("home running by time = [{}]", System.nanoTime())
-    ResponseEntity.ok("Hello")
+    ResponseEntity.ok(configService.findAll())
+  }
+
+  @GetMapping(Array("/users/{userName}"))
+  def getUserName(@PathVariable("userName") userName: String) = {
+    ResponseEntity.ok(configService.getUserName(userName))
   }
 
 }
