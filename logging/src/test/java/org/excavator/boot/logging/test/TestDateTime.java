061cd859fb1ccd2a8572a11214c371f87eab6d65
CMonkey <42.codemonkey@gmail.com>
2020-01-18 14:21:20 +0800

    add License and format code

--- logging/src/test/java/org/excavator/boot/logging/test/TestDateTime.java
+++ logging/src/test/java/org/excavator/boot/logging/test/TestDateTime.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.excavator.boot.logging.test;
 
 import org.junit.jupiter.api.DisplayName;
@@ -19,12 +35,12 @@ public class TestDateTime {
 
     @Test
     @DisplayName("testDateTimeBy2020")
-    public void testDateTimeBy2020(){
+    public void testDateTimeBy2020() {
 
         long currentTimeMillis = 1577771671190L;
         Date date = new Date(currentTimeMillis);
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        logger.info("simple format date = [{}]",simpleDateFormat.format(date));
+        logger.info("simple format date = [{}]", simpleDateFormat.format(date));
 
         Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
         calendar.setTime(date);
@@ -36,10 +52,11 @@ public class TestDateTime {
         String localDateTimeFormat = localDateTime.format(dateTimeFormatter);
         logger.info("localDateTime format = [{}]", localDateTimeFormat);
 
-        assertEquals("2019",localDateTimeFormat.split("-")[0]);
+        assertEquals("2019", localDateTimeFormat.split("-")[0]);
 
         LocalDateTime localDateTimeIso8601 = LocalDateTime.of(2019, 12, 31, 13, 59, 21);
-        DateTimeFormatter dateTimeFormatterIso8601 = DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm:ss");
+        DateTimeFormatter dateTimeFormatterIso8601 = DateTimeFormatter
+            .ofPattern("YYYY-MM-dd HH:mm:ss");
         String localDateTimeIso8601Format = localDateTimeIso8601.format(dateTimeFormatterIso8601);
         logger.info("localDateTime iso-8601 format = [{}]", localDateTimeIso8601Format);
 
