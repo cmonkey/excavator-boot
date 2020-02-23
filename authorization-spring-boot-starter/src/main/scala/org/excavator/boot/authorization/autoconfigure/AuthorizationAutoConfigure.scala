359d2a19d9b151845404a0845ea8e934245346ad
CMonkey <42.codemonkey@gmail.com>
2020-02-23 11:52:25 +0800

    fix typo AutorizationAutoConfigure to AuthorizationAutoConfigure

--- authorization-spring-boot-starter/src/main/scala/org/excavator/boot/authorization/autoconfigure/AuthorizationAutoConfigure.scala
+++ authorization-spring-boot-starter/src/main/scala/org/excavator/boot/authorization/autoconfigure/AuthorizationAutoConfigure.scala
@@ -23,8 +23,8 @@ import org.springframework.data.redis.core.StringRedisTemplate
 @AutoConfigureAfter(Array(classOf[RedisAutoConfiguration]))
 @EnableConfigurationProperties(Array(classOf[AuthorizationProperties]))
 @Import(Array(classOf[AuthorizationInterceptor], classOf[AuthorizationConfig], classOf[HandlerMethodArgumentResolverFactory], classOf[CustomerHelper]))
-class AutorizationAutoConfigure {
-  private val logger = LoggerFactory.getLogger(classOf[AutorizationAutoConfigure])
+class AuthorizationAutoConfigure {
+  private val logger = LoggerFactory.getLogger(classOf[AuthorizationAutoConfigure])
 
   @Bean
   def tokenManager(@Qualifier("stringRedisTemplate") stringRedisTemplate: StringRedisTemplate): TokenManager = {
@@ -46,7 +46,7 @@ class AutorizationAutoConfigure {
 
     val authorizationResolver = new AuthorizationResolver
 
-    logger.info("authroizationResolver init ");
+    logger.info("authorizationResolver init ");
 
     authorizationResolver
   }
