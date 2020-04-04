db5cde702b8b2522218d03a3c171d99cd94e75e5
CMonkey <42.codemonkey@gmail.com>
2020-04-04 13:32:30 +0800

    add main startup UnmodifiableCollectionBenchmark

--- benchmark/src/main/java/org/excavator/boot/benchmark/UnmodifiableCollectionBenchmark.java
+++ benchmark/src/main/java/org/excavator/boot/benchmark/UnmodifiableCollectionBenchmark.java
@@ -1,21 +1,31 @@
 package org.excavator.boot.benchmark;
 
-import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
+@BenchmarkMode(Mode.Throughput)
+@Warmup(iterations = 3)
+@Measurement(iterations = 10, time = 5, timeUnit = TimeUnit.SECONDS)
+@Threads(8)
+@Fork(2)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class UnmodifiableCollectionBenchmark {
 
-    private long size = 10;
-    Set<Integer> underlyingCollection = new Random().ints(size).mapToObj(i -> i )
+    private static final long size = 10;
+    final static Set<Integer> underlyingCollection = new Random().ints(size).mapToObj(i -> i )
             .collect(Collectors.toCollection(HashSet::new));
-    Set<Integer> unmodifiableCollection = underlyingCollection;
+    final static Set<Integer> unmodifiableCollection = underlyingCollection;
     @Benchmark
     public boolean isEmpty(){
         return unmodifiableCollection.isEmpty();
@@ -57,5 +67,12 @@ public class UnmodifiableCollectionBenchmark {
 
     private int[] getAts() {
         return IntStream.range(0, (int)size).toArray();
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options ops = new OptionsBuilder().include(UnmodifiableCollectionBenchmark.class.getSimpleName()).forks(2)
+                .warmupIterations(5).measurementIterations(5).build();
+
+        new Runner(ops).run();
     }
 }
