99ef75652e16c5cd483aeaf9a52d89b85a896f39
CMonkey <42.codemonkey@gmail.com>
2020-04-04 13:18:19 +0800

    add Benchmark methods(isEmpty forEach iterator)

--- benchmark/src/main/java/org/excavator/boot/benchmark/UnmodifiableCollectionBenchmark.java
+++ benchmark/src/main/java/org/excavator/boot/benchmark/UnmodifiableCollectionBenchmark.java
@@ -1,4 +1,34 @@
 package org.excavator.boot.benchmark;
 
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.infra.Blackhole;
+
+import javax.management.MXBean;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class UnmodifiableCollectionBenchmark {
+
+    private long size = 10;
+    Set<Integer> underlyingCollection = new Random().ints(size).mapToObj(i -> i )
+            .collect(Collectors.toCollection(HashSet::new));
+    Set<Integer> unmodifiableCollection = underlyingCollection;
+    @Benchmark
+    public boolean isEmpty(){
+        return unmodifiableCollection.isEmpty();
+    }
+
+    @Benchmark
+    public void forEach(Blackhole hole){
+        unmodifiableCollection.forEach(hole::consume);
+    }
+
+    @Benchmark
+    public void interator(Blackhole hole){
+        for (Integer i : unmodifiableCollection) {
+            hole.consume(i);
+        }
+    }
 }
