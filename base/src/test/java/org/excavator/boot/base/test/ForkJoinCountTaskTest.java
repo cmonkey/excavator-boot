aa8363a65c01225f43145cf4f8e86e4da3dda74c
CMonkey <42.codemonkey@gmail.com>
2020-07-17 22:28:48 +0800

    add ForkJoinCountTaskTest.testForkJoinCountTask method

--- base/src/test/java/org/excavator/boot/base/test/ForkJoinCountTaskTest.java
+++ base/src/test/java/org/excavator/boot/base/test/ForkJoinCountTaskTest.java
@@ -1,7 +1,23 @@
-package org.excavator.boot.base.forkjoin;
+package org.excavator.boot.base.test;
+
+import org.excavator.boot.base.forkjoin.ForkJoinCountTask;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ForkJoinCountTaskTest {
+
+    @Test
+    @DisplayName("testForkJoinCountTask")
+    public void testForkJoinCountTask(){
+        ForkJoinPool pool = new ForkJoinPool();
+        ForkJoinCountTask task = new ForkJoinCountTask(1, 100);
+        ForkJoinTask<Integer> result = pool.submit(task);
+        assertEquals(5050, result);
+    }
 
 }
