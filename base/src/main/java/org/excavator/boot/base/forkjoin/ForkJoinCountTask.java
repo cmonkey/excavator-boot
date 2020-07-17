23c681339d511bc47c8dcd92d8f058e0f97037d7
CMonkey <42.codemonkey@gmail.com>
2020-07-17 22:21:32 +0800

    add compute method

--- base/src/main/java/org/excavator/boot/base/forkjoin/ForkJoinCountTask.java
+++ base/src/main/java/org/excavator/boot/base/forkjoin/ForkJoinCountTask.java
@@ -1,4 +1,39 @@
 package org.excavator.boot.base.forkjoin;
 
-public class ForkJoinCountTask {
+import java.util.concurrent.RecursiveTask;
+
+public class ForkJoinCountTask extends RecursiveTask<Integer> {
+    public static final int THRESHOLD = 10;
+    private int start;
+    private int end;
+
+    public ForkJoinCountTask(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Integer compute() {
+        int sum = 0;
+        boolean canCompute = (end - start) <= THRESHOLD;
+        if(canCompute){
+            System.out.println(String.format("start to compute from %d to %d", start, end));
+            for (int i = 0; i < end; i++) {
+                sum += i;
+            }
+        }else{
+            int middle = (start + end ) / 2;
+            ForkJoinCountTask left = new ForkJoinCountTask(start, middle);
+            ForkJoinCountTask right = new ForkJoinCountTask(middle + 1, end);
+
+            left.fork();
+            right.fork();
+
+            Integer leftResult = left.join();
+            Integer rightResult = right.join();
+
+            sum = leftResult + rightResult;
+        }
+        return sum;
+    }
 }
