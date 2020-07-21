6f9e0388f4ec8e8aed06b5922bebdd7e5b15890f
CMonkey <42.codemonkey@gmail.com>
2020-07-21 13:31:18 +0800

    change run zookeeper container version 3.4.10 to 3.6.1

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ echo '127.0.0.1 www.excavator.boot' | sudo tee -a /etc/hosts
 echo '127.0.0.1 www.excavator.com' | sudo tee -a /etc/hosts
 
 ### init zookeeper 
-docker run --restart=always --name zookeeper -p 2181:2181 -p 2888:2888 -p 3888:3888 -d zookeeper:3.4.10
+docker run --restart=always --name zookeeper -p 2181:2181 -p 2888:2888 -p 3888:3888 -d zookeeper:3.6.1
 
 ### init mysql 
 docker run --name mysql -v ${PWD}/dockerfiles:/docker-entrypoint-initdb.d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7 --collation-server=utf8_unicode_ci --character-set-server=utf8 --lower_case_table_names=1
