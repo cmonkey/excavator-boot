da8ad0435660c26c9ab08e028be01ca9b0ab8eca
CMonkey <42.codemonkey@gmail.com>
2020-07-24 10:55:52 +0800

    add testMonixIO function to MonixIOTest

--- experiment/src/test/scala/org/excavator/boot/experiment/test/MonixIOTest.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/MonixIOTest.scala
@@ -1,5 +1,14 @@
 package org.excavator.boot.experiment.test
 
+import org.excavator.boot.experiment.MonixIO
+import org.junit.jupiter.api.{DisplayName, Test}
+
 class MonixIOTest {
+
+  @Test
+  @DisplayName("testMonixIO")
+  def testMonixIO() = {
+    MonixIO.run()
+  }
 
 }
