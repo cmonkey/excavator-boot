2a97e7f35fdb799e87c6849bea6e41c44935a7cf
CMonkey <42.codemonkey@gmail.com>
2020-07-24 11:00:22 +0800

    add run function to MonixIOscheduler

--- experiment/src/main/scala/org/excavator/boot/experiment/MonixIOscheduler.scala
+++ experiment/src/main/scala/org/excavator/boot/experiment/MonixIOscheduler.scala
@@ -1,5 +1,24 @@
 package org.excavator.boot.experiment
 
+import monix.bio.IO
+
+import monix.execution.Scheduler.Implicits.global
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+
 class MonixIOscheduler {
+
+  def run() = {
+
+    val bio = IO{println("Hello, World!"); "Hi"}
+
+    val scalaFuture: Future[String] = bio.runToFuture
+
+    val result:String = Await.result(scalaFuture, 5.seconds)
+
+    result
+
+  }
 
 }
