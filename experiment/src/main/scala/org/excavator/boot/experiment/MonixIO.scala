eb2f1eb15520f3add3083fe07f9ce1327acdd65f
CMonkey <42.codemonkey@gmail.com>
2020-07-24 10:55:42 +0800

    add run function to MonixIO

--- experiment/src/main/scala/org/excavator/boot/experiment/MonixIO.scala
+++ experiment/src/main/scala/org/excavator/boot/experiment/MonixIO.scala
@@ -1,5 +1,36 @@
 package org.excavator.boot.experiment
 
-class MonixIO {
+import monix.bio.{IO, UIO}
+import monix.execution.CancelableFuture
+import scala.concurrent.duration._
+
+import monix.execution.Scheduler.Implicits.global
+
+case class TypedError(i: Int)
+
+object MonixIO{
+
+  def run() = {
+
+    val taskA: UIO[Int] = IO.now(10)
+      .delayExecution(2.seconds)
+      .doOnCancel(UIO(println("taskA hash been cancelled")))
+
+    val taskB: IO[TypedError, Int] = IO.raiseError(TypedError(-1))
+      .delayExecution(1.second)
+      .guarantee(UIO(println("taskB hash finished")))
+
+    val t: IO[TypedError, Int] = IO.race(taskA, taskB).map {
+      case Left(value) => value * 10
+      case Right(value) => value * 20
+    }
+
+    val handled: UIO[Int] = t.onErrorHandle {
+      case TypedError(i) => i
+    }
+
+    val f: CancelableFuture[Int] = handled.runToFuture
+
+  }
 
 }
