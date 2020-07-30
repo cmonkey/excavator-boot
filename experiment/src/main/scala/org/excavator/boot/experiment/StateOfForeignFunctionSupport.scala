bc1a706ba76ce54c3f3a7c2eec50770aad0922fb
CMonkey <42.codemonkey@gmail.com>
2020-07-30 10:29:31 +0800

    add import package  to StateOfForeignFunctionSupport

--- experiment/src/main/scala/org/excavator/boot/experiment/StateOfForeignFunctionSupport.scala
+++ experiment/src/main/scala/org/excavator/boot/experiment/StateOfForeignFunctionSupport.scala
@@ -1,5 +1,10 @@
 package org.excavator.boot.experiment
-
+import jdk.incubator.foreign.MemoryAddress;
+import jdk.incubator.foreign.MemorySegment;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.util.Arrays;
 class StateOfForeignFunctionSupport {
 
 }
