67e607bcd4cca36e1613f437485489f954e3ad17
CMonkey <42.codemonkey@gmail.com>
2020-08-22 00:33:58 +0800

    add getLinuxInodeNumber function

--- experiment/src/main/scala/org/excavator/boot/experiment/LinuxInodeNumber.scala
+++ experiment/src/main/scala/org/excavator/boot/experiment/LinuxInodeNumber.scala
@@ -1,5 +1,17 @@
 package org.excavator.boot.experiment
 
+import java.nio.file.attribute.BasicFileAttributes
+import java.nio.file.{Files, Paths}
+
 class LinuxInodeNumber {
+
+  def getLinuxInodeNumber(path:String) = {
+    val path = Paths.get(path)
+    val attr = Files.readAttributes(path, classOf[BasicFileAttributes])
+    val fileKey = attr.fileKey()
+    val s = fileKey.toString()
+    val inode = s.substring(s.indexOf("ino=") + 4, s.indexOf(")"))
+    inode
+  }
 
 }
