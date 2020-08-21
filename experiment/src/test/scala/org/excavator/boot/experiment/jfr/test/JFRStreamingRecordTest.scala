5a73daaf722b6c6c1a9262e79f52c8a94ebb45bf
CMonkey <42.codemonkey@gmail.com>
2020-08-21 09:41:22 +0800

    add dynamic param to  testRecordingStream function

--- experiment/src/test/scala/org/excavator/boot/experiment/jfr/test/JFRStreamingRecordTest.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/jfr/test/JFRStreamingRecordTest.scala
@@ -1,16 +1,19 @@
 package org.excavator.boot.experiment.jfr.test
 
-import java.util.concurrent.TimeUnit
+import java.time.Duration
 
 import org.excavator.boot.experiment.jfr.JFRStreamingRecord
 import org.junit.jupiter.api.{DisplayName, Test}
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class JFRStreamingRecordTest {
 
-  @Test
   @DisplayName("testRecordingStream")
-  def testRecordingStream(): Unit = {
-    JFRStreamingRecord.recodingStream()
+  @ParameterizedTest
+  @ValueSource(ints = Array(10))
+  def testRecordingStream(seconds: Int): Unit = {
+    JFRStreamingRecord.recodingStream(Duration.ofSeconds(seconds))
   }
 
 }
