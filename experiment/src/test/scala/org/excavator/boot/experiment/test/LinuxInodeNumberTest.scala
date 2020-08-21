9dab40ac19fcd6d73fc293b6167f0a0161003310
CMonkey <42.codemonkey@gmail.com>
2020-08-22 00:44:32 +0800

    add testGetLinuxInodeNumber

--- experiment/src/test/scala/org/excavator/boot/experiment/test/LinuxInodeNumberTest.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/LinuxInodeNumberTest.scala
@@ -1,5 +1,18 @@
 package org.excavator.boot.experiment.test
 
+import org.excavator.boot.experiment.LinuxInodeNumber
+import org.junit.jupiter.api.{DisplayName, Test}
+
 class LinuxInodeNumberTest {
+
+  @Test
+  @DisplayName("testGetLinuxInodeNumber")
+  def testGetLinuxInodeNumber(): Unit = {
+    val uri = "/tmp/tmux-1000/default"
+    val inode = LinuxInodeNumber(uri)
+    println(s"inode = ${inode}")
+
+    assert(inode.toInt > 0)
+  }
 
 }
