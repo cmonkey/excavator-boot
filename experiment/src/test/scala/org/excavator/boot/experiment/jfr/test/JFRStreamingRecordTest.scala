787d6fd5f521b535bff7c85fc5667af19606f958
CMonkey <42.codemonkey@gmail.com>
2020-08-21 09:33:08 +0800

    add testRecodingStream function

--- experiment/src/test/scala/org/excavator/boot/experiment/jfr/test/JFRStreamingRecordTest.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/jfr/test/JFRStreamingRecordTest.scala
@@ -1,5 +1,17 @@
 package org.excavator.boot.experiment.jfr.test
 
+import java.util.concurrent.TimeUnit
+
+import org.excavator.boot.experiment.jfr.JFRStreamingRecord
+import org.junit.jupiter.api.{DisplayName, Test}
+
 class JFRStreamingRecordTest {
+
+  @Test
+  @DisplayName("testRecordingStream")
+  def testRecordingStream(): Unit = {
+    JFRStreamingRecord.recodingStream()
+    TimeUnit.SECONDS.sleep(10)
+  }
 
 }
