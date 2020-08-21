9bf93e4b2306fc5d41b7eea787a023f34f61deb4
CMonkey <42.codemonkey@gmail.com>
2020-08-21 09:30:06 +0800

    add recodingStream method

--- experiment/src/main/java/org/excavator/boot/experiment/jfr/JFRStreamingRecord.java
+++ experiment/src/main/java/org/excavator/boot/experiment/jfr/JFRStreamingRecord.java
@@ -1,2 +1,27 @@
-package org.excavator.boot.experiment.jfr;public class JFRStreamingRecord {
+package org.excavator.boot.experiment.jfr;
+
+import jdk.jfr.consumer.RecordingStream;
+
+import java.time.Duration;
+
+public class JFRStreamingRecord {
+
+    public static void main(String[] args) {
+        recodingStream();
+    }
+
+    public static void recodingStream(){
+        try(var rs = new RecordingStream()){
+            rs.enable("jdk.CPULoad").withPeriod(Duration.ofSeconds(1));
+            rs.enable("jdk.JavaMonitorEnter").withThreshold(Duration.ofMillis(10));
+            rs.onEvent("jdk.CPULoad", recordedEvent -> {
+                System.out.println(recordedEvent.getFloat("machineTotal"));
+            });
+            rs.onEvent("jdk.JavaMonitorEnter", recordedEvent -> {
+                System.out.println(recordedEvent.getClass("monitorClass"));
+            });
+
+            rs.start();
+        }
+    }
 }
