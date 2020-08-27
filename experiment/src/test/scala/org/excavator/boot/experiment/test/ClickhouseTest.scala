ae9a83052703480b3e7174cc2f540b76c3a57efe
CMonkey <42.codemonkey@gmail.com>
2020-08-27 14:47:38 +0800

    add init function

--- experiment/src/test/scala/org/excavator/boot/experiment/test/ClickhouseTest.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/ClickhouseTest.scala
@@ -1,20 +1,32 @@
 package org.excavator.boot.experiment.test
 
-import java.sql.DriverManager
+import java.sql.{Connection, DriverManager}
 
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.{BeforeAll, DisplayName, Test}
 
 class ClickhouseTest {
 
   @Test
   @DisplayName("testSimpleQuery")
   def testSimpleQuery(): Unit = {
-    val connection = DriverManager.getConnection("jdbc:clickhouse://127.0.0.1:9000")
-    val statement = connection.createStatement()
+    val statement = ClickhouseTest.connection.createStatement()
     val rs = statement.executeQuery("select (number % 3 + 1) as n, sum(number) from numbers(10000000) group by n")
     while(rs.next()){
       println(rs.getInt(1) + "\t" + rs.getLong(2))
     }
+  }
+
+  @Test
+  @DisplayName("testExecuteQuery")
+  def testExecuteQuery(): Unit = {
+
+  }
+}
+
+object ClickhouseTest{
+  var connection: Connection = null
+  @BeforeAll
+  def init(): Unit = {
+    connection = DriverManager.getConnection("jdbc:clickhouse://127.0.0.1:9000")
   }
 }
