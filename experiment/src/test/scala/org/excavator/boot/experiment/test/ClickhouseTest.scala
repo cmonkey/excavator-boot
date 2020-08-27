314ca971a6da9adb742f58b146455ee63741073a
CMonkey <42.codemonkey@gmail.com>
2020-08-27 15:20:41 +0800

    add testExecuteQuery

--- experiment/src/test/scala/org/excavator/boot/experiment/test/ClickhouseTest.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/ClickhouseTest.scala
@@ -20,6 +20,35 @@ class ClickhouseTest {
   @DisplayName("testExecuteQuery")
   def testExecuteQuery(): Unit = {
 
+    val stmt = ClickhouseTest.connection.createStatement()
+    stmt.executeQuery("drop table if exists test_example")
+    stmt.executeQuery("create table test_example(day default toDate(toDateTime(timestamp)), timestamp UInt64, name String, impressions UInt32) Engine=MergeTree(day, (timestamp, name), 8179)")
+    stmt.executeQuery("insert into test_example(timestamp, name, impressions) values(1598511393098, 'cmonkey', 200)")
+    stmt.executeQuery("alter table test_example add column custs Float32")
+
+    val pstmt = ClickhouseTest.connection.prepareStatement("insert into test_example(timestamp , name, impressions, custs) values(?, ?, ?,?)")
+
+    var batchSize = 0
+    for(i <- 0 until 10000){
+      val timestamp = System.currentTimeMillis()
+      val name = "cmonkey-" + i
+      val impressions = i
+      val custs = i.toFloat
+      pstmt.setLong(1, timestamp)
+      pstmt.setString(2, name)
+      pstmt.setInt(3, impressions)
+      pstmt.setFloat(4, custs)
+
+      pstmt.addBatch()
+      if(batchSize == 10000){
+        pstmt.executeBatch()
+        batchSize = 0
+      }
+
+      batchSize += 1
+    }
+
+    pstmt.executeBatch()
   }
 }
 
