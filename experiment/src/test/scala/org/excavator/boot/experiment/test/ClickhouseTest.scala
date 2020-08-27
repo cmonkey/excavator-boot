aac76cd0da9bf8b07a1c7d554ee2b60a0c045fc7
CMonkey <42.codemonkey@gmail.com>
2020-08-27 14:45:00 +0800

    add testSimpleQuery function

--- experiment/src/test/scala/org/excavator/boot/experiment/test/ClickhouseTest.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/ClickhouseTest.scala
@@ -1,5 +1,20 @@
 package org.excavator.boot.experiment.test
 
+import java.sql.DriverManager
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
 class ClickhouseTest {
 
+  @Test
+  @DisplayName("testSimpleQuery")
+  def testSimpleQuery(): Unit = {
+    val connection = DriverManager.getConnection("jdbc:clickhouse://127.0.0.1:9000")
+    val statement = connection.createStatement()
+    val rs = statement.executeQuery("select (number % 3 + 1) as n, sum(number) from numbers(10000000) group by n")
+    while(rs.next()){
+      println(rs.getInt(1) + "\t" + rs.getLong(2))
+    }
+  }
 }
