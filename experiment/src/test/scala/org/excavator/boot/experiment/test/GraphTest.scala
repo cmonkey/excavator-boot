eac97f95e7f5745cefb0102d5d71be46cedc4df9
CMonkey <42.codemonkey@gmail.com>
2020-09-03 15:55:34 +0800

    add testDirectedAcyclicGraph

--- experiment/src/test/scala/org/excavator/boot/experiment/test/GraphTest.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/GraphTest.scala
@@ -1,6 +1,6 @@
 package org.excavator.boot.experiment.test
 
-import org.excavator.boot.experiment.directed.{Graph, Node}
+import org.excavator.boot.experiment.directed.{DirectedAcyclicGraph, Graph, Node}
 import org.junit.jupiter.api.{DisplayName, Test}
 
 class GraphTest {
@@ -23,6 +23,27 @@ class GraphTest {
     graph.addNode(node3)
 
     println(graph)
+  }
+
+  @Test
+  @DisplayName("directed acyclic graph")
+  def testDirectedAcyclicGraph(): Unit = {
+    val graph = new DirectedAcyclicGraph
+    val node1 = Node(1)
+    val node2 = Node(2)
+    val node3 = Node(3)
+
+    node1.addNeighbors(2)
+    node2.addNeighbors(3)
+    node3.addNeighbors(4)
+
+    graph.addEdge(node1, node2)
+    graph.addEdge(node2, node3)
+
+    println(s"node1 children = ${graph.getChildren(node1)}")
+
+    println(s"sources = ${graph.getSources()}")
+    println(s"sinks = ${graph.getSinks()}")
   }
 
 }
