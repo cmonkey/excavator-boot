83c02066e2059d0fd7c9de97e773c59966997372
CMonkey <42.codemonkey@gmail.com>
2020-09-03 16:16:35 +0800

    override toString to Node

--- experiment/src/main/scala/org/excavator/boot/experiment/directed/Node.scala
+++ experiment/src/main/scala/org/excavator/boot/experiment/directed/Node.scala
@@ -2,6 +2,8 @@ package org.excavator.boot.experiment.directed
 
 import java.util
 
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder
+
 import scala.beans.BeanProperty
 
 case class Node(@BeanProperty id: Int){
@@ -11,4 +13,6 @@ case class Node(@BeanProperty id: Int){
   def addNeighbors(e: Int) = {
     neighbors.add(e)
   }
+
+  override def toString: String = ReflectionToStringBuilder.toString(this)
 }
