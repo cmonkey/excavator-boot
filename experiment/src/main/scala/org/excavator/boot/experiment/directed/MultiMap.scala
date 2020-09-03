498c3ef31ba576b54749c65e050ce60dc6eabdce
CMonkey <42.codemonkey@gmail.com>
2020-09-03 15:18:27 +0800

    add pub

--- experiment/src/main/scala/org/excavator/boot/experiment/directed/MultiMap.scala
+++ experiment/src/main/scala/org/excavator/boot/experiment/directed/MultiMap.scala
@@ -1,5 +1,22 @@
 package org.excavator.boot.experiment.directed
 
+import scala.collection.mutable
+
 class MultiMap {
+
+  // not key, but not null values
+  val map = new mutable.LinkedHashMap[Any, Any]
+
+  def pub(key: Any, value: Any) = {
+    var set = map.get(key).asInstanceOf[mutable.LinkedHashSet[Any]]
+    if(set.isEmpty){
+      set = new mutable.LinkedHashSet[Any]
+      map.put(key, set)
+    }
+
+    if(null != value){
+      set.add(value)
+    }
+  }
 
 }
