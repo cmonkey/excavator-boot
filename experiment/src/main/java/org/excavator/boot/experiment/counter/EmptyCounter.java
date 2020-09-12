a600b1200f59ad234df253be925a9ba32348a1c6
CMonkey <42.codemonkey@gmail.com>
2020-09-12 22:18:01 +0800

    add override methods to EmptyCounter

--- experiment/src/main/java/org/excavator/boot/experiment/counter/EmptyCounter.java
+++ experiment/src/main/java/org/excavator/boot/experiment/counter/EmptyCounter.java
@@ -1,2 +1,13 @@
-package org.excavator.boot.experiment.counter;public class EmptyCounter {
+package org.excavator.boot.experiment.counter;
+
+public class EmptyCounter extends ServerCounter{
+    @Override
+    public long getAndReset() {
+        return 0;
+    }
+
+    @Override
+    public void add(long increment) {
+
+    }
 }
