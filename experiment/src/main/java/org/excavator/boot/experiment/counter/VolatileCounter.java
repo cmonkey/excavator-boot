1b7bd7e9a069795c655c8b3b383406548c475626
CMonkey <42.codemonkey@gmail.com>
2020-09-12 22:21:02 +0800

    add override methods to VolatileCounter

--- experiment/src/main/java/org/excavator/boot/experiment/counter/VolatileCounter.java
+++ experiment/src/main/java/org/excavator/boot/experiment/counter/VolatileCounter.java
@@ -1,4 +1,16 @@
 package org.excavator.boot.experiment.counter;
 
-public class VolatileCounter {
+public class VolatileCounter extends ServerCounter{
+    private volatile long count = 0;
+    @Override
+    public long getAndReset() {
+        long result = count;
+        count = 0;
+        return result;
+    }
+
+    @Override
+    public void add(long increment) {
+        count += increment;
+    }
 }
