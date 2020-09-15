b465206e9ce6dd62dc98fcd009735a85ea627ffe
CMonkey <42.codemonkey@gmail.com>
2020-09-15 13:26:22 +0800

    add dountStock function

--- experiment/src/main/scala/org/excavator/boot/experiment/future/TutorialFuture.scala
+++ experiment/src/main/scala/org/excavator/boot/experiment/future/TutorialFuture.scala
@@ -1,5 +1,14 @@
 package org.excavator.boot.experiment.future
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
 class TutorialFuture {
+
+  def dountStock(donut:String):Future[Int] = Future {
+    // assume some long running database operation
+    println("checking donut stock")
+    10
+  }
 
 }
