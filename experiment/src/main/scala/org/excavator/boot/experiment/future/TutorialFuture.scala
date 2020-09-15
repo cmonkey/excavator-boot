e2b361ee97e256da0357bd626133659eee0f2bfc
CMonkey <42.codemonkey@gmail.com>
2020-09-15 13:44:22 +0800

    add buyDonuts

--- experiment/src/main/scala/org/excavator/boot/experiment/future/TutorialFuture.scala
+++ experiment/src/main/scala/org/excavator/boot/experiment/future/TutorialFuture.scala
@@ -2,13 +2,40 @@ package org.excavator.boot.experiment.future
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
 
 class TutorialFuture {
 
-  def dountStock(donut:String):Future[Int] = Future {
+  def dountStock(donut:String):Future[Option[Int]] = Future {
     // assume some long running database operation
     println("checking donut stock")
-    10
+    if(donut == "vanilla donut") Some(10) else None
   }
+
+  val vanillaDonutStock = Await.result(dountStock("vanilla donut"), 3.seconds)
+  println(s"Stock of vanila donut = $vanillaDonutStock")
+
+  dountStock("vanilla donut").onComplete({
+    case Success(value) => println(s"Stock fro vanilla donut $value")
+    case Failure(exception) => println(s"Failed to find vanilla donut stock, exception = $exception")
+  })
+
+  def buyDonuts(quantity:Int): Future[Boolean] = Future {
+    println(s"buying $quantity donuts")
+    if(quantity > 0)true else false
+  }
+
+  val buyingDonuts: Future[Boolean] = dountStock("plain donut").flatMap(qty => buyDonuts(qty))
+
+  val isSuccess = Await.result(buyingDonuts, 5.seconds)
+
+  for{
+    stock <- dountStock("vanilla donut")
+    isSuccess <- buyDonuts(stock.getOrElse(0))
+  }yield println(s" Buying vanilla donut was successful = $isSuccess")
+
+  dountStock("vanilla donut").map(someQty => println(s"Buying ${someQty.getOrElse(0)} vanilla donuts"))
 
 }
