aafcf53c5dea83e249fa94818276267c9486d07d
CMonkey <42.codemonkey@gmail.com>
2020-09-17 14:35:47 +0800

    add theNaiveWay function

--- experiment/src/main/scala/org/excavator/boot/experiment/monix/ParallelProcessing.scala
+++ experiment/src/main/scala/org/excavator/boot/experiment/monix/ParallelProcessing.scala
@@ -1,5 +1,18 @@
 package org.excavator.boot.experiment.monix
+import monix.execution.Scheduler.Implicits.global
+import monix.eval._
+import monix.reactive._
 
 class ParallelProcessing {
+
+  def theNaiveWay(): Unit ={
+    val items = 0 until 1000
+
+    val tasks = items.map(i => Task(i * 2))
+
+    val aggregate = Task.parSequence(tasks).map(_.toList)
+
+    aggregate.foreach(println)
+  }
 
 }
