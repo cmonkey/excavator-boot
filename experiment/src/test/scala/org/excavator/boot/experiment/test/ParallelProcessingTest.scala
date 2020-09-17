e3050b7041050ef26f6242beedb96348cd91c63c
CMonkey <42.codemonkey@gmail.com>
2020-09-17 14:49:04 +0800

    add ParallelProcessingTest

---
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/ParallelProcessingTest.scala
@@ -0,0 +1,20 @@
+package org.excavator.boot.experiment.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParallelProcessingTest {
+
+    @Test
+    void theNaiveWay() {
+    }
+
+    @Test
+    void imposingAParallelismLimit() {
+    }
+
+    @Test
+    void batchedObservables() {
+    }
+}
