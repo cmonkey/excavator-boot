a43fc0660807fad801ca3b28b132297ad3c19f8e
CMonkey <42.codemonkey@gmail.com>
2020-12-27 16:45:03 +0800

    add mehtod return type is Unit

--- cake-pattern/src/main/scala/org/excavator/boot/cake/pattern/dao/UserRepository.scala
+++ cake-pattern/src/main/scala/org/excavator/boot/cake/pattern/dao/UserRepository.scala
@@ -4,11 +4,11 @@ import org.excavator.boot.cake.pattern.domain.User
 
 trait UserRepository {
 
-  def create(user: User)
+  def create(user: User):Unit
 
   def find(name:String): Option[User]
 
-  def update(user: User)
+  def update(user: User):Unit
 
-  def delete(user: User)
+  def delete(user: User):Unit
 }
