0668e9c7d19123f6f9cf63d7d43b871732fb28a6
CMonkey <42.codemonkey@gmail.com>
2020-12-31 09:52:49 +0800

    add testConvertingCheckedIntoRuntimeExceptions method

--- experiment/src/test/java/org/excavator/boot/experiment/counter/test/ExceptionsAndStreamsTest.java
+++ experiment/src/test/java/org/excavator/boot/experiment/counter/test/ExceptionsAndStreamsTest.java
@@ -1,2 +1,17 @@
-package org.excavator.boot.experiment.counter.test;public class ExceptionsAndStreamsTest {
+package org.excavator.boot.experiment.counter.test;
+
+import org.excavator.boot.experiment.stream.ExceptionsAndStreams;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ExceptionsAndStreamsTest {
+    @Test
+    @DisplayName("testConvertingCheckedIntoRuntimeExceptions")
+    public void testConvertingCheckedIntoRuntimeExceptions(){
+        var exceptionsAndStreams = new ExceptionsAndStreams();
+        assertThrows(RuntimeException.class, () -> {
+            exceptionsAndStreams.convertingCheckedIntoRuntimeExceptions();
+        });
+    }
 }
