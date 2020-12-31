480ef09bdda8d09f1e59af4a23f5071edea80a5e
CMonkey <42.codemonkey@gmail.com>
2020-12-31 09:36:45 +0800

    add convertingCheckedIntoRuntimeException method

--- experiment/src/main/java/org/excavator/boot/experiment/stream/ExceptionsAndStreams.java
+++ experiment/src/main/java/org/excavator/boot/experiment/stream/ExceptionsAndStreams.java
@@ -1,2 +1,22 @@
-package org.excavator.boot.experiment.stream;public class ExceptionsAndStreams {
+package org.excavator.boot.experiment.stream;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+
+public class ExceptionsAndStreams {
+
+    public void convertingCheckedIntoRuntimeExceptions(){
+        var format = new SimpleDateFormat("yyyy-MM-dd") ;
+        var dateList = asList("2020-10-11", "2020-nov-12", "2020-12-01");
+        var dates = dateList.stream().map(s -> {
+            try{
+                return format.parse(s);
+            }catch(ParseException e){
+                throw new RuntimeException(e);
+            }
+        }).collect(toList());
+    }
 }
