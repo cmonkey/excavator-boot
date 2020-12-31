ca2ec4ccf582923152dabbb05c742ebc5d96bfc9
CMonkey <42.codemonkey@gmail.com>
2020-12-31 09:43:47 +0800

    call wrapAsRuntime method to convertingCheckedIntoRuntimeExceptions

--- experiment/src/main/java/org/excavator/boot/experiment/stream/ExceptionsAndStreams.java
+++ experiment/src/main/java/org/excavator/boot/experiment/stream/ExceptionsAndStreams.java
@@ -14,7 +14,10 @@ public class ExceptionsAndStreams {
     public void convertingCheckedIntoRuntimeExceptions(){
         var format = new SimpleDateFormat("yyyy-MM-dd") ;
         var dateList = asList("2020-10-11", "2020-nov-12", "2020-12-01");
-        var dates = dateList.stream().map(s -> uglyParse(format, s)).collect(toList());
+        //ThrowingFunction<String, Date> p = s -> format.parse(s);
+        ThrowingFunction<String, Date> p = format::parse;
+        var f = wrapAsRuntime(p);
+        var dates = dateList.stream().map(f).collect(toList());
     }
     @SneakyThrows
     private static Date uglyParse(SimpleDateFormat format, String s){
