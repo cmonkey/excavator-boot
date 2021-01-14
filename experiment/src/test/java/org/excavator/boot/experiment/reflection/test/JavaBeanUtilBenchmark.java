c60978f1ee9d18e479433418a39e54106ff4fddd
CMonkey <42.codemonkey@gmail.com>
2021-01-14 10:05:29 +0800

    change Param ref

--- experiment/src/test/java/org/excavator/boot/experiment/reflection/test/JavaBeanUtilBenchmark.java
+++ experiment/src/test/java/org/excavator/boot/experiment/reflection/test/JavaBeanUtilBenchmark.java
@@ -1,6 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.excavator.boot.experiment.reflection.test;
 
-import io.lettuce.core.dynamic.annotation.Param;
 import org.openjdk.jmh.annotations.*;
 
 import java.beans.JavaBean;
@@ -14,14 +29,12 @@ import java.util.concurrent.TimeUnit;
 @State(Scope.Thread)
 public class JavaBeanUtilBenchmark {
 
-    @Param({"fieldA",
-    "nestedJavaBean.fieldA",
-    "nestedJavaBean.nestedJavaBean.fieldA",
-    "nestedJavaBean.nestedJavaBean.nestedJavaBean.fieldA"})
-    String fieldName;
+    @Param({ "fieldA", "nestedJavaBean.fieldA", "nestedJavaBean.nestedJavaBean.fieldA",
+            "nestedJavaBean.nestedJavaBean.nestedJavaBean.fieldA" })
+    String   fieldName;
     JavaBean javaBean;
 
     @Setup
-    public void setup(){
+    public void setup() {
     }
 }
