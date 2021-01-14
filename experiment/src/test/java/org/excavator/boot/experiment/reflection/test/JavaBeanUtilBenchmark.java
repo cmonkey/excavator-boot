c2ae15b6ba894ef6570bc00070ef15b304b15e29
CMonkey <42.codemonkey@gmail.com>
2021-01-14 10:00:15 +0800

    add setup method to the JavaBeanUtilBenchmark

--- experiment/src/test/java/org/excavator/boot/experiment/reflection/test/JavaBeanUtilBenchmark.java
+++ experiment/src/test/java/org/excavator/boot/experiment/reflection/test/JavaBeanUtilBenchmark.java
@@ -1,2 +1,27 @@
-package org.excavator.boot.experiment.reflection.test;public class JavaBeanUtilBenchmark {
+package org.excavator.boot.experiment.reflection.test;
+
+import io.lettuce.core.dynamic.annotation.Param;
+import org.openjdk.jmh.annotations.*;
+
+import java.beans.JavaBean;
+import java.util.concurrent.TimeUnit;
+
+@Fork(3)
+@Warmup(iterations = 5, time = 3)
+@Measurement(iterations = 5, time = 1)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@State(Scope.Thread)
+public class JavaBeanUtilBenchmark {
+
+    @Param({"fieldA",
+    "nestedJavaBean.fieldA",
+    "nestedJavaBean.nestedJavaBean.fieldA",
+    "nestedJavaBean.nestedJavaBean.nestedJavaBean.fieldA"})
+    String fieldName;
+    JavaBean javaBean;
+
+    @Setup
+    public void setup(){
+    }
 }
