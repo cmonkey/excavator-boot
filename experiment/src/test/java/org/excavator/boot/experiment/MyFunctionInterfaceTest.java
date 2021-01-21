80fdc8a682d31b920ab401ec342fd0b09d2589e0
CMonkey <42.codemonkey@gmail.com>
2021-01-21 09:49:43 +0800

    add testMyFunctionInterface method to the MyFunctionInterfaceTest

--- experiment/src/test/java/org/excavator/boot/experiment/MyFunctionInterfaceTest.java
+++ experiment/src/test/java/org/excavator/boot/experiment/MyFunctionInterfaceTest.java
@@ -1,2 +1,22 @@
-package org.excavator.boot.experiment;public class MyFunctionInterfaceTest {
+package org.excavator.boot.experiment;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyFunctionInterfaceTest {
+
+    @Test
+    @DisplayName("testMyFunctionInterface")
+    public void testMyFunctionInterface(){
+
+        MyFunctionInterface<String> functionInterface= s -> s.toUpperCase(Locale.getDefault());
+
+        var str = "aaa";
+        var r = functionInterface.getValue(str);
+        assertEquals("AAA", r);
+    }
 }
