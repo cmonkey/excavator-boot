94b74c86a59be3609eef2dc1274d67a06ac5ae5f
CMonkey <42.codemonkey@gmail.com>
2021-02-04 16:42:52 +0800

    add printDefaults method

--- experiment/src/main/java/org/excavator/boot/experiment/stream/CharBufferApp.java
+++ experiment/src/main/java/org/excavator/boot/experiment/stream/CharBufferApp.java
@@ -1,4 +1,11 @@
 package org.excavator.boot.experiment.stream;
 
+import java.nio.charset.Charset;
+
 public class CharBufferApp {
+
+    public static void printDefaults(){
+        System.out.printf("Default CharacterSet: %s%n", Charset.defaultCharset().name());
+        System.out.printf("Character Bytes: %s%n", Character.BYTES);
+    }
 }
