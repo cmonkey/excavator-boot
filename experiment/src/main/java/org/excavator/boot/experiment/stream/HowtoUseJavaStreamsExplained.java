8c10451aa24eba8ee81fd4ef16277a8af333c053
CMonkey <42.codemonkey@gmail.com>
2021-02-04 16:31:50 +0800

    foramt

--- experiment/src/main/java/org/excavator/boot/experiment/stream/HowtoUseJavaStreamsExplained.java
+++ experiment/src/main/java/org/excavator/boot/experiment/stream/HowtoUseJavaStreamsExplained.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.excavator.boot.experiment.stream;
 
 import java.util.*;
@@ -14,17 +30,20 @@ public class HowtoUseJavaStreamsExplained {
                 .reduce(0, Integer::sum);
     }
 
-    protected static Stream<Map.Entry<Integer, Integer>> example2(){
-        List<Integer> arrayList = new ArrayList<>(Arrays.asList(1,2,3));
+    protected static Stream<Map.Entry<Integer, Integer>> example2() {
+        List<Integer> arrayList = new ArrayList<>(Arrays.asList(1, 2, 3));
         Stream<Integer> intStream = arrayList.stream();
 
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>(){{
-            put(1, 1);
-            put(2, 2);
-            put(3, 3);
-        }};
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>() {
+            {
+                put(1, 1);
+                put(2, 2);
+                put(3, 3);
+            }
+        };
 
-        Stream<Map.Entry<Integer, Integer>> mapStream = map.entrySet().stream();;
+        Stream<Map.Entry<Integer, Integer>> mapStream = map.entrySet().stream();
+        ;
 
         return mapStream;
     }
