00af33c784c7e440bf709314bf5f75261f0c2c37
CMonkey <42.codemonkey@gmail.com>
2021-05-19 15:05:27 +0800

    add testLocalDate function

--- experiment/src/test/scala/org/excavator/boot/experiment/test/DateTimeApiTest.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/DateTimeApiTest.scala
@@ -1,5 +1,20 @@
 package org.excavator.boot.experiment.test
 
+import org.junit.jupiter.api.{DisplayName, Test}
+
+import java.time.{LocalDate, Month}
+
 class DateTimeApiTest {
+
+  @Test
+  @DisplayName("test localDate")
+  def testLocalDate(): Unit = {
+    val localDate = LocalDate.now()
+    val localDateOf = LocalDate.of(2019, 2,15)
+    val localDateOf2 = LocalDate.of(2019, Month.FEBRUARY, 15)
+    println(s"localDate = ${localDate}")
+    println(s"localDateOf = ${localDateOf}")
+    println(s"localDateOf2 = ${localDateOf2}")
+  }
 
 }
