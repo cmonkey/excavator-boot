eae0fb5633cea88ae36fcd01169575b28fc66d4c
CMonkey <42.codemonkey@gmail.com>
2021-05-19 15:08:58 +0800

    add testLocalTime function

--- experiment/src/test/scala/org/excavator/boot/experiment/test/DateTimeApiTest.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/DateTimeApiTest.scala
@@ -2,7 +2,7 @@ package org.excavator.boot.experiment.test
 
 import org.junit.jupiter.api.{DisplayName, Test}
 
-import java.time.{LocalDate, Month}
+import java.time.{LocalDate, Month, LocalTime}
 
 class DateTimeApiTest {
 
@@ -15,6 +15,21 @@ class DateTimeApiTest {
     println(s"localDate = ${localDate}")
     println(s"localDateOf = ${localDateOf}")
     println(s"localDateOf2 = ${localDateOf2}")
+  }
+
+  @Test
+  @DisplayName("test LocalTime")
+  def testLocalTime():Unit = {
+    val localTimeNow = LocalTime.now()
+    val localTimeOf = LocalTime.of(15, 12, 16)
+    val localTimeOf2 = localTimeOf.plusMinutes(33).minusHours(2)
+
+    val localTimeParsed = LocalTime.parse("21:55")
+
+    println(s"localTimeNow = ${localTimeNow}")
+    println(s"localTimeNow = ${localTimeOf}")
+    println(s"localTimeNow = ${localTimeOf2}")
+    println(s"localTimeNow = ${localTimeParsed}")
   }
 
 }
