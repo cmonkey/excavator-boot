0b84d92e3a4ea03aeab03c673c0083733c7f5b0e
CMonkey <42.codemonkey@gmail.com>
2021-07-02 16:34:17 +0800

    add genericeDbCall to AsyncIO

--- experiment/src/main/scala/org/excavator/boot/experiment/scaling/AsyncIO.scala
+++ experiment/src/main/scala/org/excavator/boot/experiment/scaling/AsyncIO.scala
@@ -2,4 +2,23 @@ package org.excavator.boot.experiment.scaling
 
 class AsyncIO {
 
+  private var dbCallCount: Int = 0
+
+  def genericeDbCall(seconds:Int) = {
+    Thread.sleep(seconds * 1000)
+    dbCallCount += 1
+    println(s"Completed db call ${dbCallCount}")
+  }
+
+  def dbCall1() = {
+    genericeDbCall(1)
+  }
+
+  def dbCall2() = {
+    genericeDbCall(2)
+  }
+
+  def dbCall3() = {
+    genericeDbCall(3)
+  }
 }
