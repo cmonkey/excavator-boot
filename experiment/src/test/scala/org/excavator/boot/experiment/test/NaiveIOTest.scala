ea061b3f774d780e54c7d4d9460a261a550c7293
CMonkey <42.codemonkey@gmail.com>
2021-07-02 16:24:49 +0800

    add testNaiveIO function

--- experiment/src/test/scala/org/excavator/boot/experiment/test/NaiveIOTest.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/NaiveIOTest.scala
@@ -1,5 +1,22 @@
 package org.excavator.boot.experiment.test
 
-class NaiveIOTest {
+import org.excavator.boot.experiment.scaling.NaiveIO
+import org.junit.jupiter.api.{DisplayName, Test}
+
+object NaiveIOTest {
+
+  @Test
+  @DisplayName("test naiveIO")
+  def testNaiveIO(): Unit = {
+    val startTime = System.currentTimeMillis()
+    val naiveIO = new NaiveIO
+    for(i <- 0 until 5){
+      naiveIO.dbCall1()
+      naiveIO.dbCall2()
+      naiveIO.dbCall3()
+    }
+
+    println(s"complete IO calls in ${System.currentTimeMillis() - startTime} ms")
+  }
 
 }
