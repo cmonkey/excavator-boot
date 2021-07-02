d30a29217bd8031557c64c808f78ca92319e5c91
CMonkey <42.codemonkey@gmail.com>
2021-07-02 17:29:53 +0800

    add SupplierApp

---
+++ experiment/src/test/java/org/excavator/boot/experiment/SupplierApp.java
@@ -0,0 +1,12 @@
+package org.excavator.boot.experiment;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class SupplierApp {
+    public static void main(String[] args) {
+        Supplier<String> cache = () -> "From Database";
+        Optional<String> query = Optional.empty();
+        System.out.println(query.orElseGet(cache));
+    }
+}
