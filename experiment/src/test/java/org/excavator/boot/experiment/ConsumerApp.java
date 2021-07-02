421f499beb7a488d8ca5585006d4d86eeb1d7441
CMonkey <42.codemonkey@gmail.com>
2021-07-02 17:28:50 +0800

    add ConsumerApp

---
+++ experiment/src/test/java/org/excavator/boot/experiment/ConsumerApp.java
@@ -0,0 +1,12 @@
+package org.excavator.boot.experiment;
+
+import java.util.function.Consumer;
+
+public class ConsumerApp {
+    public static void main(String[] args) {
+        Consumer<String> log = s -> System.out.println("The log " + s);
+        Consumer<String> logB = s -> System.out.println("The logB " + s);
+
+        log.andThen(logB).accept("The value A");
+    }
+}
