a92eefa989431b467c988db4a28361cb569e38d7
CMonkey <42.codemonkey@gmail.com>
2021-09-08 09:25:15 +0800

    change display name

--- experiment/src/test/scala/org/excavator/boot/experiment/test/SpringVersionTest.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/SpringVersionTest.scala
@@ -2,22 +2,23 @@ package org.excavator.boot.experiment.test
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.{DisplayName, Test}
+import org.springframework.boot.SpringBootVersion
 import org.springframework.core.SpringVersion
 
 class SpringVersionTest {
 
   @Test
-  @DisplayName("test print spring version")
-  def testPrintSpringVersion(): Unit = {
+  @DisplayName("test  spring version")
+  def testSpringVersion(): Unit = {
     val version = SpringVersion.getVersion
     assertEquals("5.3.9", version)
   }
 
   @Test
-  @DisplayName("test pring spring boot version")
+  @DisplayName("test spring boot version")
   def testSpringBootVersion():Unit = {
     val version = SpringBootVersion.getVersion
-    assertEquals("2.5.6", version)
+    assertEquals("2.5.4", version)
   }
 
 }
