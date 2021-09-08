d33850251e6747e9cdb44c24449600236543ce17
CMonkey <42.codemonkey@gmail.com>
2021-09-08 09:20:22 +0800

    init testPrintSpringVersion function

--- experiment/src/test/scala/org/excavator/boot/experiment/test/SpringVersionTest.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/SpringVersionTest.scala
@@ -1,5 +1,13 @@
 package org.excavator.boot.experiment.test
 
+import org.junit.jupiter.api.{DisplayName, Test}
+
 class SpringVersionTest {
+
+  @Test
+  @DisplayName("test print spring version")
+  def testPrintSpringVersion(): Unit = {
+
+  }
 
 }
