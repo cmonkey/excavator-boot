47d774acab7bd72b9042dd3e5b258f5c8e81e718
CMonkey <42.codemonkey@gmail.com>
2021-09-08 09:22:50 +0800

    implemenet: test spring version

--- experiment/src/test/scala/org/excavator/boot/experiment/test/SpringVersionTest.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/SpringVersionTest.scala
@@ -1,13 +1,16 @@
 package org.excavator.boot.experiment.test
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.{DisplayName, Test}
+import org.springframework.core.SpringVersion
 
 class SpringVersionTest {
 
   @Test
   @DisplayName("test print spring version")
   def testPrintSpringVersion(): Unit = {
-
+    val version = SpringVersion.getVersion
+    assertEquals("5.3.9", version)
   }
 
 }
