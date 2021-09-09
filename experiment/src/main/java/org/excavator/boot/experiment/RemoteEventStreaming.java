9c37371757866850a3fd62889e0128194194d1b5
CMonkey <42.codemonkey@gmail.com>
2021-09-09 09:38:27 +0800

    add RemoteEventStreaming

--- experiment/src/main/java/org/excavator/boot/experiment/RemoteEventStreaming.java
+++ experiment/src/main/java/org/excavator/boot/experiment/RemoteEventStreaming.java
@@ -16,64 +16,31 @@
  */
 package org.excavator.boot.experiment;
 
-import jdk.jfr.consumer.EventStream;
-import jdk.jfr.consumer.RecordingStream;
 import jdk.management.jfr.RemoteRecordingStream;
 
+import javax.management.MBeanServerConnection;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 import java.io.IOException;
-import java.nio.file.Path;
+import java.time.Duration;
 
-public class RemoteEvnetStreaming {
-    public static void main(String[] args) {
-        // Passive, in process
-        try(var stream = EventStream.openRepository()){
-            stream.onEvent("jdk.JavaMonitorEnter", System.out::println);
-            stream.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Passive, out of process
-        var path = Path.of("/tmp/2021-09-09_60185");
-        try(var stream = EventStream.openRepository(path)){
-            stream.onEvent("jdk.JavaMonitorEnter", System.out::println);
-            stream.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Active, in process
-        try(var stream = new RecordingStream()){
-            stream.enable("jdk.JavaMonitorEnter").withStackTrace();
-            stream.onEvent("jdk.JavaMonitorEnter", System.out::println);
-            stream.start();
-        }
-
-        // Active, out of process (and over the network)
-
-        remoteRecordStream();
+public class RemoteEventStreaming {
+    public static void main(String[] args) throws IOException {
+        var connection = getMBeanServerConnection("myhost:1099");
+        var stream = new RemoteRecordingStream(connection);
+        stream.enable("jdk.CPULoad").withPeriod(Duration.ofSeconds(1));
+        stream.onEvent("jdk.CPULoad", event -> {
+            System.out.println("Total       : " + event.getDouble("machineTotal"));
+            System.out.println("JVM System  : " + event.getDouble("jvmSystem"));
+            System.out.println("JVM User    : " + event.getDouble("jvmUser"));
+        });
+        stream.startAsync();
     }
 
-    private static void remoteRecordStream(){
-        // Active, out of process (and over the network)
-        var host = "com.example";
-        var port = 7091;
-        var url = "service:jmx:rmi:///jndi/rmi://" + host + ":" + port + "/jmxrmi";
-
-        try {
-            var u = new JMXServiceURL(url);
-            var c = JMXConnectorFactory.connect(u);
-            var connection = c.getMBeanServerConnection();
-
-            try (var stream = new RemoteRecordingStream(connection)) {
-                stream.enable("jdk.JavaMonitorEnter").withStackTrace();
-                stream.onEvent("jdk.JavaMonitorEnter", System.out::println);
-                stream.start();
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+    private static MBeanServerConnection getMBeanServerConnection(String url) throws IOException {
+        var u = new JMXServiceURL(url);
+        var c = JMXConnectorFactory.connect(u);
+        var connection = c.getMBeanServerConnection();
+        return connection;
     }
 }
