4d48344d975ba1cd76019ddc254aa70f7afc30fa
CMonkey <42.codemonkey@gmail.com>
2021-09-09 09:42:13 +0800

    implement: test NPE

--- experiment/src/main/java/org/excavator/boot/experiment/HelpfulNpes.java
+++ experiment/src/main/java/org/excavator/boot/experiment/HelpfulNpes.java
@@ -1,2 +1,23 @@
-package org.excavator.boot.experiment;public class HelpfulNpes {
+package org.excavator.boot.experiment;
+
+public class HelpfulNpes {
+
+    public static void main(String[] args) {
+
+        var countryName = new Customer().address.country.name; // cannot read field "country" because "address" is null
+                                                                       // 无法读取字段country, 因为address 是空
+        System.out.println(countryName);
+    }
+
+    public static class Customer{
+        public Address address;
+    }
+
+    public static class Address{
+        public Country country;
+    }
+
+    public static class Country{
+        public String name;
+    }
 }
