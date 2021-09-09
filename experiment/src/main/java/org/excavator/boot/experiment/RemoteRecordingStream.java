4c4a6dd60e900e684bf3d9030ae3b3a7939886ce
CMonkey <42.codemonkey@gmail.com>
2021-09-09 09:17:05 +0800

    add jdk javaMonitorEnter to RemoteRecordingStream

--- experiment/src/main/java/org/excavator/boot/experiment/RemoteRecordingStream.java
+++ experiment/src/main/java/org/excavator/boot/experiment/RemoteRecordingStream.java
@@ -1,4 +1,16 @@
 package org.excavator.boot.experiment;
 
+import jdk.jfr.consumer.EventStream;
+
+import java.io.IOException;
+
 public class RemoteRecordingStream {
+    public static void main(String[] args) {
+        try(var stream = EventStream.openRepository()){
+            stream.onEvent("jdk.JavaMonitorEnter", System.out::println);
+            stream.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
