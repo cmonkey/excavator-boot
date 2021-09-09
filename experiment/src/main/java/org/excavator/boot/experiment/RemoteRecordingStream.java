13850b4e276e8ed6a88c1fe51cb399f5ad5a9789
CMonkey <42.codemonkey@gmail.com>
2021-09-09 09:19:13 +0800

    add out of process

--- experiment/src/main/java/org/excavator/boot/experiment/RemoteRecordingStream.java
+++ experiment/src/main/java/org/excavator/boot/experiment/RemoteRecordingStream.java
@@ -3,11 +3,22 @@ package org.excavator.boot.experiment;
 import jdk.jfr.consumer.EventStream;
 
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class RemoteRecordingStream {
     public static void main(String[] args) {
+        // Passive, in process
         try(var stream = EventStream.openRepository()){
             stream.onEvent("jdk.JavaMonitorEnter", System.out::println);
+            stream.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Passive, out of process
+        var path = Path.of("/tmp/2021-09-09_60185");
+        try(var stream = EventStream.openRepository(path)){
+            stream.onEvent("Jdk.JavaMonitorEnter", System.out::println);
             stream.start();
         } catch (IOException e) {
             e.printStackTrace();
