6aeb0913cd1db426587f989d99d42916357e2f92
CMonkey <42.codemonkey@gmail.com>
2021-09-09 09:21:49 +0800

    active in process

--- experiment/src/main/java/org/excavator/boot/experiment/RemoteRecordingStream.java
+++ experiment/src/main/java/org/excavator/boot/experiment/RemoteRecordingStream.java
@@ -1,6 +1,7 @@
 package org.excavator.boot.experiment;
 
 import jdk.jfr.consumer.EventStream;
+import jdk.jfr.consumer.RecordingStream;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -18,10 +19,17 @@ public class RemoteRecordingStream {
         // Passive, out of process
         var path = Path.of("/tmp/2021-09-09_60185");
         try(var stream = EventStream.openRepository(path)){
-            stream.onEvent("Jdk.JavaMonitorEnter", System.out::println);
+            stream.onEvent("jdk.JavaMonitorEnter", System.out::println);
             stream.start();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        // Active, in process
+        try(var stream = new RecordingStream()){
+            stream.enable("jdk.JavaMonitorEnter").withStackTrace();
+            stream.onEvent("jdk.JavaMonitorEnter", System.out::println);
+            stream.start();
         }
     }
 }
