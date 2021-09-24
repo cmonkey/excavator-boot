529a1939651132e309beaf0659e914db0791debf
CMonkey <42.codemonkey@gmail.com>
2021-09-24 18:11:44 +0800

    add License and format method

--- experiment/src/main/java/org/excavator/boot/experiment/Amber.java
+++ experiment/src/main/java/org/excavator/boot/experiment/Amber.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.excavator.boot.experiment;
 
 import static java.lang.System.*;
@@ -9,5 +25,15 @@ public class Amber {
         out.println(format(21));
         out.println(format(21d));
         out.println(format(Long.MAX_VALUE));
+    }
+
+    static String format(Object o){
+        return switch (o){
+            case Integer i -> String.format("int %d", i);
+            case Long l -> String.format("long %d", l);
+            case Double d -> String.format("double %f", d);
+            case String s -> String.format("String %s", s);
+            default ->  o.toString();
+        };
     }
 }
