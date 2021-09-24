d25a109acb135ed8c773bd8e683054b0419f4808
CMonkey <42.codemonkey@gmail.com>
2021-09-24 17:49:28 +0800

    add out println call format method

--- experiment/src/main/java/org/excavator/boot/experiment/Amber.java
+++ experiment/src/main/java/org/excavator/boot/experiment/Amber.java
@@ -1,2 +1,13 @@
-package org.excavator.boot.experiment;public class Amber {
+package org.excavator.boot.experiment;
+
+import static java.lang.System.*;
+
+public class Amber {
+
+    public static void main(String[] args) {
+        out.println(format("Hello World"));
+        out.println(format(21));
+        out.println(format(21d));
+        out.println(format(Long.MAX_VALUE));
+    }
 }
