cdc4bf283fee19b57622b9be04e168483cbe0616
CMonkey <42.codemonkey@gmail.com>
2021-10-13 10:21:13 +0800

    add parse file and group record

--- experiment/src/test/java/org/excavator/boot/experiment/CollectorFile.java
+++ experiment/src/test/java/org/excavator/boot/experiment/CollectorFile.java
@@ -1,2 +1,20 @@
-package org.excavator.boot.experiment;public class CollectorFile {
+package org.excavator.boot.experiment;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public class CollectorFile {
+    public static void main(String[] args) throws IOException {
+        var filename = args[0];
+        var lines = Files.readAllLines(Paths.get(filename));
+        var map = lines.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+        var atomic = new AtomicInteger(0);
+        map.forEach((k, v) -> {
+            System.out.println(k+","+v);
+            atomic.incrementAndGet();
+        });
+    }
 }
