945199854d8ed230c51f0c4dd0054ec13ab960a6
CMonkey <42.codemonkey@gmail.com>
2021-10-29 10:20:11 +0800

    add doSomeMaths function

--- experiment/src/test/scala/org/excavator/boot/experiment/test/WithoutParams.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/WithoutParams.scala
@@ -1,5 +1,7 @@
 package org.excavator.boot.experiment.test
 
+import scala.math._
+
 class WithoutParams {
 
   def withoutParams(): Boolean = {
@@ -11,6 +13,14 @@ class WithoutParams {
     val y = if (a > b) 5 else 7
     y == 7
     y > 4
+  }
+
+  def doSomeMaths():Unit = {
+    val x = max(5, 2 * min(7, abs(-3)))
+    val y = sqrt(10)
+    if(!(x == 6 && y > 3.0)){
+      //
+    }
   }
 
 }
