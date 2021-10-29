ed5cee073584187b71276ac07041263749760345
CMonkey <42.codemonkey@gmail.com>
2021-10-29 10:35:00 +0800

    add License

--- experiment/src/test/java/org/excavator/boot/experiment/InterproceduralAnalysis.java
+++ experiment/src/test/java/org/excavator/boot/experiment/InterproceduralAnalysis.java
@@ -1,16 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.excavator.boot.experiment;
 
 public class InterproceduralAnalysis {
-    private int absoluteDifference(int x, int y){
-        if(y > x){
+    private int absoluteDifference(int x, int y) {
+        if (y > x) {
             return y - x;
-        }else{
+        } else {
             return x - y;
         }
     }
-    public void analyze(){
-        int x= absoluteDifference(12, 15) + absoluteDifference(9, 2);
-        if(x == 10){
+
+    public void analyze() {
+        int x = absoluteDifference(12, 15) + absoluteDifference(9, 2);
+        if (x == 10) {
             System.out.println(x);
         }
     }
