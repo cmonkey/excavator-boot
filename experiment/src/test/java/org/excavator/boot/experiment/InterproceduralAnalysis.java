d16f00f61ed9ec9609b87c9c6bff75551ad736f2
CMonkey <42.codemonkey@gmail.com>
2021-10-29 10:28:19 +0800

    add absoluteDifference/analyze methods to InterproceduralAnalysis

--- experiment/src/test/java/org/excavator/boot/experiment/InterproceduralAnalysis.java
+++ experiment/src/test/java/org/excavator/boot/experiment/InterproceduralAnalysis.java
@@ -1,2 +1,17 @@
-package org.excavator.boot.experiment;public class InterproceduralAnalysis {
+package org.excavator.boot.experiment;
+
+public class InterproceduralAnalysis {
+    private int absoluteDifference(int x, int y){
+        if(y > x){
+            return y - x;
+        }else{
+            return x - y;
+        }
+    }
+    public void analyze(){
+        int x= absoluteDifference(12, 15) + absoluteDifference(9, 2);
+        if(x == 10){
+            System.out.println(x);
+        }
+    }
 }
