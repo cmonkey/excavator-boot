b06165df10a1a493515944e45c1445947cb32c6d
CMonkey <42.codemonkey@gmail.com>
2021-10-29 10:30:00 +0800

    add absoluteDifference/analyze functions

--- experiment/src/test/scala/org/excavator/boot/experiment/test/InterproceduralAnalysis.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/InterproceduralAnalysis.scala
@@ -2,4 +2,15 @@ package org.excavator.boot.experiment.test
 
 class InterproceduralAnalysis {
 
+  private def absoluteDifference(x: Int, y:Int):Int = {
+    if(y > x) y - x else x - y
+  }
+
+  def analyze():Unit = {
+    val x = absoluteDifference(12, 15) + absoluteDifference(9, 2)
+    if (x == 10){
+      println(x)
+    }
+  }
+
 }
