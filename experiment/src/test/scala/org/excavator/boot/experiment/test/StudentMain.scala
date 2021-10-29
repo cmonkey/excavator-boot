5d343fccf459cbbe7590ce3165a5ce70d8a418f0
CMonkey <42.codemonkey@gmail.com>
2021-10-29 10:33:45 +0800

    add StudentMain

--- experiment/src/test/scala/org/excavator/boot/experiment/test/StudentMain.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/StudentMain.scala
@@ -1,5 +1,14 @@
 package org.excavator.boot.experiment.test
 
-class StudentMain {
+case class Student(age:Int, grades: List[Int])
 
+object StudentMain extends App{
+
+  val grades = List(3, 4, 1)
+  val student = Student(22, grades)
+  if(student.age < 20){
+    student.grades(5)
+  }else{
+    student.grades(4)
+  }
 }
