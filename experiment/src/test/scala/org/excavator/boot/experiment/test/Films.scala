4db186d52acac520715a3b24eac499dc744ca78f
CMonkey <42.codemonkey@gmail.com>
2021-10-29 10:25:36 +0800

    add sequences function

--- experiment/src/test/scala/org/excavator/boot/experiment/test/Films.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/Films.scala
@@ -15,4 +15,15 @@ object Films {
     else additionalFilms.head
   }
 
+  def sequences(f: Int => Boolean): Unit = {
+    val original = List(4, 6, 20, 55)
+    val filtered = original.filter(f)
+    original.size == 3 // always false, size is 4
+    filtered.size > 3 // unknown, size in range [0, 4]
+    filtered.size < 6 // always true, size in range [ 0, list.size] == [0, 4]
+    filtered == original // unknown, filtered can be original or its subsequence
+    original.map(x => 2 * x).size == 4
+    // original hs size 4, map doesn't change it 
+  }
+
 }
