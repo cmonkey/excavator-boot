494a55c9244675f1fd20d5695156a3c048506415
CMonkey <42.codemonkey@gmail.com>
2021-10-29 10:23:24 +0800

    add suggestFilm function

--- experiment/src/test/scala/org/excavator/boot/experiment/test/Films.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/Films.scala
@@ -1,5 +1,18 @@
 package org.excavator.boot.experiment.test
 
-class Films {
+object Films {
+
+  case class Film(title:String)
+
+  def currentUserRank = 7
+
+  def suggestFilm(suggestionSeed: Int):Film = {
+    val defaultFilms = List(Film("Godfather"), Film("The Room"), Film("Fargo"))
+    val additionalFilms = if (currentUserRank < 10) Nil else List(Film("Ran"), Film("Stalker"))
+
+    if(suggestionSeed % 3 == 0) defaultFilms(3) // IndexOutOfBoundsException
+    else if (suggestionSeed % 3 == 1) defaultFilms(2) // No problems
+    else additionalFilms.head
+  }
 
 }
