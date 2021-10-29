86ef8b77cb7ee032a108466972727c9c3ebae568
CMonkey <42.codemonkey@gmail.com>
2021-10-29 10:17:22 +0800

    add withoutParams functions

--- experiment/src/test/scala/org/excavator/boot/experiment/test/WithoutParams.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/WithoutParams.scala
@@ -2,4 +2,15 @@ package org.excavator.boot.experiment.test
 
 class WithoutParams {
 
+  def withoutParams(): Boolean = {
+    val x = if (2 > 3) 5 else 7
+    x == 7
+  }
+
+  def withParams(a:Int, b:Int):Boolean = {
+    val y = if (a > b) 5 else 7
+    y == 7
+    y > 4
+  }
+
 }
