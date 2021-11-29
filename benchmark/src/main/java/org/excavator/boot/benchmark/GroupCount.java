8638912b0238ad3e6007f1da26d41e7b8848abc1
CMonkey <42.codemonkey@gmail.com>
2021-11-29 18:10:25 +0800

    add fused/fissured methods

--- benchmark/src/main/java/org/excavator/boot/benchmark/GroupCount.java
+++ benchmark/src/main/java/org/excavator/boot/benchmark/GroupCount.java
@@ -1,4 +1,62 @@
 package org.excavator.boot.benchmark;
 
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.Arrays;
+import java.util.SplittableRandom;
+
+@State(Scope.Benchmark)
 public class GroupCount {
+
+    @Param({"8", "64", "128"})
+    int groups;
+
+    @Param({"256", "512", "1024", "2048", "4096"})
+    int length;
+
+    private byte[] source;
+    private byte[] dest;
+    private boolean[] presence;
+
+    @Setup(Level.Trial)
+    public void setup(){
+        source = new byte[length];
+        dest = new byte[length];
+        presence = new boolean[groups];
+
+        SplittableRandom random = new SplittableRandom(42);
+        for (int i = 0; i < source.length; i++) {
+            source[i] = (byte)random.nextInt(groups);
+        }
+    }
+
+    @Benchmark
+    public void fused(Blackhole bb){
+        int newGroups = 0;
+        for (int i = 0; i < source.length & i < dest.length; i++) {
+            dest[i] = source[i];
+            if(newGroups < groups && !presence[source[i]]){
+                presence[source[i] & 0xFF] = true;
+                newGroups++;
+            }
+        }
+        bb.consume(presence);
+        Arrays.fill(presence, false);
+    }
+
+    @Benchmark
+    public void fissured(Blackhole bh){
+        System.arraycopy(source, 0, dest, 0, source.length);
+        int numGroups = 0;
+
+        for (int i = 0; i < source.length & i < dest.length & numGroups < groups; i++) {
+            if(!presence[source[i]]){
+                presence[source[i] & 0xFF] = true;
+                numGroups++;
+            }
+        }
+        bh.consume(presence);
+        Arrays.fill(presence, true);
+    }
 }
