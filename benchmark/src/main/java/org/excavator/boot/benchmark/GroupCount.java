67816f17da10ddd445263e82089dfdb6ace6c721
CMonkey <42.codemonkey@gmail.com>
2021-11-29 18:19:04 +0800

    add license and format code

--- benchmark/src/main/java/org/excavator/boot/benchmark/GroupCount.java
+++ benchmark/src/main/java/org/excavator/boot/benchmark/GroupCount.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.excavator.boot.benchmark;
 
 import org.openjdk.jmh.annotations.*;
@@ -13,34 +29,34 @@ import java.util.SplittableRandom;
 @State(Scope.Benchmark)
 public class GroupCount {
 
-    @Param({"8", "64", "128"})
-    int groups;
+    @Param({ "8", "64", "128" })
+    int               groups;
 
-    @Param({"256", "512", "1024", "2048", "4096"})
-    int length;
+    @Param({ "256", "512", "1024", "2048", "4096" })
+    int               length;
 
-    private byte[] source;
-    private byte[] dest;
+    private byte[]    source;
+    private byte[]    dest;
     private boolean[] presence;
 
     @Setup(Level.Trial)
-    public void setup(){
+    public void setup() {
         source = new byte[length];
         dest = new byte[length];
         presence = new boolean[groups];
 
         SplittableRandom random = new SplittableRandom(42);
         for (int i = 0; i < source.length; i++) {
-            source[i] = (byte)random.nextInt(groups);
+            source[i] = (byte) random.nextInt(groups);
         }
     }
 
     @Benchmark
-    public void fused(Blackhole bb){
+    public void fused(Blackhole bb) {
         int numGroups = 0;
         for (int i = 0; i < source.length & i < dest.length; i++) {
             dest[i] = source[i];
-            if(numGroups < groups && !presence[source[i]]){
+            if (numGroups < groups && !presence[source[i]]) {
                 presence[source[i] & 0xFF] = true;
                 numGroups++;
             }
@@ -50,12 +66,12 @@ public class GroupCount {
     }
 
     @Benchmark
-    public void fissured(Blackhole bh){
+    public void fissured(Blackhole bh) {
         System.arraycopy(source, 0, dest, 0, source.length);
         int numGroups = 0;
 
         for (int i = 0; i < source.length & i < dest.length & numGroups < groups; i++) {
-            if(!presence[source[i]]){
+            if (!presence[source[i]]) {
                 presence[source[i] & 0xFF] = true;
                 numGroups++;
             }
@@ -65,11 +81,8 @@ public class GroupCount {
     }
 
     public static void main(String[] args) throws RunnerException {
-        Options opts = new OptionsBuilder().include(GroupCount.class.getSimpleName())
-                .forks(1)
-                .warmupIterations(5)
-                .measurementIterations(5)
-                .build();
+        Options opts = new OptionsBuilder().include(GroupCount.class.getSimpleName()).forks(1)
+            .warmupIterations(5).measurementIterations(5).build();
         new Runner(opts).run();
     }
 }
--- benchmark/src/main/java/org/excavator/boot/benchmark/XorCount.java
+++ benchmark/src/main/java/org/excavator/boot/benchmark/XorCount.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.excavator.boot.benchmark;
 
 import org.openjdk.jmh.annotations.*;
@@ -11,13 +27,13 @@ import java.util.concurrent.ThreadLocalRandom;
 @State(Scope.Benchmark)
 public class XorCount {
 
-    @Param({"256", "512", "1024", "2048", "4096"})
-    int size;
+    @Param({ "256", "512", "1024", "2048", "4096" })
+    int            size;
     private long[] left;
     private long[] right;
 
     @Setup(Level.Trial)
-    public void setup(){
+    public void setup() {
         left = new long[size];
         right = new long[size];
 
@@ -28,7 +44,7 @@ public class XorCount {
     }
 
     @Benchmark
-    public int fused(){
+    public int fused() {
         int count = 0;
         for (int i = 0; i < left.length & i < right.length; i++) {
             left[i] ^= right[i];
@@ -39,13 +55,13 @@ public class XorCount {
     }
 
     @Benchmark
-    public int fissured(){
+    public int fissured() {
         for (int i = 0; i < left.length & i < right.length; i++) {
             left[i] ^= right[i];
         }
 
         int count = 0;
-        for (long l : left){
+        for (long l : left) {
             count += Long.bitCount(l);
         }
 
@@ -54,10 +70,10 @@ public class XorCount {
 
     public static void main(String[] args) throws RunnerException {
         Options opts = new OptionsBuilder().include(XorCount.class.getSimpleName()) // 测试类的名字，可以通过正则表达式进行匹配
-                .forks(1) // fork 次数，fork 多少个进程进行测试
-                .warmupIterations(5) // 预热的迭代次数
-                .measurementIterations(5) // 实际测量的次数
-                .build();
+            .forks(1) // fork 次数，fork 多少个进程进行测试
+            .warmupIterations(5) // 预热的迭代次数
+            .measurementIterations(5) // 实际测量的次数
+            .build();
         new Runner(opts).run();
     }
 }
