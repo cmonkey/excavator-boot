9c314df8e07c182d488152d3d7ca6ed010fa0ccf
CMonkey <42.codemonkey@gmail.com>
2021-12-23 14:28:15 +0800

    implement LongAccumulatorExample

--- experiment/src/main/java/org/excavator/boot/experiment/LongAccumulatorExample.java
+++ experiment/src/main/java/org/excavator/boot/experiment/LongAccumulatorExample.java
@@ -1,2 +1,22 @@
-package org.excavator.boot.experiment;public class LongAccumulatorExample {
+package org.excavator.boot.experiment;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.LongAccumulator;
+
+public class LongAccumulatorExample {
+    public static void main(String[] args) throws InterruptedException {
+        var balance = new LongAccumulator(Long::sum, 10000L);
+        Runnable w = () -> balance.accumulate(1000L);
+
+        var executor = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 50; i++) {
+            executor.submit(w);
+        }
+
+        executor.shutdown();
+        if(executor.awaitTermination(1000L, java.util.concurrent.TimeUnit.MILLISECONDS)) {  // wait for all tasks to finish
+            System.out.println("balance: " + balance.get());
+        }
+
+    }
 }
