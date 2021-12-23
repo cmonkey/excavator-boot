8e5c080a165bf83145c092dfab22ea4211c78a46
CMonkey <42.codemonkey@gmail.com>
2021-12-23 14:42:21 +0800

    format code and add License

--- experiment/src/main/java/org/excavator/boot/experiment/HexFormatExample.java
+++ experiment/src/main/java/org/excavator/boot/experiment/HexFormatExample.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.excavator.boot.experiment;
 
 import java.util.Arrays;
@@ -7,7 +23,7 @@ public class HexFormatExample {
 
     public static void main(String[] args) {
         var format = HexFormat.of();
-        var input = new byte[]{127, 0, -50, 105};
+        var input = new byte[] { 127, 0, -50, 105 };
         var hex = format.formatHex(input);
         System.out.println(hex);
 
