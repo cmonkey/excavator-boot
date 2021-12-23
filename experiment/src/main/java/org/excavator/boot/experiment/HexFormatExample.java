9584b5538b723a49d1013d7380757ea427bcdd9f
CMonkey <42.codemonkey@gmail.com>
2021-12-23 14:18:09 +0800

    implement HexFormatExample

--- experiment/src/main/java/org/excavator/boot/experiment/HexFormatExample.java
+++ experiment/src/main/java/org/excavator/boot/experiment/HexFormatExample.java
@@ -1,2 +1,17 @@
-package org.excavator.boot.experiment;public class HexFormatExample {
+package org.excavator.boot.experiment;
+
+import java.util.Arrays;
+import java.util.HexFormat;
+
+public class HexFormatExample {
+
+    public static void main(String[] args) {
+        var format = HexFormat.of();
+        var input = new byte[]{127, 0, -50, 105};
+        var hex = format.formatHex(input);
+        System.out.println(hex);
+
+        var output = format.parseHex(hex);
+        assert Arrays.compare(input, output) == 0;
+    }
 }
