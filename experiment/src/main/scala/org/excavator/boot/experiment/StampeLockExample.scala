64339a7e95f943f6b3c8593d42f2d70993912f09
CMonkey <42.codemonkey@gmail.com>
2021-12-23 14:12:47 +0800

    implement StampedLock example

--- experiment/src/main/scala/org/excavator/boot/experiment/StampeLockExample.scala
+++ experiment/src/main/scala/org/excavator/boot/experiment/StampeLockExample.scala
@@ -1,5 +1,31 @@
 package org.excavator.boot.experiment
 
-class StampeLockExample {
+import java.util.concurrent.Executors
+import java.util.concurrent.locks.StampedLock
+
+object StampeLockExample extends App{
+
+  val lock = new StampedLock()
+
+  val b = new Balance(10000)
+
+  val w:Runnable = () => {
+    val stamp = lock.writeLock()
+    b.setAmount(b.getAmount + 1000)
+    println(s"Write: ${b.getAmount}")
+    lock.unlockWrite(stamp)
+  }
+
+  val r:Runnable = () => {
+    val stmp = lock.tryOptimisticRead()
+    println(s"Read: ${b.getAmount}")
+    lock.unlockRead(stmp)
+  }
+
+  val executor = Executors.newFixedThreadPool(10)
+  for (i <- 1 to 50) {
+    executor.submit(w)
+    executor.submit(r)
+  }
 
 }
