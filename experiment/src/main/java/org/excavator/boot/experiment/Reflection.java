f6a3229be69abbaec4f834e8bcad5e0fb65b39a8
CMonkey <42.codemonkey@gmail.com>
2022-01-04 18:30:08 +0800

    implement Reflection

--- experiment/src/main/java/org/excavator/boot/experiment/Reflection.java
+++ experiment/src/main/java/org/excavator/boot/experiment/Reflection.java
@@ -1,2 +1,16 @@
-package org.excavator.boot.experiment;public class Reflection {
+package org.excavator.boot.experiment;
+
+public class Reflection {
+
+    public static boolean get(){
+        return true;
+    }
+
+    public static void main(String[] args) throws Exception{
+        var method = Reflection.class.getMethod("get");
+        for (int i = 0; i < 30; i++) {
+            var value = method.invoke(null);
+            System.out.println(value == Boolean.TRUE);
+        }
+    }
 }
