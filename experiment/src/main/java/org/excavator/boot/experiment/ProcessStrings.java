020b302220c1ea7ad26dc7d7f935406f55075015
CMonkey <42.codemonkey@gmail.com>
2022-01-18 09:51:14 +0800

    add processStrings method

--- experiment/src/main/java/org/excavator/boot/experiment/ProcessStrings.java
+++ experiment/src/main/java/org/excavator/boot/experiment/ProcessStrings.java
@@ -1,4 +1,24 @@
 package org.excavator.boot.experiment;
 
+import java.util.List;
+
 public class ProcessStrings {
+
+    private static void processStrings(List<String> strings, boolean skipHeader) {
+        var stream = strings.stream();
+        if(skipHeader) {
+            stream.skip(1);
+        }else{
+            stream.skip(0);
+        }
+
+        stream.map(String::toUpperCase).forEach(System.out::println);
+    }
+
+    public static void main(String[] args) {
+        processStrings(
+                List.of("streams", "are", "cool"),
+                false
+        );
+    }
 }
