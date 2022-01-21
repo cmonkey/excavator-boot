86d758f26f2cc9712ae16a7cce677102d25a8a0f
CMonkey <42.codemonkey@gmail.com>
2022-01-21 14:48:55 +0800

    Labeled breaks and continues
        However, just because you can, doesn't mean you should Use this
        trick with care

--- experiment/src/main/java/org/excavator/boot/experiment/ContinueWithLabelDemo.java
+++ experiment/src/main/java/org/excavator/boot/experiment/ContinueWithLabelDemo.java
@@ -1,2 +1,26 @@
-package org.excavator.boot.experiment;public class ContinueWithLabelDemo {
+package org.excavator.boot.experiment;
+
+public class ContinueWithLabelDemo {
+    public static void main(String[] args) {
+        var searchMe = "Look for a substring in me";
+        var substring = "sub";
+        var foundIt = false;
+
+        var max = searchMe.length() - substring.length();
+
+    test:
+    for (int i = 0; i < max; i++) {
+        var n = substring.length();
+        var j = i;
+        var k = 0;
+        while(n -- != 0) {
+            if(searchMe.charAt(j++) != substring.charAt(k++)) {
+                continue test;
+            }
+        }
+        foundIt = true;
+        break test;
+    }
+    System.out.println(foundIt ? "Found it" : "Didn't find it");
+    }
 }
