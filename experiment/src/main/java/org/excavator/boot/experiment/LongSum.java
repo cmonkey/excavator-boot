4ec5993570299fdd5bad5cc17656190c90ef929e
CMonkey <42.codemonkey@gmail.com>
2022-01-27 17:38:21 +0800

    add compute method of LongSum

--- experiment/src/main/java/org/excavator/boot/experiment/LongSum.java
+++ experiment/src/main/java/org/excavator/boot/experiment/LongSum.java
@@ -1,2 +1,40 @@
-package org.excavator.boot.experiment;public class LongSum {
+package org.excavator.boot.experiment;
+
+import java.util.Arrays;
+import java.util.concurrent.RecursiveTask;
+
+public class LongSum extends RecursiveTask<Long> {
+
+    private static final int LIMIT = 1000;
+
+    private long[] numbers;
+    private int start;
+    private int end;
+
+    public LongSum(long[] numbers, int start, int end){
+        this.numbers = numbers;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Long compute(){
+        int length = end - start;
+        if(length <= LIMIT){
+            return computeSequentially();
+        }
+
+        LongSum left = new LongSum(numbers, start, length / 2);
+        left.fork();
+
+        LongSum right = new LongSum(numbers, start + length /2, end);
+        Long rightResult = right.compute();
+        Long leftResult = left.join();
+
+        return leftResult + rightResult;
+    }
+
+    private Long computeSequentially(){
+        return Arrays.stream(numbers).sum();
+    }
 }
