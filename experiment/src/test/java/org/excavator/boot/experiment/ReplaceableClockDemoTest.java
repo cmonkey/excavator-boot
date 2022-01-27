f1df7ad46fe86c63f639a3f14cc8b63734ffe618
CMonkey <42.codemonkey@gmail.com>
2022-01-27 17:30:38 +0800

    add shouldNotBeBeforePiDay2017 method

--- experiment/src/test/java/org/excavator/boot/experiment/ReplaceableClockDemoTest.java
+++ experiment/src/test/java/org/excavator/boot/experiment/ReplaceableClockDemoTest.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ReplaceableClockDemoTest {
@@ -18,5 +19,14 @@ public class ReplaceableClockDemoTest {
         LocalDateTime stPatricksDay = LocalDateTime.of(2017,3,17,0,0);
         ReplaceableClockDemo demo = new ReplaceableClockDemo(now);
         assertTrue(demo.isNowBefore(stPatricksDay));
+    }
+
+    @Test
+    public void shouldNotBeBeforePiDay2017() throws Exception{
+        Clock now = Clock.fixed(LocalDateTime.of(2017,5,18,9,45)
+                .toInstant(ZoneOffset.UTC), ZoneId.of("UTC"));
+        LocalDateTime piDay = LocalDateTime.of(2017,3,14,0,0);
+        ReplaceableClockDemo demo = new ReplaceableClockDemo(now);
+        assertFalse(demo.isNowBefore(piDay));
     }
 }
