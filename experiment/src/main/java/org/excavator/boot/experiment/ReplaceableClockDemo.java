90c6150a27765670d9478866b16320775cb69fc0
CMonkey <42.codemonkey@gmail.com>
2022-01-27 17:25:16 +0800

    add isNowBefore method

--- experiment/src/main/java/org/excavator/boot/experiment/ReplaceableClockDemo.java
+++ experiment/src/main/java/org/excavator/boot/experiment/ReplaceableClockDemo.java
@@ -1,2 +1,23 @@
-package org.excavator.boot.experiment;public class ReplaceableClockDemo {
+package org.excavator.boot.experiment;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+
+public class ReplaceableClockDemo {
+
+    public static void main(String[] args) {
+        ReplaceableClockDemo demo  = new ReplaceableClockDemo(Clock.systemUTC());
+        demo.isNowBefore(LocalDateTime.of(2017, 3, 1, 12, 0));
+
+    }
+
+    private final Clock clock;
+
+    public ReplaceableClockDemo(Clock clock){
+        this.clock = clock;
+    }
+
+    public boolean isNowBefore(LocalDateTime dateTime){
+        return LocalDateTime.now(clock).isBefore(dateTime);
+    }
 }
