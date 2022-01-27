dfda1cdfffe41cc58642783852baf8d2245167da
CMonkey <42.codemonkey@gmail.com>
2022-01-27 17:51:55 +0800

    add testLongSum method

--- experiment/src/test/java/org/excavator/boot/experiment/LongSumTest.java
+++ experiment/src/test/java/org/excavator/boot/experiment/LongSumTest.java
@@ -1,2 +1,19 @@
-package org.excavator.boot.experiment;public class LongSumTest {
+package org.excavator.boot.experiment;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.LongStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LongSumTest {
+
+    @Test
+    public void testLongSum() {
+        long[] numbers = LongStream.rangeClosed(1, 1000000).toArray();
+        LongSum longSum = new LongSum(numbers, 0, numbers.length);
+        long sum = new ForkJoinPool().invoke(longSum);
+        assertEquals(28000028000000L, sum);
+    }
 }
