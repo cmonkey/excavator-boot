167697efa7119a0f6da39716f3eed389dc1aaf27
CMonkey <42.codemonkey@gmail.com>
2022-01-27 17:33:43 +0800

    add License and format code

--- experiment/src/test/java/org/excavator/boot/experiment/ReplaceableClockDemoTest.java
+++ experiment/src/test/java/org/excavator/boot/experiment/ReplaceableClockDemoTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.excavator.boot.experiment;
 
 import org.junit.jupiter.api.Test;
@@ -13,19 +29,19 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class ReplaceableClockDemoTest {
 
     @Test
-    public void shouldBeBeforeStPartickDay2017()throws Exception{
-        Clock now = Clock.fixed(LocalDateTime.of(2017,2,15,12,34).toInstant(ZoneOffset.UTC),
-                ZoneId.of("UTC"));
-        LocalDateTime stPatricksDay = LocalDateTime.of(2017,3,17,0,0);
+    public void shouldBeBeforeStPartickDay2017() throws Exception {
+        Clock now = Clock.fixed(LocalDateTime.of(2017, 2, 15, 12, 34).toInstant(ZoneOffset.UTC),
+            ZoneId.of("UTC"));
+        LocalDateTime stPatricksDay = LocalDateTime.of(2017, 3, 17, 0, 0);
         ReplaceableClockDemo demo = new ReplaceableClockDemo(now);
         assertTrue(demo.isNowBefore(stPatricksDay));
     }
 
     @Test
-    public void shouldNotBeBeforePiDay2017() throws Exception{
-        Clock now = Clock.fixed(LocalDateTime.of(2017,5,18,9,45)
-                .toInstant(ZoneOffset.UTC), ZoneId.of("UTC"));
-        LocalDateTime piDay = LocalDateTime.of(2017,3,14,0,0);
+    public void shouldNotBeBeforePiDay2017() throws Exception {
+        Clock now = Clock.fixed(LocalDateTime.of(2017, 5, 18, 9, 45).toInstant(ZoneOffset.UTC),
+            ZoneId.of("UTC"));
+        LocalDateTime piDay = LocalDateTime.of(2017, 3, 14, 0, 0);
         ReplaceableClockDemo demo = new ReplaceableClockDemo(now);
         assertFalse(demo.isNowBefore(piDay));
     }
