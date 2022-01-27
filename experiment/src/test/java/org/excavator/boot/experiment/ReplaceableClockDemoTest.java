01ecb2a973a04d54f9c47ff0b98c04ec1d8fc9bc
CMonkey <42.codemonkey@gmail.com>
2022-01-27 17:28:18 +0800

    add shouldBeBeforeStPartickDay2017 method

--- experiment/src/test/java/org/excavator/boot/experiment/ReplaceableClockDemoTest.java
+++ experiment/src/test/java/org/excavator/boot/experiment/ReplaceableClockDemoTest.java
@@ -1,2 +1,22 @@
-package org.excavator.boot.experiment;public class ReplaceableClockDemoTest {
+package org.excavator.boot.experiment;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ReplaceableClockDemoTest {
+
+    @Test
+    public void shouldBeBeforeStPartickDay2017()throws Exception{
+        Clock now = Clock.fixed(LocalDateTime.of(2017,2,15,12,34).toInstant(ZoneOffset.UTC),
+                ZoneId.of("UTC"));
+        LocalDateTime stPatricksDay = LocalDateTime.of(2017,3,17,0,0);
+        ReplaceableClockDemo demo = new ReplaceableClockDemo(now);
+        assertTrue(demo.isNowBefore(stPatricksDay));
+    }
 }
