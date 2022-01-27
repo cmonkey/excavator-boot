27b83f90467799cb6731613e214206690897798f
CMonkey <42.codemonkey@gmail.com>
2022-01-27 17:54:12 +0800

    add License and format code

--- experiment/src/main/java/org/excavator/boot/experiment/LongSum.java
+++ experiment/src/main/java/org/excavator/boot/experiment/LongSum.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.excavator.boot.experiment;
 
 import java.util.Arrays;
@@ -7,34 +23,34 @@ public class LongSum extends RecursiveTask<Long> {
 
     private static final int LIMIT = 1000;
 
-    private long[] numbers;
-    private int start;
-    private int end;
+    private long[]           numbers;
+    private int              start;
+    private int              end;
 
-    public LongSum(long[] numbers, int start, int end){
+    public LongSum(long[] numbers, int start, int end) {
         this.numbers = numbers;
         this.start = start;
         this.end = end;
     }
 
     @Override
-    protected Long compute(){
+    protected Long compute() {
         int length = end - start;
-        if(length <= LIMIT){
+        if (length <= LIMIT) {
             return computeSequentially();
         }
 
         LongSum left = new LongSum(numbers, start, length / 2);
         left.fork();
 
-        LongSum right = new LongSum(numbers, start + length /2, end);
+        LongSum right = new LongSum(numbers, start + length / 2, end);
         Long rightResult = right.compute();
         Long leftResult = left.join();
 
         return leftResult + rightResult;
     }
 
-    private Long computeSequentially(){
+    private Long computeSequentially() {
         return Arrays.stream(numbers).sum();
     }
 }
