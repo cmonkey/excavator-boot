5de25a51fb282f4ae343fafa01aadd4f6aef1982
CMonkey <42.codemonkey@gmail.com>
2022-01-28 10:10:57 +0800

    add testPredicate method

--- experiment/src/test/java/org/excavator/boot/experiment/PredicateTest.java
+++ experiment/src/test/java/org/excavator/boot/experiment/PredicateTest.java
@@ -1,2 +1,23 @@
-package org.excavator.boot.experiment;public class PredicateTest {
+package org.excavator.boot.experiment;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+
+import java.util.Set;
+
+public class PredicateTest {
+
+    public static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(PredicateTest.class);
+
+    @Test
+    public void testPredicate(){
+        var set = Set.of("a", "b", "c");
+
+        set.stream().filter(s -> {
+            LOGGER.info("filter: " + s);
+            return s.length() > 1;
+        }).findFirst().ifPresent(s -> {
+            LOGGER.info("findFirst: " + s);
+        });
+    }
 }
