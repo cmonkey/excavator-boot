7e43289830dafd03e9e9d3d12d6e36441a1b33ef
CMonkey <42.codemonkey@gmail.com>
2022-02-21 14:24:08 +0800

    implement ExecutorServiceSample2

--- experiment/src/main/java/org/excavator/boot/experiment/ExecutorServiceSample2.java
+++ experiment/src/main/java/org/excavator/boot/experiment/ExecutorServiceSample2.java
@@ -1,2 +1,31 @@
-package org.excavator.boot.experiment;public class ExecutorServiceSample2 {
+package org.excavator.boot.experiment;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+
+public class ExecutorServiceSample2 {
+    public static void main(String[] args) {
+        var executor = Executors.newScheduledThreadPool(2, new MyThreadFactory());
+        //no return value
+        CompletableFuture.runAsync(() -> {
+            System.out.println("Inside Runnable " + Thread.currentThread().getName());
+        }, executor);
+
+        //with return value
+        var cf = CompletableFuture.supplyAsync(() -> {
+            System.out.println("Inside Supplier " + Thread.currentThread().getName());
+            return true;
+        },executor);
+
+        //get blocks the thread and waits for the future to complete
+        try{
+            System.out.println("Result from suppler " + cf.get());
+        }catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        //shutdown executor manually
+        executor.shutdown();
+    }
 }
