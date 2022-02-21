d0e3cd3a9c16ceb5e1b6612932dbe0311d0d30d2
CMonkey <42.codemonkey@gmail.com>
2022-02-21 14:33:52 +0800

    implement ExecutorServiceSample4

--- experiment/src/main/java/org/excavator/boot/experiment/ExecutorServiceSample4.java
+++ experiment/src/main/java/org/excavator/boot/experiment/ExecutorServiceSample4.java
@@ -1,2 +1,22 @@
-package org.excavator.boot.experiment;public class ExecutorServiceSample4 {
+package org.excavator.boot.experiment;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+
+public class ExecutorServiceSample4 {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        var executor = Executors.newScheduledThreadPool(2, new MyThreadFactory());
+        var cf = CompletableFuture.supplyAsync(() -> 1,executor);
+        //result of cf is passed to another future object that invokes add method
+        var result = cf.thenCompose(x -> CompletableFuture.supplyAsync(() -> add(x, 2), executor));
+        System.out.println("Result " + result.get());
+
+        //shutdown executor manually
+        executor.shutdown();
+    }
+
+    public static int add(int x, int y) {
+        return x + y;
+    }
 }
