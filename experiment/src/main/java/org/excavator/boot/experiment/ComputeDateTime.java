80e766c992563152b9a06f317f01ae4cb5fb5c89
CMonkey <42.codemonkey@gmail.com>
2022-02-28 09:50:55 +0800

    implement calculating date time

--- experiment/src/main/java/org/excavator/boot/experiment/ComputeDateTime.java
+++ experiment/src/main/java/org/excavator/boot/experiment/ComputeDateTime.java
@@ -1,4 +1,17 @@
 package org.excavator.boot.experiment;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 public class ComputeDateTime {
+
+    public static void main(String[] args) {
+        var now = System.currentTimeMillis();
+        System.out.println("current: " + now);
+
+        var dateTime = LocalDateTime.now().withMonth(2).withDayOfMonth(15).withHour(20).withMinute(0).withSecond(0);
+        var zdt = ZonedDateTime.of(dateTime, ZoneId.systemDefault());
+        System.out.println("millis: " + zdt.toInstant().toEpochMilli());
+    }
 }
