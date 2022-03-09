84e0a382452b11631a14e72acf8af736487534b2
CMonkey <42.codemonkey@gmail.com>
2022-03-09 10:48:10 +0800

    implement multiple Exception

--- experiment/src/main/java/org/excavator/boot/experiment/ExceptionChaosChallenge.java
+++ experiment/src/main/java/org/excavator/boot/experiment/ExceptionChaosChallenge.java
@@ -1,2 +1,34 @@
-package org.excavator.boot.experiment;public class ExceptionChaosChallenge {
+package org.excavator.boot.experiment;
+
+import java.io.FileNotFoundException;
+
+public class ExceptionChaosChallenge {
+    static String s = "-";
+
+    public static void main(String[] args) {
+        try{
+            throw new IllegalArgumentException();
+        }catch (Exception e){
+            try{
+                try{
+                    throw new FileNotFoundException();
+                }catch (RuntimeException ex){
+                    s += "run";
+                }
+            }catch (Exception x){
+                s += "exc";
+            }finally {
+                s += "fin";
+            }
+        }finally {
+            s += "of";
+            try{
+                throw new UnknownError("JVMError");
+            }catch (Error error){
+                s += "Error" + error.getMessage();
+            }
+        }
+
+        System.out.println(s);
+    }
 }
