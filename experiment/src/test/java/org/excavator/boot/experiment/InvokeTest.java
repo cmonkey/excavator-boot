57b9f33546b5bde798c3964ffbe73c0930c2541d
CMonkey <42.codemonkey@gmail.com>
2022-08-10 11:24:02 +0800

    implement testInvoke method

--- experiment/src/test/java/org/excavator/boot/experiment/InvokeTest.java
+++ experiment/src/test/java/org/excavator/boot/experiment/InvokeTest.java
@@ -1,2 +1,28 @@
-package org.excavator.boot.experiment;public class InvokeTest {
+package org.excavator.boot.experiment;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InvokeTest {
+
+    @SneakyThrows
+    @Test
+    @DisplayName("test invoke")
+    public void testInvoke(){
+        var names = new ArrayList<String>();
+        var add = List.class.getMethod("add", Object.class);
+        add.invoke(names, "John");
+        add.invoke(names, "Anton");
+        add.invoke(names, "Heinz");
+        System.out.println("names = " + names);
+        add.invoke(names, null);
+        System.out.println("names = " + names);
+        add.invoke(names, 42);
+        System.out.println("names = " + names);
+        for (String name: names)System.out.println(name);
+    }
 }
