416d27467d777096b5cfe012058231e8908b2e77
CMonkey <42.codemonkey@gmail.com>
2022-08-19 15:12:09 +0800

    add write method

--- experiment/src/main/java/org/excavator/boot/experiment/WriteHelper.java
+++ experiment/src/main/java/org/excavator/boot/experiment/WriteHelper.java
@@ -1,2 +1,12 @@
-package org.excavator.boot.experiment;public class WriteHelper {
+package org.excavator.boot.experiment;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class WriteHelper {
+
+    public static void write(String out_file, List<String> outLines) throws IOException {
+        Files.write(Paths.get(out_file),outLines);
+    }
 }
