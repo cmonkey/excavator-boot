99e3c6c465b257d169af041205c7ca1d08fa254d
CMonkey <42.codemonkey@gmail.com>
2022-08-31 10:11:49 +0800

    format and add license

--- experiment/src/test/java/org/excavator/boot/experiment/InvokeTest.java
+++ experiment/src/test/java/org/excavator/boot/experiment/InvokeTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.excavator.boot.experiment;
 
 import lombok.SneakyThrows;
@@ -12,7 +28,7 @@ public class InvokeTest {
     @SneakyThrows
     @Test
     @DisplayName("test invoke")
-    public void testInvoke(){
+    public void testInvoke() {
         var names = new ArrayList<String>();
         var add = List.class.getMethod("add", Object.class);
         add.invoke(names, "John");
@@ -23,6 +39,7 @@ public class InvokeTest {
         System.out.println("names = " + names);
         add.invoke(names, 42);
         System.out.println("names = " + names);
-        for (String name: names)System.out.println(name);
+        for (String name : names)
+            System.out.println(name);
     }
 }
