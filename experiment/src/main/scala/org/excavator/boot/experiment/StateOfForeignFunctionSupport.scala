e8e805f7c3efeb93d16fc23a6c7b97fc3e20e8c9
CMonkey <42.codemonkey@gmail.com>
2022-11-16 14:45:58 +0800

    change foreign package from jdk.incubator.foreign to java.lang.foreign

--- experiment/src/main/scala/org/excavator/boot/experiment/StateOfForeignFunctionSupport.scala
+++ experiment/src/main/scala/org/excavator/boot/experiment/StateOfForeignFunctionSupport.scala
@@ -1,6 +1,6 @@
 package org.excavator.boot.experiment
-import jdk.incubator.foreign.MemoryAddress;
-import jdk.incubator.foreign.MemorySegment;
+import java.lang.foreign.MemoryAddress;
+import java.lang.foreign.MemorySegment;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
