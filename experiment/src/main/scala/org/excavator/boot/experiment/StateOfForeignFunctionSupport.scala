7f5b53119f1549f3d25b96f2613ffe1726b33c3d
CMonkey <42.codemonkey@gmail.com>
2022-11-16 15:14:08 +0800

    unused import to StateOfForeignFunctionSupport

--- experiment/src/main/scala/org/excavator/boot/experiment/StateOfForeignFunctionSupport.scala
+++ experiment/src/main/scala/org/excavator/boot/experiment/StateOfForeignFunctionSupport.scala
@@ -1,6 +1,4 @@
 package org.excavator.boot.experiment
-import java.lang.foreign.MemoryAddress;
-import java.lang.foreign.MemorySegment;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
