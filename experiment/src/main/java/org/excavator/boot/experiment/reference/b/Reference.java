f487db1326067f2452deb66e16a2e099630936bf
CMonkey <42.codemonkey@gmail.com>
2022-12-21 17:28:33 +0800

    add main method

--- experiment/src/main/java/org/excavator/boot/experiment/reference/b/Reference.java
+++ experiment/src/main/java/org/excavator/boot/experiment/reference/b/Reference.java
@@ -1,2 +1,9 @@
-package org.excavator.boot.experiment.reference.b;public class Reference {
+package org.excavator.boot.experiment.reference.b;
+
+import org.excavator.boot.experiment.reference.a.Outer;
+
+public class Reference {
+    public static void main(String[] args) {
+        Outer.invoke(() -> 42);
+    }
 }
