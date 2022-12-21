604b2ccd5963961b5244dcd83dd0664ee5a45d46
CMonkey <42.codemonkey@gmail.com>
2022-12-21 18:12:30 +0800

    init Java19HttpServer MyHandler

---
+++ experiment/src/main/java/org/excavator/boot/experiment/httpserver/Java19HttpServer.java
@@ -0,0 +1,2 @@
+package org.excavator.boot.experiment.httpserver;public class Java19HttpServer {
+}
---
+++ experiment/src/main/java/org/excavator/boot/experiment/httpserver/MyHandler.java
@@ -0,0 +1,6 @@
+package org.excavator.boot.experiment.httpserver;
+
+import com.sun.net.httpserver.HttpHandler;
+
+public class MyHandler implements HttpHandler {
+}
