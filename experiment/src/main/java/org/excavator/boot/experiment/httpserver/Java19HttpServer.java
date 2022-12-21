86f033f49c97d0329d19c339347a2757ee6960a5
CMonkey <42.codemonkey@gmail.com>
2022-12-21 18:12:40 +0800

    implement handler method

--- experiment/src/main/java/org/excavator/boot/experiment/httpserver/Java19HttpServer.java
+++ experiment/src/main/java/org/excavator/boot/experiment/httpserver/Java19HttpServer.java
@@ -1,2 +1,19 @@
-package org.excavator.boot.experiment.httpserver;public class Java19HttpServer {
+package org.excavator.boot.experiment.httpserver;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+public class Java19HttpServer {
+
+    public static void main(String[] args) throws IOException {
+
+        var httpServer = HttpServer.create(new InetSocketAddress(8080), 0);
+        httpServer.createContext("/persons", new MyHandler());
+        httpServer.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+
+        httpServer.start();
+    }
 }
