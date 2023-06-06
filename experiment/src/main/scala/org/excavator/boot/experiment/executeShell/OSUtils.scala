8bead5d55dc3740e4f6ca8d01e069d38e230a819
CMonkey <42.codemonkey@gmail.com>
2023-06-06 16:39:39 +0800

    add executeCommand executeShellCommand functions

--- experiment/src/main/scala/org/excavator/boot/experiment/executeShell/OSUtils.scala
+++ experiment/src/main/scala/org/excavator/boot/experiment/executeShell/OSUtils.scala
@@ -1,5 +1,47 @@
 package org.excavator.boot.experiment.executeShell
 
-class OSUtils {
+import cats.effect.IO
+import org.apache.commons.text.StringEscapeUtils
+import sun.nio.cs.UTF_8
+
+import java.nio.file.Path
+
+object OSUtils {
+
+  def executeCommand(executable:Path, args:String*):IO[CommandResult] =
+    IO.blocking{
+      val commandArgs = executable.toAbsolutePath.toString +: args
+      Runtime.getRuntime.exec(commandArgs.toArray)
+    }.bracket{
+      proc =>
+        val collectStdout = IO.blocking{
+          new String(proc.getInputStream.readAllBytes(), UTF_8)
+        }
+        val collectStderr = IO.blocking{
+          new String(proc.getErrorStream.readAllBytes(), UTF_8)
+        }
+
+        val awaitReturnCode = IO.interruptible{
+          proc.waitFor()
+        }
+
+        for{
+          stdoutFiber <- collectStdout.start
+          stderrFier <- collectStderr.start
+          code <- awaitReturnCode
+          stdout <- stdoutFiber.joinWithNever
+          stderr <- stderrFier.joinWithNever
+        }yield{
+          CommandResult(code, stdout, stderr)
+        }
+    }{proc =>
+      IO.blocking{
+        println("Destroying process")
+        proc.destroy()
+      }
+    }
+
+  def executeShellCommand(command:String, args:String*):IO[CommandResult] =
+    executeCommand(Path.of("/bin/sh"), "-c", (command +: args).map(StringEscapeUtils.escapeXSI).mkString(" "))
 
 }
