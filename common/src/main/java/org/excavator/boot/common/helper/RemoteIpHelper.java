161a3a300fb56fd4109ed3d71a92474956d1b709
CMonkey <42.codemonkey@gmail.com>
2023-07-28 15:15:27 +0800

    replace javax.servlet.http jakarta.servlet.http

--- common/src/main/java/org/excavator/boot/common/helper/RemoteIpHelper.java
+++ common/src/main/java/org/excavator/boot/common/helper/RemoteIpHelper.java
@@ -16,7 +16,7 @@
  */
 package org.excavator.boot.common.helper;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 import static org.excavator.boot.common.helper.HttpHeader.*;
 
