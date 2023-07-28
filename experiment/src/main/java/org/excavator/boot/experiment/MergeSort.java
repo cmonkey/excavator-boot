8763018ac0775174eaa6798b232157ffd0840614
CMonkey <42.codemonkey@gmail.com>
2023-07-28 17:05:05 +0800

    add License

--- experiment/src/main/java/org/excavator/boot/experiment/MergeSort.java
+++ experiment/src/main/java/org/excavator/boot/experiment/MergeSort.java
@@ -1,13 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.excavator.boot.experiment;
 
 import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveAction;
 
 public class MergeSort extends RecursiveAction {
-    private static final int  SEQUENTIAL_THRESHOLD = 50;
-    final int[] numbers;
-    final int startPos, endPos;
-    final int[] result;
+    private static final int SEQUENTIAL_THRESHOLD = 50;
+    final int[]              numbers;
+    final int                startPos, endPos;
+    final int[]              result;
 
     public MergeSort(int[] numbers, int startPos, int endPos) {
         super();
@@ -17,29 +33,29 @@ public class MergeSort extends RecursiveAction {
         this.result = new int[endPos - startPos];
     }
 
-    public int size(){
+    public int size() {
         return endPos - startPos;
     }
 
-    private void merge(MergeSort left, MergeSort right){
-        int i = 0, leftPos = 0, rightPos = 0, leftSize = left.size(), rightSize= right.size();
+    private void merge(MergeSort left, MergeSort right) {
+        int i = 0, leftPos = 0, rightPos = 0, leftSize = left.size(), rightSize = right.size();
         while (leftPos < leftSize && rightPos < rightSize)
             result[i++] = (left.result[leftPos] <= right.result[rightPos]) ? left.result[leftPos++]
-                    : right.result[rightPos++];
-        while(leftPos < leftSize){
+                : right.result[rightPos++];
+        while (leftPos < leftSize) {
             result[i++] = left.result[leftPos++];
         }
 
-        while(rightPos < rightSize){
+        while (rightPos < rightSize) {
             result[i++] = right.result[rightPos++];
         }
     }
 
     @Override
     protected void compute() {
-        if(size() < SEQUENTIAL_THRESHOLD){
+        if (size() < SEQUENTIAL_THRESHOLD) {
             System.arraycopy(numbers, startPos, result, 0, size());
-        }else{
+        } else {
             int midpoint = size() / 2;
             var left = new MergeSort(numbers, startPos, startPos + midpoint);
             var right = new MergeSort(numbers, startPos + midpoint, endPos);
--- experiment/src/main/java/org/excavator/boot/experiment/executeShell/TaskUtils.java
+++ experiment/src/main/java/org/excavator/boot/experiment/executeShell/TaskUtils.java
@@ -1,16 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.excavator.boot.experiment.executeShell;
 
 import java.time.Duration;
 import java.util.concurrent.*;
 
 public class TaskUtils {
-    public static <A> A withTimeout(ExecutorService es,
-                                    Duration timeout,
-                                    Callable<A> task)throws InterruptedException, TimeoutException{
+    public static <A> A withTimeout(ExecutorService es, Duration timeout, Callable<A> task)
+                                                                                           throws InterruptedException,
+                                                                                           TimeoutException {
         final var ft = new FutureTask<>(task);
-        try{
-           es.submit(ft) ;
-           return ft.get(timeout.toMillis(), TimeUnit.MILLISECONDS);
+        try {
+            es.submit(ft);
+            return ft.get(timeout.toMillis(), TimeUnit.MILLISECONDS);
         } catch (ExecutionException e) {
             ft.cancel(true);
             throw new RuntimeException(e);
@@ -20,9 +36,9 @@ public class TaskUtils {
         }
     }
 
-    public static void cancelAll(Future<?>... futures){
-        for(final var f : futures){
-            if(f != null){
+    public static void cancelAll(Future<?>... futures) {
+        for (final var f : futures) {
+            if (f != null) {
                 f.cancel(true);
             }
         }
