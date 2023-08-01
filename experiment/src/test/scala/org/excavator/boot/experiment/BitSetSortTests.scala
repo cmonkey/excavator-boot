70699ea1d4eeb85e724d57fc6ec4f65dd245a018
CMonkey <42.codemonkey@gmail.com>
2023-08-01 15:48:36 +0800

    implement testGenerateFile

--- experiment/src/test/scala/org/excavator/boot/experiment/BitSetSortTests.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/BitSetSortTests.scala
@@ -4,13 +4,17 @@ import org.junit.jupiter.api.{DisplayName, Test}
 
 import java.nio.file.Files
 
-class BitSetSortTest {
+class BitSetSortTests {
 
   @Test
   @DisplayName("test generate file")
   def testGenerateFile():Unit = {
     val outputPath = Files.createTempFile("random", ".txt")
-    GenerationFile.generateRandomNumbers(outputPath)
+    val measurementResult = MeasureUtils.measure(
+      GenerationFile.generateRandomNumbers(outputPath)
+    )
+    println(s"程序执行时间:${measurementResult.executionTime / 1000000} 毫秒")
+    println(s"程序内存消耗:${measurementResult.memoryConsumed.toDouble / (1024 * 1024) } MB")
   }
 
 }
