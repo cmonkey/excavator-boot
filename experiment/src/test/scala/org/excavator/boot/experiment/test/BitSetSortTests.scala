080b1c52e81fe19b037adc6f37bb5a6f30e63408
CMonkey <42.codemonkey@gmail.com>
2023-08-01 17:13:10 +0800

    implement testBitSetSortByFile

--- experiment/src/test/scala/org/excavator/boot/experiment/test/BitSetSortTests.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/BitSetSortTests.scala
@@ -2,8 +2,10 @@ package org.excavator.boot.experiment.test
 
 import org.excavator.boot.experiment.{BitsetSort, GenerationFile, MeasureUtils}
 import org.junit.jupiter.api.{DisplayName, Test}
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
-import java.nio.file.Files
+import java.nio.file.{Files, Paths}
 
 class BitSetSortTests {
 
@@ -37,6 +39,28 @@ class BitSetSortTests {
 
     println(s"排序数据的程序执行时间:${measurementBitSetSortResult.executionTime / 1000000} 毫秒")
     println(s"排序数据程序内存消耗:${measurementBitSetSortResult.memoryConsumed} 字节")
+  }
+
+  @DisplayName("test bit set sort by file")
+  @ParameterizedTest
+  @ValueSource(strings = Array("/tmp/random4228685429583332152.txt"))
+  def testBitSetSortByFile(inputFilePath:String):Unit = {
+    println(s"inputFilePath is [${inputFilePath}]")
+    val sortInputPath = Paths.get(inputFilePath)
+    val isExists = sortInputPath.toFile.exists()
+    if(isExists) {
+      val sortOutputPath = Files.createTempFile("random-bitset-sort", ".txt")
+
+      System.gc()
+
+      val measurementBitSetSortResult = MeasureUtils.measure(
+        BitsetSort.sortFile(sortInputPath, sortOutputPath)
+      )
+
+      println(s"排序数据后写入的文件:${sortOutputPath}")
+      println(s"排序数据的程序执行时间:${measurementBitSetSortResult.executionTime / 1000000} 毫秒")
+      println(s"排序数据程序内存消耗:${measurementBitSetSortResult.memoryConsumed} 字节")
+    }
   }
 
 }
