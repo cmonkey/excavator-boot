aaa07bf047e69fda89ae880ea4ecb95cc938a76f
CMonkey <42.codemonkey@gmail.com>
2023-08-09 11:20:39 +0800

    implement testCronExpressionParseNext function

--- experiment/src/test/scala/org/excavator/boot/experiment/test/CronExpressionParseNextTests.scala
+++ experiment/src/test/scala/org/excavator/boot/experiment/test/CronExpressionParseNextTests.scala
@@ -1,5 +1,19 @@
 package org.excavator.boot.experiment.test
 
+import org.junit.jupiter.api.{DisplayName, Test}
+import org.springframework.scheduling.support.CronExpression
+
+import java.time.LocalDateTime
+
 class CronExpressionParseNextTests {
+
+  @Test
+  @DisplayName("test cron expression parse next")
+  def testCronExpressionParseNext():Unit = {
+
+    val expression = "0 0 0 * * *"
+    val nextTime = CronExpression.parse(expression).next(LocalDateTime.now())
+    println(s"nextTime is ${nextTime}")
+  }
 
 }
