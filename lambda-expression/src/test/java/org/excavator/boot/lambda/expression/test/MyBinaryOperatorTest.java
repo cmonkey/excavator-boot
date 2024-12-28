f209e4d3136a39a3d09d3fce5d74d4eab1c41fdf
CMonkey <42.codemonkey@gmail.com>
2024-12-28 21:53:39 +0800

    optimize: cancel lombok dependency

--- lambda-expression/src/test/java/org/excavator/boot/lambda/expression/test/MyBinaryOperatorTest.java
+++ lambda-expression/src/test/java/org/excavator/boot/lambda/expression/test/MyBinaryOperatorTest.java
@@ -16,14 +16,12 @@
  */
 package org.excavator.boot.lambda.expression.test;
 
-import lombok.extern.slf4j.Slf4j;
 import org.excavator.boot.lambda.expression.MyBinaryOperator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Slf4j
 public class MyBinaryOperatorTest {
 
     @Test
