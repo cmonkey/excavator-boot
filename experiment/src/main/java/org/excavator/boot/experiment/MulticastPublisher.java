265f8c1c541b06d754dff4297a997aa6b81025d2
CMonkey <42.codemonkey@gmail.com>
2025-10-18 23:01:22 +0800

    feature: implement multicast

--- experiment/src/main/java/org/excavator/boot/experiment/MulticastPublisher.java
+++ experiment/src/main/java/org/excavator/boot/experiment/MulticastPublisher.java
@@ -1,4 +1,19 @@
 package org.excavator.boot.experiment;
 
+import java.io.IOException;
+import java.net.*;
+
 public class MulticastPublisher {
+    private DatagramSocket socket;
+    private InetAddress group;
+    private byte[] buf;
+    public void multicast(String multicastMessage) throws IOException {
+
+        socket = new DatagramSocket();
+        group = InetAddress.getByName("224.0.0.1");
+        buf = multicastMessage.getBytes();
+        var packet = new DatagramPacket(buf, buf.length, group, 4446);
+        socket.send(packet);
+        socket.close();
+    }
 }
