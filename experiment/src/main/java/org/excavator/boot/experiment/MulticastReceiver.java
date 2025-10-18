a0e8502293be443746c090ee2123b5748b8cd140
CMonkey <42.codemonkey@gmail.com>
2025-10-18 23:06:49 +0800

    optimize: format code style and add lience

--- experiment/src/main/java/org/excavator/boot/experiment/MulticastReceiver.java
+++ experiment/src/main/java/org/excavator/boot/experiment/MulticastReceiver.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.excavator.boot.experiment;
 
 import java.io.IOException;
@@ -5,21 +21,21 @@ import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 
-public class MulticastReceiver extends Thread{
+public class MulticastReceiver extends Thread {
     protected MulticastSocket socket = null;
-    protected byte[] buf = new byte[256];
+    protected byte[]          buf    = new byte[256];
 
-    public void run(){
+    public void run() {
         try {
             socket = new MulticastSocket(4446);
             var group = InetAddress.getByName("224.0.0.1");
             socket.joinGroup(group);
-            while(true){
+            while (true) {
                 var packet = new DatagramPacket(buf, buf.length);
                 socket.receive(packet);
                 var received = new String(packet.getData(), 0, packet.getLength());
                 System.out.println(received);
-                if("end".equals(received)){
+                if ("end".equals(received)) {
                     break;
                 }
             }
