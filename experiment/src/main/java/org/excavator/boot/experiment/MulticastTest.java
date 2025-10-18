f17affe3d5057ea74b176d3687dd1c3a8ad11d03
CMonkey <42.codemonkey@gmail.com>
2025-10-18 23:01:55 +0800

    feature: implement test multicast

--- experiment/src/main/java/org/excavator/boot/experiment/MulticastTest.java
+++ experiment/src/main/java/org/excavator/boot/experiment/MulticastTest.java
@@ -1,4 +1,50 @@
 package org.excavator.boot.experiment;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
 public class MulticastTest {
+    public static void main(String[] args) throws IOException {
+        var multicastTest = new MulticastTest();
+        multicastTest.listAllBroadcastAddresses().forEach(System.out::println);
+        var receiver = new MulticastReceiver();
+        receiver.start();
+
+        var publisher = new MulticastPublisher();
+        IntStream.range(0, 10).forEach(i -> {
+            try {
+                publisher.multicast("hello"+i);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        publisher.multicast("end");
+    }
+
+    List<InetAddress> listAllBroadcastAddresses() throws SocketException {
+        List<InetAddress> broadcastList = new ArrayList<>();
+        Enumeration<NetworkInterface> interfaces
+                = NetworkInterface.getNetworkInterfaces();
+        while (interfaces.hasMoreElements()) {
+            NetworkInterface networkInterface = interfaces.nextElement();
+
+            if (networkInterface.isLoopback() || !networkInterface.isUp()) {
+                continue;
+            }
+
+            networkInterface.getInterfaceAddresses().stream()
+                    .map(a -> a.getBroadcast())
+                    .filter(Objects::nonNull)
+                    .forEach(broadcastList::add);
+        }
+        return broadcastList;
+    }
 }
